3602023da449c8db9e61403d5665765557190923
osoken <osoken.devel@outlook.jp>
2020-02-29 13:01:06 +0900

    fix layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,90 +1,51 @@
 <template>
   <v-app dark>
     <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+      permanent
       app
+      width=360
     >
       <v-list>
-        <v-list-item
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              {{ menu.title }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="subtitle">
+              {{ menu.subtitle }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-container
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
           router
           exact
         >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider v-show="item.divider"></v-divider>
+        </v-container>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -92,24 +53,44 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
+      menu: {
+        title: 'TOKYO METROPOLITAN GOVERNMENT',
+        subtitle: 'Covid-19 Operation Dashboard'
+      },
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: '東京都最新感染動向',
           to: '/'
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          title: '症状が心配な方へ',
+          to: '/inspire',
+          divider: true
+        },
+        {
+          icon: 'mdi-account-child',
+          title: 'お子さんをお持ちのみなさんへ',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '都民の皆さんへ',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '企業の皆さんへ',
+          to: '/inspire',
+          divider: true
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: '公式発表',
+          to: '/inspire',
+        },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: 'Vuetify.js'
     }
   }
