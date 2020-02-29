46c45832869339fb955bae1baeba0a575536d03d
osoken <osoken.devel@outlook.jp>
2020-02-29 19:28:48 +0900

    make DataView template

---
+++ components/DataView.vue
@@ -0,0 +1,23 @@
+<template>
+<v-card>
+  <v-toolbar>
+    <v-toolbar-title>
+      {{ title }}
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <slot name="button">button</slot>
+  </v-toolbar>
+  <v-card-text>
+    <slot></slot>
+  </v-card-text>
+</v-card>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
