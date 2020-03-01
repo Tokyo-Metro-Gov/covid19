ce2a081b1db59e8fd2aa69bba295215019864ce7
osoken <osoken.devel@outlook.jp>
2020-03-01 22:23:15 +0900

    implement DataTable component

---
+++ components/DataTable.vue
@@ -0,0 +1,28 @@
+<template>
+  <data-view :title="title">
+    <template v-slot:button>
+      {{ `${chartData.datasets.length} 人` }}
+    </template>
+    <v-data-table
+      :headers="chartData.headers"
+      :items="chartData.datasets"
+      :items-per-page=-1
+      :hide-default-footer="true"
+      :height=200
+    ></v-data-table>
+  </data-view>
+</template>
+
+<style></style>
+
+<script>
+import DataView from '@/components/DataView.vue'
+
+export default {
+  components: { DataView },
+  props: {
+    title: String,
+    chartData: Object
+  }
+}
+</script>
