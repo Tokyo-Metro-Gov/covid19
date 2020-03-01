75839baeb9b00cb729b154f6b1f88010fd980b6c
mikkame <matchasoft.jp@gmail.com>
2020-03-01 16:49:34 +0900

    try capture screenshot with CI

---
+++ ui-test/screenshot.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import os
+
+os.mkdir('screenshots')
+
+def capture(browser, path):
+	driver = getattr(webdriver, browser)()
+	driver.set_window_size(1280, 720)
+	driver.get("http://localhost:3000"+path)
+	driver.save_screenshot('screenshots/'+browser+'.png')
+PATHS = ['/']
+BROWSERS = ['Chrome', 'Firefox']
+
+for browser in BROWSERS:
+	for path in PATHS:
+		capture(browser,path)
