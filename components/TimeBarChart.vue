68c418f66aa80b2477d2c6cc3cb2303e0f07f040
yokinist <ikd618@gmail.com>
2020-03-01 10:49:05 +0900

    :recycle: formart by lint

--- components/TimeBarChart.vue
+++ components/TimeBarChart.vue
@@ -1,50 +1,54 @@
 <template>
-<data-view :title="title">
-  <template v-slot:button>
-    <data-selector v-model="dataKind"></data-selector>
-  </template>
-  <bar :chart-data="displayData" :options="chartOption"></bar>
-</data-view>
+  <data-view :title="title">
+    <template v-slot:button>
+      <data-selector v-model="dataKind" />
+    </template>
+    <bar :chart-data="displayData" :options="chartOption" />
+  </data-view>
 </template>
 
 <style>
 </style>
 
 <script>
-import DataView from '@/components/DataView.vue';
-import DataSelector from '@/components/DataSelector.vue';
+import DataView from '@/components/DataView.vue'
+import DataSelector from '@/components/DataSelector.vue'
 
 export default {
   components: { DataView, DataSelector },
-  props: [ 'title', 'chartData', 'chartOption' ],
-  data() {
+  props: {
+    title: String,
+    chartData: Object,
+    chartOption: Object
+  },
+  data () {
     return {
       dataKind: 'transition'
-    };
-  },
-  computed: {
-    displayData() {
-      if (this.dataKind === 'transition') {
-        return {
-          labels: this.chartData.map(function(d) {return d.label;}),
-          datasets: [{
-              label: this.dataKind,
-              data: this.chartData.map(function(d) {return d.transition;}),
-              backgroundColor: 'green',
-              borderWidth: 1
-          }]
-        };
-      }
-      return {
-        labels: this.chartData.map(function(d) {return d.label;}),
-        datasets: [{
-            label: this.dataKind,
-            data: this.chartData.map(function(d) {return d.cummulative;}),
-            backgroundColor: 'green',
-            borderWidth: 1
-        }]
-      };
     }
   },
-};
+  computed: {
+    displayData () {
+      if (this.dataKind === 'transition') {
+        return {
+          labels: this.chartData.map(function (d) { return d.label }),
+          datasets: [{
+            label: this.dataKind,
+            data: this.chartData.map(function (d) { return d.transition }),
+            backgroundColor: 'green',
+            borderWidth: 1
+          }]
+        }
+      }
+      return {
+        labels: this.chartData.map(function (d) { return d.label }),
+        datasets: [{
+          label: this.dataKind,
+          data: this.chartData.map(function (d) { return d.cummulative }),
+          backgroundColor: 'green',
+          borderWidth: 1
+        }]
+      }
+    }
+  }
+}
 </script>
