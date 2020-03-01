70d5b2a3e82944a6f04618aeb50113bf600824fe
yokinist <ikd618@gmail.com>
2020-03-01 18:27:10 +0900

    :+1: Create flow page

---
+++ pages/flow.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/flowing.png" alt="相談手順の画像" class="flow-image" />
+    </v-flex>
+  </v-layout>
+</template>
+
+<style>
+.flow-image {
+  max-width: 80%;
+}
+</style>
