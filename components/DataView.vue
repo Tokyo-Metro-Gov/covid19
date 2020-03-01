63fa3162b8562b36e3b1f2db0ab0b523ea7d1cf8
osoken <osoken.devel@outlook.jp>
2020-03-01 12:45:31 +0900

    tidy card header and footer styles

--- components/DataView.vue
+++ components/DataView.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card>
-    <v-toolbar flat class="CardTitleBar">
+    <v-toolbar flat class="CardHeader">
       <v-toolbar-title class="CardTitle">
         {{ title }}
       </v-toolbar-title>
@@ -10,11 +10,29 @@
     <v-card-text>
       <slot />
     </v-card-text>
+    <v-footer class="CardFooter">
+      {{ lastUpdateDate | format-date }}
+    </v-footer>
   </v-card>
 </template>
 
 <style>
-.CardTitleBar {
+.CardHeader {
+  font-family: Hiragino Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;
+  color: #4D4D4D;
+}
+.CardFooter {
+  font-family: Hiragino Sans;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 10px;
+  line-height: 15px;
+
+  color: #808080;
 }
 </style>
 
@@ -24,5 +42,8 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class DataView extends Vue {
   @Prop() private title!: string;
+  @Prop({
+    default: new Date()
+  }) private lastUpdateDate!: Date;
 }
 </script>
