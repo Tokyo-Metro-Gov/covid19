df62fed2726116b0aa69c5e6099384203fc1feb0
kenshir0f <khts.wir17@gmail.com>
2020-03-01 21:54:13 +0900

    create TextCard component

---
+++ components/TextCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="TextCard">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from 'vue-property-decorator'
+
+@Component
+export default class TextCard extends Vue {
+  @Prop({
+    required: true
+  })
+  title!: string
+}
+</script>
+
+<style lang="scss">
+.TextCard {
+  @include card-container();
+}
+</style>
