37abc853bda21814d85d4d5a8e4763fba9d50e45
yokinist <ikd618@gmail.com>
2020-03-01 14:14:44 +0900

    :recycle: format code

--- components/StaticInfo.vue
+++ components/StaticInfo.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="staticInfoOuter">
     <a :href="url">{{ text }}</a>
-    <div class="btn">{{ btnText }}</div>
+    <div class="btn">
+      {{ btnText }}
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['url', 'text', 'btnText'],
-  }
+export default {
+  props: ['url', 'text', 'btnText']
+}
 </script>
 
 <style>
-  .staticInfoOuter {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background: #FFF;
-    border: 1px solid #D9D9D9;
-    box-shadow: 0px 0px 2px rgba(0, 0, 0, .15);
-    border-radius: 4px;
-    padding: .5em 1em;
-  }
-  .btn {
-    border: 1px solid #00B849;
-    border-radius: 4px;
-    color: #00A040;
-    font-size: .875rem;
-    padding: .5em;
-  }
+.staticInfoOuter {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  border: 1px solid #d9d9d9;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  padding: 0.5em 1em;
+}
+.btn {
+  border: 1px solid #00b849;
+  border-radius: 4px;
+  color: #00a040;
+  font-size: 0.875rem;
+  padding: 0.5em;
+}
 </style>
