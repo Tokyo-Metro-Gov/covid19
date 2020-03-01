0fb2dd640fb3c94d854f7ae0a870c0a7acc624d7
kenshir0f <khts.wir17@gmail.com>
2020-03-01 22:37:07 +0900

    show about details

--- components/TextCard.vue
+++ components/TextCard.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="TextCard">
-    <h2>{{ title }}</h2>
+    <h2 v-if="title" class="TextCard-Heading">
+      {{ title }}
+    </h2>
+    <p class="TextCard-Body">
+      {{ body }}
+    </p>
   </div>
 </template>
 
@@ -10,14 +15,31 @@ import { Vue, Prop, Component } from 'vue-property-decorator'
 @Component
 export default class TextCard extends Vue {
   @Prop({
-    required: true
+    default: '',
+    required: false
   })
   title!: string
+
+  @Prop({
+    default: '',
+    required: true
+  })
+  body!: string
 }
 </script>
 
 <style lang="scss">
 .TextCard {
   @include card-container();
+  padding: 20px;
+  margin-bottom: 20px;
+  &-Heading {
+    @include card-h1();
+    margin-bottom: 12px;
+  }
+  &-Body {
+    @include body-text();
+    margin-bottom: 0px !important;
+  }
 }
 </style>
