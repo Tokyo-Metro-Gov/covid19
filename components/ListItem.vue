d1cbf3ffd430dfebdd4c5d3287c4a240334433f5
kenshir0f <khts.wir17@gmail.com>
2020-03-01 15:21:00 +0900

    disable ripple effect

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -1,15 +1,23 @@
 <template>
   <v-list-item
+    v-ripple="false"
     :to="isInternalLink(link) ? link : ''"
     :href="!isInternalLink(link) ? link : ''"
     router
     exact
+    class="ListItem-Container"
+    style="color: transparent"
   >
-    <v-list-item-action v-if="icon">
-      <v-icon>{{ icon }}</v-icon>
+    <v-list-item-action v-if="icon" class="ListItem-IconContainer">
+      <v-icon :class="isActive(icon)" size="20">
+        {{ icon }}
+      </v-icon>
     </v-list-item-action>
     <v-list-item-content>
-      <v-list-item-title v-text="title" />
+      <v-list-item-title
+        :class="['ListItem-Text', isActive(icon)]"
+        v-text="title"
+      />
     </v-list-item-content>
   </v-list-item>
 </template>
@@ -40,5 +48,33 @@ export default class ListItem extends Vue {
   isInternalLink(path: string): boolean {
     return !/^https?:\/\//.test(path)
   }
+
+  isActive(icon: string): string | undefined {
+    if (icon === 'mdi-chart-timeline-variant') {
+      return 'isActive'
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.ListItem {
+  &-Container {
+    padding: 0;
+    color: transparent !important;
+    &:hover {
+      color: transparent !important;
+    }
+  }
+  &-Text {
+    font-size: 12px;
+  }
+  &-IconContainer {
+    margin: 8px 6px 8px 0 !important;
+  }
+}
+.isActive {
+  color: $green-1 !important;
+  font-weight: 600;
+}
+</style>
