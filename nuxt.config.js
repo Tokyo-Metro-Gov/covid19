70d92afed51ad483d9e293ef6e16839b826f9d86
osoken <osoken.devel@outlook.jp>
2020-03-01 12:45:31 +0900

    tidy card header and footer styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,8 @@ module.exports = {
     {
       src: '@/plugins/vue-chart.js',
       ssr: true
-    }
+    },
+    '@/plugins/datetime-formatter.js'
   ],
   /*
   ** Nuxt.js dev-modules
@@ -60,7 +61,8 @@ module.exports = {
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    ['@nuxtjs/moment', ['ja']]
   ],
   /*
   ** Axios module configuration
