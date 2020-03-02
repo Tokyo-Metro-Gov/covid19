980edf145f5ea9eb90e55bc746aba9b3d1116f67
kenshir0f <khts.wir17@gmail.com>
2020-03-03 01:33:28 +0900

    fix title

--- pages/worker.vue
+++ pages/worker.vue
@@ -18,16 +18,19 @@ export default {
     return {
       items: [
         {
+          title: '中小企業者等特別相談窓口',
           body:
-            '中小企業者等特別相談窓口<br />〇　手洗い、咳エチケット等により、感染予防に努めてください。<br />　<a href="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/01/30/15.html" target="_blank">https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/01/30/15.html</a> <br />資金繰りに関する相談、経営に関する相談（東京都産業労働局 報道発表）'
+            '<a href="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/01/30/15.html" target="_blank">https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/01/30/15.html</a> <br />資金繰りに関する相談、経営に関する相談（東京都産業労働局 報道発表）'
         },
         {
+          title: '緊急労働相談ダイヤル',
           body:
-            '緊急労働相談ダイヤル<br /><a href="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/26/22.html" target="_blank">https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/26/22.html</a><br />新型コロナウイルスに関する休暇や休業の取り扱い、職場のハラスメントなどについての相談（東京都産業労働局　報道発表）'
+            '<a href="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/26/22.html" target="_blank">https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/02/26/22.html</a><br />新型コロナウイルスに関する休暇や休業の取り扱い、職場のハラスメントなどについての相談（東京都産業労働局　報道発表）'
         },
         {
+          title: '新しいワークスタイルや企業活動の東京モデル「スムーズビズ」',
           body:
-            '新しいワークスタイルや企業活動の東京モデル「スムーズビズ」<br /><a href="https://smooth-biz.metro.tokyo.lg.jp/" target="_blank">https://smooth-biz.metro.tokyo.lg.jp/</a><br />テレワーク・時差出勤などスムーズビズの取組は、新型コロナウイルス感染症の対策としても有効です。感染症対策として、東京2020大会時の交通混雑緩和に向けた取組の前倒しをお願いします。'
+            '<a href="https://smooth-biz.metro.tokyo.lg.jp/" target="_blank">https://smooth-biz.metro.tokyo.lg.jp/</a><br />テレワーク・時差出勤などスムーズビズの取組は、新型コロナウイルス感染症の対策としても有効です。感染症対策として、東京2020大会時の交通混雑緩和に向けた取組の前倒しをお願いします。'
         }
       ]
     }
