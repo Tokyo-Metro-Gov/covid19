314502c9b4920213241fe0cab88edc49492d3219
sunecosuri <sunecosuri@gmail.com>
2020-03-04 22:57:27 +0900

    update: fix style

--- components/DataView.vue
+++ components/DataView.vue
@@ -79,7 +79,7 @@ export default class DataView extends Vue {
   }
   &-CardTextForXS {
     margin-bottom: 46px;
-    margin-top: 50px;
+    margin-top: 70px;
   }
   &-Footer {
     background-color: $white !important;
