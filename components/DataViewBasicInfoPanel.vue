961c97d2caeb7c11ab3bf25968eec45a2c71d6b4
Jxck <block.rxckin.beats@gmail.com>
2020-03-04 21:55:45 +0900

    component 内で使われない footer の CSS が上書きしされているようなので削除(ほかのも要らない気がするがそのまま)

--- components/DataViewBasicInfoPanel.vue
+++ components/DataViewBasicInfoPanel.vue
@@ -50,14 +50,6 @@
     margin-bottom: 46px;
     margin-top: 20px;
   }
-  &-Footer {
-    background-color: $white !important;
-    text-align: right;
-    margin: 2px 4px 12px;
-    flex-direction: row-reverse;
-    @include font-size(12);
-    color: $gray-3 !important;
-  }
 }
 .v-toolbar__content {
   height: 80px !important;
