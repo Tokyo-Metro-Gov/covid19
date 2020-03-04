3ef8fd49a43b84cdba1d98e7526fc832fd057d9e
takanakahiko <takanakahiko@gmail.com>
2020-03-05 01:26:01 +0900

    style: migrate css to BEM

--- components/WhatsNew.vue
+++ components/WhatsNew.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="wrapper">
-    <h2 class="heading">
-      <v-icon size="24" class="heading-icon">mdi-information</v-icon>
+  <div class="WhatsNew">
+    <h2 class="WhatsNew-heading">
+      <v-icon size="24" class="WhatsNew-heading-icon">mdi-information</v-icon>
       最新のお知らせ
     </h2>
     <div v-for="(item, i) in items" :key="i">
-      <a class="item" :href="item.url" target="_blank">
-        <time class="time px-2">{{ item.date }}</time>
-        <span class="link">{{ item.text }}</span>
+      <a class="WhatsNew-item" :href="item.url" target="_blank">
+        <time class="WhatsNew-item-time px-2">{{ item.date }}</time>
+        <span class="WhatsNew-item-link">{{ item.text }}</span>
       </a>
     </div>
   </div>
@@ -25,13 +25,13 @@ export default {
 </script>
 
 <style lang="scss">
-.wrapper{
+.WhatsNew{
   @include card-container();
   padding: 10px;
   margin-bottom: 20px;
 }
 
-.heading {
+.WhatsNew-heading {
   display: flex;
   align-items: center;
   @include card-h2();
@@ -44,18 +44,18 @@ export default {
   }
 }
 
-.item {
+.WhatsNew-item {
   display: flex;
   text-decoration: none;
   margin: 5px;
   font-size: 14px;
 
-  .time {
+  &-time {
     flex: 0 0 90px;
     color: $gray-1;
   }
 
-  .link {
+  &-link {
     flex: 0 1 auto;
     @include text-link();
   }
