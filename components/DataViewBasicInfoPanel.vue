875681edf38b48b65fa13e90d189bc114d10288a
sunecosuri <sunecosuri@gmail.com>
2020-03-04 22:57:27 +0900

    update: fix style

--- components/DataViewBasicInfoPanel.vue
+++ components/DataViewBasicInfoPanel.vue
@@ -46,10 +46,6 @@
   &-Title {
     @include card-h2();
   }
-  &-CardText {
-    margin-bottom: 46px;
-    margin-top: 20px;
-  }
   &-Footer {
     background-color: $white !important;
     text-align: right;
