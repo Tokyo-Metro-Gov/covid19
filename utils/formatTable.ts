4e157d2a623022f7a81b172cb8cf98ec195a515e
Masaya Kazama <miyan@t-p.jp>
2020-03-04 23:54:16 +0900

    日付情報を数値化してフォーマット

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -1,14 +1,12 @@
-import moment from 'moment'
-
 const headers = [
-  { text: '日付', value: '日付' },
-  { text: '居住地', value: '居住地' },
-  { text: '年代', value: '年代' },
-  { text: '性別', value: '性別' }
+  { text: '日付', value: 'time' },
+  { text: '居住地', value: 'residence' },
+  { text: '年代', value: 'age' },
+  { text: '性別', value: 'gender' }
 ]
 
 type DataType = {
-  リリース日: Date
+  リリース日: string
   居住地: string | null
   年代: string | null
   性別: '男性' | '女性'
@@ -16,31 +14,26 @@ type DataType = {
 }
 
 type TableDataType = {
-  日付: string
-  居住地: DataType['居住地']
-  年代: DataType['年代']
-  性別: DataType['性別'] | '不明'
-}
-
-type TableDateType = {
-  headers: typeof headers
-  datasets: TableDataType[]
+  date: Date,
+  time: number,
+  residence: DataType['居住地']
+  age: DataType['年代']
+  gender: DataType['性別'] | '不明'
 }
 
 export default (data: DataType[]) => {
-  const tableDate: TableDateType = {
-    headers,
-    datasets: []
-  }
-  data.forEach(d => {
-    const TableRow: TableDataType = {
-      日付: moment(d['リリース日']).format('MM/DD') ?? '不明',
-      居住地: d['居住地'] ?? '不明',
-      年代: d['年代'] ?? '不明',
-      性別: d['性別'] ?? '不明'
+  const datasets: TableDataType[] = data.map(item => {
+    const date = new Date(item['リリース日'])
+    return {
+      date,
+      time: date.getTime(),
+      residence: item['居住地'] ?? '不明',
+      age: item['年代'] ?? '不明',
+      gender: item['性別'] ?? '不明'
     }
-    tableDate.datasets.push(TableRow)
   })
-  tableDate.datasets.sort((a, b) => (a === b ? 0 : a < b ? 1 : -1))
-  return tableDate
+  return {
+    headers,
+    datasets
+  }
 }
