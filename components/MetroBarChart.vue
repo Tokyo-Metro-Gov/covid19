4e3fbcbf419e7196bd8d62e9e8c478bf4398683b
shgtkshruch <shgtk.shruch@gmail.com>
2020-03-05 15:59:48 +0900

    Move up graph description

--- components/MetroBarChart.vue
+++ components/MetroBarChart.vue
@@ -1,10 +1,12 @@
 <template>
   <data-view :title="title" :date="date">
-    <p class="MetroGraph-Desc">
-      {{ chartData.base_period }}の利用者数*の平均値を100としたときの相対値
-      <br />
-      *都営地下鉄4路線の自動改札出場数
-    </p>
+    <template v-slot:button>
+      <p class="MetroGraph-Desc">
+        {{ chartData.base_period }}の利用者数*の平均値を100としたときの相対値
+        <br />
+        *都営地下鉄4路線の自動改札出場数
+      </p>
+    </template>
     <bar :chart-data="displayData" :options="chartOption" :height="240" />
   </data-view>
 </template>
@@ -12,7 +14,8 @@
 <style lang="scss">
 .MetroGraph {
   &-Desc {
-    margin-top: -20px;
+    margin-top: 10px;
+    margin-bottom: 0 !important;
     font-size: 12px;
     color: #808080;
   }
