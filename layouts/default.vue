88074ef060cc6559ff1bac75ffa695f85c368636
kawoka <br2jn51@yahoo.co.jp>
2020-03-05 23:41:00 +0900

    fix lint layouts/default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,13 +29,6 @@ export default {
     ScaleLoader,
     SideNavigation
   },
-  head() {
-    return {
-      link: [
-        { rel: 'canonical', href: `https://stopcovid19.metro.tokyo.lg.jp${this.$route.path}` },
-      ],
-    };
-  },
   data() {
     return {
       isNaviOpen: false,
