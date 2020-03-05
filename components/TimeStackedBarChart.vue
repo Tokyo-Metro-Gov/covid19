bb44933fb1c8532d40cb04e023fb7641c99723f7
Kazuki Imamura <kaizumaki@gmail.com>
2020-03-05 22:25:58 +0900

    fix tooltip to display sum number & refarctaring

--- components/TimeStackedBarChart.vue
+++ components/TimeStackedBarChart.vue
@@ -19,36 +19,6 @@ import DataView from '@/components/DataView.vue'
 import DataSelector from '@/components/DataSelector.vue'
 import DataViewBasicInfoPanel from '@/components/DataViewBasicInfoPanel.vue'
 
-function cumulative(array) {
-  const cumulativeArray = [];
-  let patSum = 0;
-  array.forEach(d => {
-    patSum += d;
-    cumulativeArray.push(patSum);
-  });
-  return cumulativeArray;
-}
-
-function sum(array) {
-  return array.reduce((acc, cur) => {
-    return acc + cur;
-  })
-}
-
-function pickLastNumber(chartDataArray) {
-  return chartDataArray.map(array => {
-    return array[array.length - 1];
-  })
-}
-
-function cumulativeSum(chartDataArray) {
-  return chartDataArray.map(array => {
-    return array.reduce((acc, cur) => {
-      return acc + cur;
-    })
-  });
-}
-
 export default {
   components: { DataView, DataSelector, DataViewBasicInfoPanel },
   props: {
@@ -88,17 +58,52 @@ export default {
       dataKind: 'transition'
     }
   },
+  methods: {
+    cumulative: function(array) {
+      const cumulativeArray = [];
+      let patSum = 0;
+      array.forEach(d => {
+        patSum += d;
+        cumulativeArray.push(patSum);
+      });
+      return cumulativeArray;
+    },
+    sum: function(array) {
+      return array.reduce((acc, cur) => {
+        return acc + cur;
+      })
+    },
+    pickLastNumber: function(chartDataArray) {
+      return chartDataArray.map(array => {
+        return array[array.length - 1];
+      })
+    },
+    cumulativeSum: function(chartDataArray) {
+      return chartDataArray.map(array => {
+        return array.reduce((acc, cur) => {
+          return acc + cur;
+        })
+      });
+    },
+    eachArraySum: function(chartDataArray) {
+      const sumArray = [];
+      for(let i = 0; i < chartDataArray[0].length; i++){
+        sumArray.push(chartDataArray[0][i] + chartDataArray[1][i]);
+      }
+      return sumArray;
+    }
+  },
   computed: {
     displayInfo() {
       if (this.dataKind === 'transition') {
         return {
-          lText: sum(pickLastNumber(this.chartData)).toLocaleString(),
+          lText: this.sum(this.pickLastNumber(this.chartData)).toLocaleString(),
           sText: `${this.labels[this.labels.length - 1]} の合計`,
           unit: this.unit
         }
       }
       return {
-        lText: sum(cumulativeSum(this.chartData)).toLocaleString(),
+        lText: this.sum(this.cumulativeSum(this.chartData)).toLocaleString(),
         sText: `${this.labels[this.labels.length - 1]} の全体累計`,
         unit: this.unit
       }
@@ -123,7 +128,7 @@ export default {
         datasets: this.chartData.map((item, index) => {
           return {
             label: this.items[index],
-            data: cumulative(item),
+            data: this.cumulative(item),
             backgroundColor: colorArray[index],
             borderWidth: 0
           }
@@ -131,12 +136,17 @@ export default {
       }
     },
     options() {
+      const unit = this.unit
+      const sumArray = this.eachArraySum(this.chartData)
+      const data = this.chartData
+      const cumulativeData = this.chartData.map(item => {return this.cumulative(item)})
+      const cumulativeSumArray = this.eachArraySum(cumulativeData)
       return {
         tooltips: {
           displayColors: false,
           callbacks: {
-            label(tooltipItem) {
-              const labelText = tooltipItem.value + '人'
+            label: (tooltipItem) => {
+              const labelText = this.dataKind === 'transition' ? `${sumArray[tooltipItem.index]}${unit}（都内: ${data[0][tooltipItem.index]}/その他: ${data[1][tooltipItem.index]}）` : `${cumulativeSumArray[tooltipItem.index]}${unit}（都内: ${cumulativeData[0][tooltipItem.index]}/その他: ${cumulativeData[1][tooltipItem.index]}）`
               return labelText
             }
           }
