107fc6d0e6cada7ff8a15fe8a375b56b7496dca4
MaySoMusician <maysomusician@gmail.com>
2020-03-06 00:12:58 +0900

    Fix small lint errors

--- pages/flow.vue
+++ pages/flow.vue
@@ -76,9 +76,9 @@
             />
           </div>
           <div class="TelLink">
-            <a href="tel:0570550571"
-              ><img src="/flow/sp/sp_flow_tel_01@2x.png" alt="0570-550571"
-            /></a>
+            <a href="tel:0570550571">
+              <img src="/flow/sp/sp_flow_tel_01@2x.png" alt="0570-550571" />
+            </a>
           </div>
           <div class="mt-4">
             <a href="#consult">
