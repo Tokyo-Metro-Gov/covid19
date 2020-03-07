2960594fe9ca0f6c8bb2690b99bb1b83bf195efb
yusakuOono <61856777+yusakuOono@users.noreply.github.com>
2020-03-07 22:56:05 +0900

    🐶エラーが取れない為yarn run lint --fixを実行🐶

--- components/flow/FlowSpGeneral.vue
+++ components/flow/FlowSpGeneral.vue
@@ -1,8 +1,10 @@
 <template>
   <div :class="$style.General">
     <div class="mx-2 mb-5">
-      <HumanIcon :class="$style.GeneralIcon"/>
-      <p :class="$style.GeneralHeader">一般の方</p>
+      <HumanIcon :class="$style.GeneralIcon" />
+      <p :class="$style.GeneralHeader">
+        一般の方
+      </p>
     </div>
 
     <div :class="$style.GeneralFlex">
@@ -10,24 +12,30 @@
         <CheckIcon />
         <p>
           風邪
-          <span>のような症状</span>
+          <span>
+            のような症状
+          </span>
         </p>
       </div>
       <div :class="$style.GeneralFlexItem">
         <CheckIcon />
         <p>
           <span>発熱</span>
-          <br/>
+          <br />
           37.5℃以上
         </p>
       </div>
       <div :class="$style.GeneralFlexItem">
         <CheckIcon />
-        <p>強いだるさ</p>
+        <p>
+          強いだるさ
+        </p>
       </div>
       <div :class="$style.GeneralFlexItem">
         <CheckIcon />
-        <p>息苦しさ</p>
+        <p>
+          息苦しさ
+        </p>
       </div>
     </div>
 
@@ -40,21 +48,21 @@
     </p>
 
     <a :class="$style.GeneralBtn" href="#consult">
-        <p>
-          新型コロナ受診相談窓口へ
-          <ArrowIcon/>
-        </p>
+      <p>
+        新型コロナ受診相談窓口へ
+        <ArrowIcon />
+      </p>
     </a>
   </div>
 </template>
 
 <script>
-  import HumanIcon from '@/static/flow/accessibility-24px.svg'
-  import CheckIcon from '@/static/flow/check_circle-24px.svg'
-  import ArrowIcon from '@/static/flow/arrow_forward-24px.svg'
-  export default {
-    components: { HumanIcon,CheckIcon,ArrowIcon },
-  }
+import HumanIcon from '@/static/flow/accessibility-24px.svg'
+import CheckIcon from '@/static/flow/check_circle-24px.svg'
+import ArrowIcon from '@/static/flow/arrow_forward-24px.svg'
+export default {
+  components: { HumanIcon, CheckIcon, ArrowIcon }
+}
 </script>
 
 <style module lang="scss">
@@ -63,38 +71,38 @@
   text-align: center;
   padding: 24px 20px 18px;
   margin-bottom: 20px;
-  &Icon{
+  &Icon {
     width: 40px;
     height: 40px;
   }
-  &Header{
+  &Header {
     @include font-size(14);
     color: $gray-1;
     font-weight: bold;
   }
-  &Flex{
+  &Flex {
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
-    &Item{
+    &Item {
       width: 48%;
       margin: 4% 2%;
       line-height: 14px;
       border-radius: 4px;
-      border: 2px solid #00A040;
+      border: 2px solid #00a040;
       position: relative;
-      &:nth-child(odd){
+      &:nth-child(odd) {
         margin-left: 0;
       }
-      &:nth-child(even){
+      &:nth-child(even) {
         margin-right: 0;
       }
-      &:nth-child(2){
-        p{
+      &:nth-child(2) {
+        p {
           line-height: 1;
         }
       }
-      svg{
+      svg {
         position: absolute;
         left: 50%;
         top: -2px;
@@ -103,54 +111,54 @@
         width: 24px;
         transform: translate(-50%, -50%);
       }
-      p{
+      p {
         @include font-size(18);
         color: $gray-2;
         font-weight: 600;
         padding: 8px;
         margin: 0;
         line-height: 34px;
-        span{
+        span {
           @include font-size(12);
         }
       }
     }
   }
-  &Text{
+  &Text {
     @include font-size(16);
     color: $gray-2;
     font-weight: bold;
     margin-bottom: 8px;
-    span{
+    span {
       border-bottom: 4px solid $green-1;
       letter-spacing: -2px;
       @include font-size(24);
       margin-right: 4px;
-      strong{
+      strong {
         @include font-size(40);
       }
     }
   }
-  &Btn{
+  &Btn {
     text-decoration: none;
-    p{
+    p {
       color: $gray-2;
       text-align: left;
       @include font-size(16);
       font-weight: bold;
       padding: 18px 22px;
-      background: #FFE200;
+      background: #ffe200;
       margin: 0;
       border-radius: 4px;
-      box-shadow: -1px 3px 3px 0px rgba(0,0,0,0.2);
+      box-shadow: -1px 3px 3px 0px rgba(0, 0, 0, 0.2);
       position: relative;
-      svg{
-          position: absolute;
-          width: 36px;
-          height: 36px;
-          top: 50%;
-          right: -8px;
-          transform: translate(-50%, -50%) rotate(90deg);
+      svg {
+        position: absolute;
+        width: 36px;
+        height: 36px;
+        top: 50%;
+        right: -8px;
+        transform: translate(-50%, -50%) rotate(90deg);
       }
     }
   }
