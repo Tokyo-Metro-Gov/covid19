3da473f54d60d44b08d81b2c945bb99804c9f52f
Yasuhiro Manai <yasuhiro.manai@gmail.com>
2020-03-08 04:41:28 +0900

    refactor: separate data card component begins from confirmed cases card

---
+++ pages/cards/_card.vue
@@ -0,0 +1,17 @@
+<template>
+  <confirmed-cases-card />
+</template>
+
+<script>
+import ConfirmedCasesCard from '@/components/cards/ConfirmedCasesCard.vue'
+
+export default {
+  components: {
+    ConfirmedCasesCard
+  },
+  // layout: 'card'
+}
+</script>
+
+<style lang="scss">
+</style>
