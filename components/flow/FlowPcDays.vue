22f3a815489892bccff118935696ac5831fa6d34
Taro Matsuzawa <taro@georepublic.co.jp>
2020-03-07 15:44:28 +0900

    #611 finish create FlowPcDays

--- components/flow/FlowPcDays.vue
+++ components/flow/FlowPcDays.vue
@@ -1,50 +1,76 @@
 <template>
-  <div class="Flow-Card-Parts Flow-PC-Days">
-    <div class="row">
-      <div>
+  <div class="Flow-PC-Days Flow-Card-Parts">
+    <div class="Flow-PC-Days-Row">
+      <div class="Flow-PC-Days-Row-3">
         <img src="/flow/accessibility-24px.svg" />
         <p>一般の方</p>
       </div>
       <div>
         <p>
-          <span class="emphasis">
-            <span class="day">４</span>
+          <span class="Flow-PC-Days-Emphasis">
+            <span class="Flow-PC-Days-Emphasis-Day">４</span>
             日程度
           </span>
           続いている
         </p>
       </div>
     </div>
-    <div class="row">
-      <div class="condition">
-        <p>風邪 のような症状</p>
+    <div class="Flow-PC-Days-Row">
+      <div class="Flow-PC-Days-Condition">
+        <i>
+          <img src="/flow/check_circle-24px.svg" />
+        </i>
+        <p>
+          <span>風邪 </span>
+          のような症状
+        </p>
       </div>
-      <div class="condition">
-        <p>発熱 37.5℃ 以上</p>
+      <div class="Flow-PC-Days-Condition">
+        <i>
+          <img src="/flow/check_circle-24px.svg" />
+        </i>
+        <p>
+          発熱
+          <span> 37.5℃ </span>
+          以上
+        </p>
       </div>
-      <div class="condition">
+      <div class="Flow-PC-Days-Condition">
+        <i>
+          <img src="/flow/check_circle-24px.svg" />
+        </i>
         <p>強いだるさ</p>
       </div>
-      <div class="condition">
+      <div class="Flow-PC-Days-Condition">
+        <i>
+          <img src="/flow/check_circle-24px.svg" />
+        </i>
         <p>息苦しさ</p>
       </div>
     </div>
-    <div class="row">
-      <div>
-        <p><img src="/flow/directions_walk-24px.svg" />ご高齢な方</p>
-        <p><img src="/flow/accessible-24px.svg" />基礎疾患のある方</p>
-        <p><img src="/flow/pregnant_woman-24px.svg" />妊娠中の方</p>
+    <div class="Flow-PC-Days-Row">
+      <div class="Flow-PC-Days-Row-3">
+        <div class="Flow-PC-Days-Care-Target-List">
+          <p>
+            <img src="/flow/directions_walk-24px.svg" />ご高齢な方<br />
+            <img src="/flow/accessible-24px.svg" />基礎疾患のある方<br />
+            <img src="/flow/pregnant_woman-24px.svg" />妊娠中の方<br />
+          </p>
+        </div>
       </div>
       <div>
         <p>
-          <span class="emphasis">
-            <span class="day">２</span>
+          <span class="Flow-PC-Days-Emphasis">
+            <span class="Flow-PC-Days-Emphasis-Day">２</span>
             日程度
           </span>
           続いている
         </p>
       </div>
     </div>
+    <i class="Flow-PC-Days-Arrow">
+      <img src="/flow/flow_arrow.svg" />
+    </i>
   </div>
 </template>
 
@@ -53,24 +79,66 @@
   display: flex;
   flex-direction: row;
   height: 256px;
-  .row {
+  padding: 20px 20px !important;
+  position: relative;
+  .Flow-PC-Days-Row {
     flex: 1;
     display: flex;
     flex-direction: column;
+    justify-content: center;
+    .Flow-PC-Days-Row-3 {
+      flex: 3;
+      margin-top: 20px;
+      .Flow-PC-Days-Care-Target-List {
+        margin: 16px auto;
+        text-align: center;
+        display: block;
+        p {
+          text-align: left;
+          display: inline-block;
+        }
+      }
+    }
+    .Flow-PC-Days-Condition {
+      flex: 1;
+      width: 170px;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-bottom: 10px;
+      position: relative;
+      border: 2px solid $green-1 !important;
+      border-radius: 2px;
+      p {
+        margin: auto;
+        text-align: center;
+        display: inline-block;
+        font-size: 14px;
+        span {
+          font-size: 20px;
+        }
+      }
+      i {
+        position: absolute;
+        top: -12px;
+        left: -12px;
+        background-color: $white;
+      }
+    }
+    .Flow-PC-Days-Emphasis {
+      font-size: 24px;
+      border-bottom: solid 3px $green-1;
+      .Flow-PC-Days-Emphasis-Day {
+        font-size: 41px;
+      }
+    }
   }
-}
-.emphasis {
-  font-size: 24px;
-  border-bottom: solid 3px $green-1;
-  .day {
-    font-size: 41px;
+  .Flow-PC-Days-Arrow {
+    position: absolute;
+    right: -20px;
+    bottom: 20px;
   }
-}
-.condition {
-  border: 2px solid $green-1 !important;
-  border-radius: 2px;
-  width: 170px;
-  height: 50px;
 }
 </style>
 
