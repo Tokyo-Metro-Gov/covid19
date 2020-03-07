dbfe37417ebfeaebea282ad109ce2d443bb80b78
tatsuki.e <ik.t2k.62@gmail.com>
2020-03-07 18:22:39 +0900

    whatsnew news enclosed in list #724

--- components/WhatsNew.vue
+++ components/WhatsNew.vue
@@ -7,19 +7,26 @@
       最新のお知らせ
     </h2>
     <div v-for="(item, i) in items" :key="i">
-      <a class="WhatsNew-item" :href="item.url" target="_blank" rel="noopener">
-        <time class="WhatsNew-item-time px-2">{{ item.date }}</time>
-        <span class="WhatsNew-item-link">
-          {{ item.text }}
-          <v-icon
-            v-if="!isInternalLink(item.url)"
-            class="WhatsNew-item-ExternalLinkIcon"
-            size="12"
-          >
-            mdi-open-in-new
-          </v-icon>
-        </span>
-      </a>
+      <li class="WhatsNew-list">
+        <a
+          class="WhatsNew-list-item"
+          :href="item.url"
+          target="_blank"
+          rel="noopener"
+        >
+          <time class="WhatsNew-list-item-time px-2">{{ item.date }}</time>
+          <span class="WhatsNew-list-item-link">
+            {{ item.text }}
+            <v-icon
+              v-if="!isInternalLink(item.url)"
+              class="WhatsNew-item-ExternalLinkIcon"
+              size="12"
+            >
+              mdi-open-in-new
+            </v-icon>
+          </span>
+        </a>
+      </li>
     </div>
   </div>
 </template>
@@ -59,36 +66,39 @@ export default {
     margin: 3px;
   }
 }
+.WhatsNew-list {
+  list-style-type: none;
 
-.WhatsNew-item {
-  display: flex;
-  text-decoration: none;
-  margin: 5px;
-  font-size: 14px;
+  &-item {
+    display: flex;
+    text-decoration: none;
+    margin: 5px;
+    font-size: 14px;
 
-  @include lessThan($medium) {
-    flex-wrap: wrap;
-  }
-
-  &-time {
-    flex: 0 0 90px;
     @include lessThan($medium) {
-      flex: 0 0 100%;
+      flex-wrap: wrap;
     }
-    color: $gray-1;
-  }
 
-  &-link {
-    flex: 0 1 auto;
-    @include text-link();
-    @include lessThan($medium) {
-      padding-left: 8px;
+    &-time {
+      flex: 0 0 90px;
+      @include lessThan($medium) {
+        flex: 0 0 100%;
+      }
+      color: $gray-1;
     }
-  }
 
-  &-ExternalLinkIcon {
-    margin-left: 2px;
-    color: $gray-3 !important;
+    &-link {
+      flex: 0 1 auto;
+      @include text-link();
+      @include lessThan($medium) {
+        padding-left: 8px;
+      }
+    }
+
+    &-ExternalLinkIcon {
+      margin-left: 2px;
+      color: $gray-3 !important;
+    }
   }
 }
 </style>
