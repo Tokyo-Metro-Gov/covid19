6e6641068c5e11431997404d32c594f6ffa429f2
Yuki Kanasugi <yuki.kanasugi@okan.jp>
2020-03-07 16:21:37 +0900

    create FlowSpHospitalized.vue

---
+++ components/flow/FlowSpHospitalized.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="Hospitalization">
+    <img
+      width="40"
+      height="40"
+      src="/flow/hotel-24px.svg"
+      alt="入院となります／感染症指定医療機関等"
+    />
+    <p class="text">
+      入院となります
+    </p>
+    <p class="largeText">
+      感染症指定医療機関等
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.Hospitalization {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .text {
+    color: $gray-2;
+    font-weight: bold;
+    @include font-size(14);
+  }
+  .largeText {
+    color: $gray-2;
+    font-weight: bold;
+    @include font-size(20);
+  }
+}
+</style>
+
+<script>
+export default {}
+</script>
