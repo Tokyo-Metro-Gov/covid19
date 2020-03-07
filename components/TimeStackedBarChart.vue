25ee7c93b53bb14282650ce0ff5c3e224775ca0c
koji-koji <k.koyama37@gmail.com>
2020-03-07 22:49:52 +0900

    :beginner:ラベルの日本語化。

--- components/TimeStackedBarChart.vue
+++ components/TimeStackedBarChart.vue
@@ -188,7 +188,22 @@ export default {
                 padding: 3,
                 fontStyle: 'bold',
                 callback: label => {
-                  return label.split(' ')[0]
+                  const monthStringArry = [
+                    'Jan',
+                    'Feb',
+                    'Mar',
+                    'Apr',
+                    'May',
+                    'Jun',
+                    'Jul',
+                    'Aug',
+                    'Sep',
+                    'Oct',
+                    'Nov',
+                    'Dec'
+                  ]
+                  const month = monthStringArry.indexOf(label.split(' ')[0]) + 1
+                  return month + '月'
                 }
               },
               type: 'time',
