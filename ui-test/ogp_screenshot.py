6783b31601e57907e2bc7fa7f1b8a87200d05b0b
Yasuhiro Manai <yasuhiro.manai@gmail.com>
2020-03-08 18:40:38 +0900

    fix: ogp screenshot

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -5,6 +5,7 @@ os.mkdir('ogps')
 
 size = [1200,630]
 PATHS = [
+    '/',
     '/cards/details-of-confirmed-cases',
     '/cards/number-of-confirmed-cases',
     '/cards/attributes-of-confirmed-cases'
@@ -14,5 +15,5 @@ browser = 'Chrome'
 driver = getattr(webdriver, browser)()
 driver.set_window_size(size[0], size[1])
 for path in PATHS:
-    driver.get("http://localhost:8000"+path+'?embed=true')
-    driver.save_screenshot('ogps/'+path.replace('/', '_')+'.png')
+    driver.get("http://localhost:8000"+path+"?embed=true")
+    driver.save_screenshot('ogps/'+path.replace('/cards/', '').replace('/', '_')+'.png')
