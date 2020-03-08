63bcdb15799644f5d2feae05b01cbd269b30721a
Tomohiro hibino <cg4526kp@gmail.com>
2020-03-08 09:14:26 +0900

    lint用に修正2

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -13,7 +13,7 @@
       exact
       :class="['ListItem-Link', isActive(link)]"
     >
-      <v-list-item-action v-if="icon" class="ListItem-IconContainer" >
+      <v-list-item-action v-if="icon" class="ListItem-IconContainer">
         <v-icon
           v-if="checkIconType(icon) === 'material'"
           :class="['ListItem-Icon', isActive(link)]"
