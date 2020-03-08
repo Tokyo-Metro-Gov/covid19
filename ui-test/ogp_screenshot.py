f83a563988cea2af8a9a977bd577e30d015d1c20
Yasuhiro Manai <yasuhiro.manai@gmail.com>
2020-03-09 01:38:08 +0900

    fix: Optimize ogp images

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -3,22 +3,22 @@ import os
 
 os.mkdir('ogps')
 
-size = [959,910]
-PATHS = [
-    '/cards/details-of-confirmed-cases',
-    '/cards/number-of-confirmed-cases',
-    '/cards/attributes-of-confirmed-cases',
-    '/cards/number-of-tested',
-    '/cards/number-of-reports-to-covid19-telephone-advisory-center',
-    '/cards/number-of-reports-to-covid19-consultation-desk',
-    '/cards/predicted-number-of-toei-subway-passengers'
-]
+SIZES = [[959,650],[959,600],[959,920]]
+PATHS = {
+    '/cards/details-of-confirmed-cases': SIZES[0],
+    '/cards/number-of-confirmed-cases': SIZES[1],
+    '/cards/attributes-of-confirmed-cases': SIZES[1],
+    '/cards/number-of-tested': SIZES[1],
+    '/cards/number-of-reports-to-covid19-telephone-advisory-center': SIZES[1],
+    '/cards/number-of-reports-to-covid19-consultation-desk': SIZES[1],
+    '/cards/predicted-number-of-toei-subway-passengers': SIZES[2]
+}
 
 options = webdriver.ChromeOptions()
 options.add_argument("--hide-scrollbars")
 
 driver = webdriver.Chrome(options=options)
-driver.set_window_size(size[0], size[1])
-for path in PATHS:
-    driver.get("http://localhost:8000"+path+"?embed=true&share=false")
+for path, size in PATHS.items():
+    driver.set_window_size(size[0], size[1])
+    driver.get("http://localhost:8000"+path+"?embed=true")
     driver.save_screenshot('ogps/ogp-'+path.replace('/cards/', '').replace('/', '_')+'.png')
