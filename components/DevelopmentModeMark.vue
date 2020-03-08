2973c0fba219ff0b920f56a421af9d5b61d2b10d
DJYoko <yokochan.recurrent@gmail.com>
2020-03-09 00:37:27 +0900

    #837 開発中の表示用のコンポーネント新規作成

---
+++ components/DevelopmentModeMark.vue
@@ -0,0 +1,66 @@
+<template>
+  <div v-if="isDevelopmentMode" class="DevelopmentModeMark">
+    開発中（development mode）
+    <a
+      href="https://stopcovid19.metro.tokyo.lg.jp/"
+      target="_blank"
+      rel="noopener"
+    >公開サイトへ</a
+    >
+  </div>
+</template>
+
+<style lang="scss">
+.DevelopmentModeMark {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  font-size: 12px;
+  z-index: 1000;
+  width: 100%;
+  height: 20px;
+  text-align: center;
+  background-color: #ffe200;
+  color: #4d4d4d;
+  line-height: 20px;
+  opacity: 0.9;
+
+  // mobile view
+  @include lessThan($small) {
+    height: 40px;
+    a {
+      display: block;
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  name: 'DevelopmentModeMark',
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  computed: {
+    // TODO 本来は開発環境か否かの判定は当コンポーネントの読み込みの外で用意し
+    // 当コンポーネントの出力判定に用いるべき
+    isDevelopmentMode: () => {
+      // 暫定的にURLの正規表現を公開状態の判定に用いている
+      // 可能であれば環境変数から状態を判定したい
+      const url = 'https://stopcovid19.metro.tokyo.lg.jp'
+      const regex = url.replace(/\//g, '\/').replace(/\./g, '\.')
+      console.log(
+        regex,
+        location.href,
+        new RegExp(regex),
+        location.href.match(new RegExp(regex))
+      )
+      return location.href.match(new RegExp(regex)) === null
+    }
+  }
+}
+</script>
