a427cd51e9dc6f01b8862706766a71c0fed5feba
kebhr <42484226+kebhr@users.noreply.github.com>
2020-03-09 13:13:36 +0900

    [i18n] 言語変更時に同じページに遷移させる

--- components/LanguageSelector.vue
+++ components/LanguageSelector.vue
@@ -29,7 +29,11 @@ import SelectMenuIcon from '@/static/selectmenu.svg'
 })
 export default class LanguageSelector extends Vue {
   navigate(locale: string) {
-    const path = locale === 'ja' ? '/' : '/' + locale
+    const matches = this.$router.currentRoute.path.match(/.*(\/.*)/)
+    if (matches === null) {
+      return
+    }
+    const path = locale === 'ja' ? matches[1] : '/' + locale + matches[1]
     this.$router.push(path)
   }
 }
