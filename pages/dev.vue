d14577c042dd995d6e4dc73a9f96ea6bd14af05a
dmamira <49590399+dmamira@users.noreply.github.com>
2020-03-09 17:05:37 +0900

    fixed

---
+++ pages/dev.vue
@@ -0,0 +1,14 @@
+<template>
+    <FlowSpAccording/>
+</template>
+
+<script>
+  import FlowSpAccording from '../components/flow/FlowSpAccording'
+  export default {
+    components: { FlowSpAccording },
+  }
+</script>
+
+<style scoped>
+
+</style>
