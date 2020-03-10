3bc25c7aa60f7e0ad35ee229b344220fda896017
Yasuhiro Manai <yasuhiro.manai@gmail.com>
2020-03-11 02:23:40 +0900

    fix: remove dynamic url

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -93,7 +93,7 @@ export default {
     return data
   },
   head() {
-    const url = location.protocol + '//' + location.host
+    const url = 'https://deploy-preview-835--dev-covid19-tokyo.netlify.com'
     const ogpImage = url + '/ogp-' + this.$route.params.card + '.png'
     const description =
       this.updatedAt +
