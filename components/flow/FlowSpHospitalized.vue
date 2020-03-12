67bd16047595e0ed4135d71197b829b478d5fb77
sidemt <25644062+sidemt@users.noreply.github.com>
2020-03-13 04:36:37 +0900

    Fix the English translation of 「入院」

--- components/flow/FlowSpHospitalized.vue
+++ components/flow/FlowSpHospitalized.vue
@@ -10,7 +10,38 @@
   </div>
 </template>
 
+<<<<<<< HEAD
 <i18n src="./FlowSpHospitalized.i18n.json"></i18n>
+=======
+<i18n>
+{
+  "ja": {
+    "入院となります": "入院となります",
+    "感染症指定医療機関等": "感染症指定医療機関等"
+  },
+  "en": {
+    "入院となります": "Need to be hospitalized",
+    "感染症指定医療機関等": "designated medical institutions for specified infectious diseases and equivalent institutions"
+  },
+  "zh-cn": {
+    "入院となります": "需要住院",
+    "感染症指定医療機関等": "到传染病指定医疗机构"
+  },
+  "zh-tw": {
+    "入院となります": "須住院治療",
+    "感染症指定医療機関等": "至傳染病專責醫療機構"
+  },
+  "ko": {
+    "入院となります": "입원치료하게됩니다",
+    "感染症指定医療機関等": "감영병전문 의료기관등"
+  },
+  "ja-basic": {
+    "入院となります": "入院（にゅういん）と なります",
+    "感染症指定医療機関等": "感染症指定医療機関（かんせんしょう していいりょうきかん）等（など）"
+  }
+}
+</i18n>
+>>>>>>> Fix the English translation of 「入院」
 
 <script lang="ts">
 import HotelIcon from '@/static/flow/hotel-24px.svg'
