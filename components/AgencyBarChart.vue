5cf6d5406b6140b26e81891c33ff43341c23081c
shgtkshruch <shgtk.shruch@gmail.com>
2020-03-12 23:18:57 +0900

    Add description to agency bar chart

--- components/AgencyBarChart.vue
+++ components/AgencyBarChart.vue
@@ -1,5 +1,10 @@
 <template>
   <data-view :title="title" :title-id="titleId" :date="date" :url="url">
+    <template v-slot:button>
+      <small :class="$style.DataViewDesc">
+        ※土・日・祝日を除く庁舎開庁日の1週間累計数
+      </small>
+    </template>
     <bar
       :chart-id="chartId"
       :chart-data="displayData"
@@ -8,6 +13,17 @@
     />
   </data-view>
 </template>
+
+<style module lang="scss">
+.DataView {
+  &Desc {
+    margin-top: 10px;
+    margin-bottom: 0 !important;
+    font-size: 12px;
+    color: $gray-3;
+  }
+}
+</style>
 
 <i18n>
 {
