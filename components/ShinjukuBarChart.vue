4f2d94c2f4ca8c798cd1eadac157edf72f691b41
shgtkshruch <shgtk.shruch@gmail.com>
2020-03-13 19:09:18 +0900

    Add initial graph of Shinjuku visitors

---
+++ components/ShinjukuBarChart.vue
@@ -0,0 +1,69 @@
+<template>
+  <data-view :title="title" :title-id="titleId" :date="date">
+    <template v-slot:button>
+      <p :class="$style.ShinjukuDesc" />
+    </template>
+    <bar
+      :chart-id="chartId"
+      :chart-data="displayData"
+      :options="chartOption"
+      :height="240"
+    />
+  </data-view>
+</template>
+
+<style module lang="scss">
+.Shinjuku {
+  &Desc {
+    margin-top: 10px;
+    margin-bottom: 0 !important;
+    font-size: 12px;
+    color: $gray-3;
+  }
+}
+</style>
+
+<script>
+import DataView from '@/components/DataView.vue'
+
+export default {
+  components: { DataView },
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    titleId: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    chartId: {
+      type: String,
+      required: false,
+      default: 'metro-bar-chart'
+    },
+    chartData: {
+      type: Object,
+      required: false,
+      default: () => {}
+    },
+    chartOption: {
+      type: Object,
+      required: false,
+      default: () => {}
+    },
+    date: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  },
+  computed: {
+    displayData() {
+      return {}
+    }
+  }
+}
+</script>
---
+++ components/cards/ShinjukuCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <shinjuku-bar-chart
+      title="新宿区の来訪者数の推移(実数値)"
+      :title-id="'shinjuku-visitors'"
+      :chart-id="'shinjuuku-visitors'"
+      :chart-data="ShinjukuData"
+      :chart-option="{}"
+      :date="ShinjukuData.date"
+    />
+  </v-col>
+</template>
+
+<script>
+import ShinjukuData from '@/data/shinjuku.json'
+import ShinjukuBarChart from '@/components/ShinjukuBarChart.vue'
+
+export default {
+  components: {
+    ShinjukuBarChart
+  },
+  data() {
+    return {
+      ShinjukuData
+    }
+  }
+}
+</script>
