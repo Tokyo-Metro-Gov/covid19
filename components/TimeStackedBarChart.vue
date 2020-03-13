b7b5e44ad4ffb56cb9b5389da3efc3ce1d2b4b26
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-03-13 23:36:33 +0900

    Update TimeStackedBarChart.vue

--- components/TimeStackedBarChart.vue
+++ components/TimeStackedBarChart.vue
@@ -69,6 +69,11 @@ export default {
       type: String,
       required: false,
       default: ''
+    },
+    url: {
+      type: String,
+      required: false,
+      default: ''
     }
   },
   data() {
