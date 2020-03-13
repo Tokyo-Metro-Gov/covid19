629476d41b563b11329e8138e1a75a1a2119cf42
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-03-13 23:31:04 +0900

    Update TimeStackedBarChart.vue

--- components/TimeStackedBarChart.vue
+++ components/TimeStackedBarChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <data-view :title="title" :title-id="titleId" :date="date">
+  <data-view :title="title" :title-id="titleId" :date="date" :url="url">
     <template v-slot:button>
       <p class="Graph-Desc">
         （注）同一の対象者について複数の検体を調査する場合あり
