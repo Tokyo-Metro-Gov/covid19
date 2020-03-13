89d25a866ea8ee3c536b292c72f69ef34e866fbd
Kazuki Imamura <kaizumaki@gmail.com>
2020-03-13 21:53:18 +0900

    lint

--- components/flow/FlowSpElder.vue
+++ components/flow/FlowSpElder.vue
@@ -63,7 +63,11 @@
       </i18n>
     </p>
 
-    <a v-scroll-to="'#consult'" href="#consult" :class="[$style.button, $style.clickable]">
+    <a
+      v-scroll-to="'#consult'"
+      href="#consult"
+      :class="[$style.button, $style.clickable]"
+    >
       <span :class="$style.text">{{ $t('新型コロナ受診相談窓口へ') }}</span>
       <ArrowForwardIcon :class="$style.icon" />
     </a>
