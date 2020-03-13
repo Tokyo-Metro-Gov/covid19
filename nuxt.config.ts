b47921f1893febc01705124e07bd039d19758d0a
osoken <osoken.devel@outlook.jp>
2020-03-14 06:02:11 +0900

    remove hardSource

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -215,7 +215,8 @@ const config: Configuration = {
       ]
     },
     // https://ja.nuxtjs.org/api/configuration-build/#hardsource
-    hardSource: process.env.NODE_ENV === 'development'
+    // hardSource: process.env.NODE_ENV === 'development'
+    hardSource: false
   },
   manifest: {
     name: '東京都 新型コロナウイルス感染症対策サイト',
