3c9bb0fca3dff9bbaa64e3e0ea880ac973cec306
shgtkshruch <shgtk.shruch@gmail.com>
2020-03-13 22:02:22 +0900

    Enable change graph mode

--- components/ShinjukuBarChart.vue
+++ components/ShinjukuBarChart.vue
@@ -6,6 +6,7 @@
         <br />
         習慣平均値（週毎の1日あたりの平均値）を算出
       </p>
+      <data-selector v-model="dataKind" />
     </template>
     <bar
       :chart-id="chartId"
@@ -19,6 +20,7 @@
 <style module lang="scss">
 .Shinjuku {
   &Desc {
+    width: 100%;
     margin-top: 10px;
     margin-bottom: 0 !important;
     font-size: 12px;
@@ -29,10 +31,11 @@
 
 <script>
 import dayjs from 'dayjs'
+import 'dayjs/locale/en'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import updateLocale from 'dayjs/plugin/updateLocale'
 import DataView from '@/components/DataView.vue'
-import 'dayjs/locale/en'
+import DataSelector from '@/components/DataTypeSelector.vue'
 
 dayjs.extend(updateLocale)
 dayjs.extend(weekOfYear)
@@ -41,7 +44,7 @@ dayjs.updateLocale('en', {
 })
 
 export default {
-  components: { DataView },
+  components: { DataView, DataSelector },
   props: {
     title: {
       type: String,
@@ -74,6 +77,11 @@ export default {
       default: ''
     }
   },
+  data() {
+    return {
+      dataKind: 'absolute'
+    }
+  },
   computed: {
     groupByWeekData() {
       return this.chartData.reduce((res, d) => {
@@ -82,8 +90,8 @@ export default {
         return res[weekNum].push(d) && res
       }, {})
     },
-    displayData() {
-      const labels = Object.keys(this.groupByWeekData).map(weekNum => {
+    labels() {
+      return Object.keys(this.groupByWeekData).map(weekNum => {
         const start = dayjs('2020-01-01')
           .week(weekNum)
           .startOf('week')
@@ -94,20 +102,37 @@ export default {
           .format('M/D')
         return `${start}~${end}`
       })
-      const data = Object.values(this.groupByWeekData).map(days => {
+    },
+    weeklyAvg() {
+      return Object.values(this.groupByWeekData).map(days => {
         const avg = days.reduce((sum, d) => (sum += d.value), 0) / days.length
         // [要確認] 小数点以下の処理
         return Math.floor(avg)
       })
-      return {
-        labels,
-        datasets: [
-          {
-            label: 'test',
-            data,
-            backgroundColor: '#008b41'
-          }
-        ]
+    },
+    displayData() {
+      if (this.dataKind === 'absolute') {
+        return {
+          labels: this.labels,
+          datasets: [
+            {
+              data: this.weeklyAvg,
+              backgroundColor: '#008b41'
+            }
+          ]
+        }
+      } else {
+        const mean = this.weeklyAvg[0]
+        const data = this.weeklyAvg.map(val => (val / mean) * 100 - 100)
+        return {
+          labels: this.labels,
+          datasets: [
+            {
+              data,
+              backgroundColor: '#008b41'
+            }
+          ]
+        }
       }
     }
   }
