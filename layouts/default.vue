2720bd55725961ff7b0c72d34e6682ed4b948bb3
Amon Sawamura <amon.sawamura@gmail.com>
2020-03-14 11:40:47 +0900

    Revert "feat: remove loader from print layout"
    This reverts commit 2aba6dd68e4e63bf89e81f2f7924a68ed870fefd.

--- layouts/default.vue
+++ layouts/default.vue
@@ -159,4 +159,16 @@ export default Vue.extend({
     }
   }
 }
+.loader {
+  height: 200px;
+  width: 150px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  img {
+    display: block;
+    margin: 0 auto 20px;
+  }
+}
 </style>
