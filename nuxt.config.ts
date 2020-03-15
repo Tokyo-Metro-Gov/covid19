bf04bcb8d37c537289b0a2e90c265e67c13c7780
Seitaro Hirakawa <hirakawa@code4chiba.org>
2020-03-15 16:30:31 +0900

    twitter_idをcode4chibaに更新
    tokyo_bousai→code4chiba

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,12 +56,12 @@ const config: Configuration = {
       {
         hid: 'twitter:site',
         name: 'twitter:site',
-        content: '@tokyo_bousai'
+        content: '@code4chiba'
       },
       {
         hid: 'twitter:creator',
         name: 'twitter:creator',
-        content: '@tokyo_bousai'
+        content: '@code4chiba'
       },
       {
         hid: 'twitter:image',
