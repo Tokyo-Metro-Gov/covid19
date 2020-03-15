2621e5995168e63a41a7d608f788609d9345e23e
potyama <kamikaze117117@gmail.com>
2020-03-15 20:35:51 +0900

    リンクを千葉に差し替え、その他に教育に関する情報を追記

--- pages/parent.vue
+++ pages/parent.vue
@@ -25,11 +25,12 @@ export default {
         {
           title: '2 感染症を疑う場合の対応',
           body:
-            '〇　風邪の症状や、37.5度以上の発熱が４日以上続いている、強いだるさ（倦怠感）、息苦しさ（呼吸困難）がある場合は、各保健所にご相談ください。<br />〇  「新型コロナウイルス感染症にかかる相談窓口について」（東京都福祉保健局）<br /><a href="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html" target="_blank" rel="noopener">https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html</a>'
+            '〇　風邪の症状や、37.5度以上の発熱が４日以上続いている、強いだるさ（倦怠感）、息苦しさ（呼吸困難）がある場合は、各保健所にご相談ください。<br />〇  「電話相談窓口について｜新型コロナウイルス感染症」<br /><a href="https://www.pref.chiba.lg.jp/kenfuku/kansenshou/singata-koronauirusu-kannrennhaienn.html" target="_blank" rel="noopener">https://www.pref.chiba.lg.jp/kenfuku/kansenshou/singata-koronauirusu-kannrennhaienn.html</a>'
         },
         {
           title: '3 その他',
-          body: '〇  詳細は、各学校からのお知らせ等をご確認ください。'
+          body:
+            '〇  詳細は、各学校からのお知らせ等をご確認ください。<br />〇  「教育に関する情報｜新型コロナウイルス感染症」<br /><a href="https://www.pref.chiba.lg.jp/kouhou/homepage/2019/kyo-index.html" target="_blank" rel="noopener">https://www.pref.chiba.lg.jp/kouhou/homepage/2019/kyo-index.html</a>'
         }
       ]
     }
