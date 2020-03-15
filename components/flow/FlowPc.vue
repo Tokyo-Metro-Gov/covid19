9c69efcc8b7597fbe76b1a2edcb805a012846446
Takuma Hashimoto <takumaxd+github@gmail.com>
2020-03-15 10:43:52 +0900

    Edgeでカード幅が広くなってしまう対応
    外枠よりはみだしてしまうので、％指定を行なうようにする

--- components/flow/FlowPc.vue
+++ components/flow/FlowPc.vue
@@ -135,8 +135,8 @@ export default {
     margin-bottom: 36px;
   }
   &Lower {
-    grid-template-columns: repeat(2, 1fr);
-    -ms-grid-columns: 1fr 12px 1fr;
+    grid-template-columns: repeat(2, calc(50% - 6px));
+    -ms-grid-columns: calc(50% - 6px) 12px calc(50% - 6px);
     grid-template-rows: repeat(3, auto);
     -ms-grid-rows: auto 12px auto 12px auto;
     // HACK: IEでGridの順番がうまくいかない対応
