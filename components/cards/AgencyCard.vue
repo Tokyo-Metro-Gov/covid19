2411956aa31e04b26bba91369874ad795b6bad8e
Hiroki Maekawa <niche.and.such@gmail.com>
2020-03-15 17:07:09 +0900

    reviewdog対応
    文字列の括りを変更
    hyphenated対応

--- components/cards/AgencyCard.vue
+++ components/cards/AgencyCard.vue
@@ -10,7 +10,7 @@
       :unit="$t('人')"
     >
       <template v-slot:description>
-        ※{{ $t("土・日・祝日を除く庁舎開庁日の1週間累計数") }}
+        ※{{ $t('土・日・祝日を除く庁舎開庁日の1週間累計数') }}
       </template>
     </agency-bar-chart>
   </v-col>
