7dce0f00068e7df0f4f6bcba655a80b15a4b01ac
kazuyukiHATANKAKA <cavy@nifty.com>
2020-03-17 20:53:44 +0900

    電話番号画像のalt属性を変更
    alt="0570-550571"→alt="043-238-9966"

--- pages/flow.vue
+++ pages/flow.vue
@@ -78,7 +78,7 @@
           </div>
           <div class="TelLink">
             <a href="tel:043-238-9966"
-              ><img src="/flow/sp/sp_flow_tel_01@2x.png" alt="0570-550571"
+              ><img src="/flow/sp/sp_flow_tel_01@2x.png" alt="043-238-9966"
             /></a>
           </div>
           <div class="mt-4">
