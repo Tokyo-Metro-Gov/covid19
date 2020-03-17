040822dfa04241c3a8b647e46094211f79f8e09d
nishidayoshikatsu <nsd24technology@gmail.com>
2020-03-17 21:38:08 +0900

    「検査陽性者の状況」部分のグラフを削除

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,7 @@ import Data from '@/data/data.json'
 import formatGraph from '@/utils/formatGraph'
 // import formatTable from '@/utils/formatTable'
 import News from '@/data/news.json'
-import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
+// import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
 import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
@@ -51,7 +51,7 @@ export default {
     PageHeader,
     // WhatsNew,
     StaticInfo,
-    ConfirmedCasesDetailsCard,
+    // ConfirmedCasesDetailsCard,
     TestedNumberCard,
     ConfirmedCasesNumberCard,
     ConfirmedCasesAttributesCard
