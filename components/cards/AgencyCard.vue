983ee3021f2dc94fb766d64f8bdc7d2f02e0e6e8
Shu Fujita <s.fujita@nard.tech>
2020-03-19 01:28:11 +0900

    Refactor AgencyCard and AgencyBarChart (move description)

--- components/cards/AgencyCard.vue
+++ components/cards/AgencyCard.vue
@@ -6,7 +6,11 @@
       :chart-id="'agency'"
       :url="''"
       :unit="$t('人')"
-    />
+    >
+      <template v-slot:description>
+        {{ $t('※土・日・祝日を除く庁舎開庁日の1週間累計数') }}
+      </template>
+    </agency-bar-chart>
   </v-col>
 </template>
 
