45c7c2f233361f9491150f7d45aa60bc57f6f711
Shu Fujita <s.fujita@nard.tech>
2020-03-19 02:34:11 +0900

    Refactor AgencyCard and AgencyBarChart (change structure of components)

--- components/cards/AgencyCard.vue
+++ components/cards/AgencyCard.vue
@@ -4,6 +4,8 @@
       :title="$t('都庁来庁者数の推移')"
       :title-id="'agency'"
       :chart-id="'agency'"
+      :chart-data="agencyData"
+      :date="agencyData.date"
       :url="''"
       :unit="$t('人')"
     >
@@ -15,11 +17,26 @@
 </template>
 
 <script>
+import agencyData from '@/data/agency.json'
 import AgencyBarChart from '@/components/AgencyBarChart.vue'
 
 export default {
   components: {
     AgencyBarChart
+  },
+  data() {
+    const agencies = [
+      this.$t('第一庁舎計'),
+      this.$t('第二庁舎計'),
+      this.$t('議事堂計')
+    ]
+    agencyData.datasets.map(dataset => {
+      dataset.label = this.$t(dataset.label)
+    })
+    return {
+      agencyData,
+      agencies
+    }
   }
 }
 </script>
