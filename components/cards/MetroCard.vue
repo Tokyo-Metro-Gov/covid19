df8e1a9436872114a85d1c2df9c6073245b82805
Shu Fujita <s.fujita@nard.tech>
2020-03-19 02:31:01 +0900

    Refactor MetroCard and MetroBarChart (change structure of components)

--- components/cards/MetroCard.vue
+++ components/cards/MetroCard.vue
@@ -5,8 +5,11 @@
       :title-id="'predicted-number-of-toei-subway-passengers'"
       :chart-id="'metro-bar-chart'"
       :chart-data="metroGraph"
-      :chart-option="metroGraphOption"
       :date="metroGraph.date"
+      :tooltips-title="metroGraphTooltipTitle"
+      :tooltips-label="metroGraphTooltipLabel"
+      :url="''"
+      :unit="$t('%')"
     >
       <template v-slot:description>
         {{
@@ -58,60 +61,8 @@ export default {
     const data = {
       Data,
       metroGraph,
-      metroGraphOption: {
-        responsive: true,
-        legend: {
-          display: true,
-          onHover: e => {
-            e.currentTarget.style.cursor = 'pointer'
-          },
-          onLeave: e => {
-            e.currentTarget.style.cursor = 'default'
-          },
-          labels: {
-            boxWidth: 20
-          }
-        },
-        scales: {
-          xAxes: [
-            {
-              position: 'bottom',
-              stacked: false,
-              gridLines: {
-                display: true
-              },
-              ticks: {
-                fontSize: 10,
-                maxTicksLimit: 20,
-                fontColor: '#808080'
-              }
-            }
-          ],
-          yAxes: [
-            {
-              stacked: false,
-              gridLines: {
-                display: true
-              },
-              ticks: {
-                fontSize: 12,
-                maxTicksLimit: 10,
-                fontColor: '#808080',
-                callback(value) {
-                  return value.toFixed(2) + '%'
-                }
-              }
-            }
-          ]
-        },
-        tooltips: {
-          displayColors: false,
-          callbacks: {
-            title: metroGraphTooltipTitle,
-            label: metroGraphTooltipLabel
-          }
-        }
-      }
+      metroGraphTooltipTitle,
+      metroGraphTooltipLabel
     }
     return data
   }
