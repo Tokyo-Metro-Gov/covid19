fc1edeb80f90154cd8ab40db5b83d08170f8b666
kebhr <42484226+kebhr@users.noreply.github.com>
2020-03-18 17:02:54 +0900

    fix #1756 translate page title correctly

--- pages/about.vue
+++ pages/about.vue
@@ -228,9 +228,11 @@ export default Vue.extend({
   components: {
     StaticCard
   },
-  head: (): MetaInfo => ({
-    title: '当サイトについて'
-  })
+  head(): MetaInfo {
+    return {
+      title: this.$t('当サイトについて') as string
+    }
+  }
 })
 </script>
 
