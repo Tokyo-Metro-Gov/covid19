40168fbd3b37f70a87a95d49b0457634be297ec1
MaySoMusician <maysomusician@gmail.com>
2020-03-18 15:26:41 +0900

    Revert "Merge branch 'staging' into development"
    This reverts commit e1aa2bf570cb38458cee48c1613e9e69abd4eb93, reversing
    changes made to 5b52f76ee44e3d88c5c00470ad5964b81e8d7525.
    
    # Conflicts:
    #	README_TH.md
    #	assets/locales/th.json

--- layouts/default.vue
+++ layouts/default.vue
@@ -102,7 +102,7 @@ export default Vue.extend({
         },
         {
           hid: 'og:site_name',
-          name: 'og:site_name',
+          property: 'og:site_name',
           content:
             this.$t('東京都') +
             ' ' +
@@ -111,8 +111,18 @@ export default Vue.extend({
             this.$t('対策サイト')
         },
         {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `https://stopcovid19.metro.tokyo.lg.jp${this.$route.path}`
+        },
+        {
+          hid: 'og:locale',
+          property: 'og:locale',
+          content: this.$i18n.locale
+        },
+        {
           hid: 'og:title',
-          name: 'og:title',
+          property: 'og:title',
           content:
             this.$t('東京都') +
             ' ' +
@@ -122,14 +132,14 @@ export default Vue.extend({
         },
         {
           hid: 'og:description',
-          name: 'og:description',
+          property: 'og:description',
           content: this.$tc(
             '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
           )
         },
         {
           hid: 'og:image',
-          name: 'og:image',
+          property: 'og:image',
           content: this.$tc('ogp.og:image')
         },
         {
