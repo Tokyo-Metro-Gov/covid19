695ec4dbc4b25d2ea3ddf946b974122841eb6fc2
resessh <info@resessh.net>
2020-03-19 01:56:06 +0900

    hide card

--- pages/index.vue
+++ pages/index.vue
@@ -86,7 +86,7 @@
           :url="''"
         />
       </v-col> -->
-      <v-col cols="12" md="6" class="DataCard">
+      <!-- <v-col cols="12" md="6" class="DataCard">
         <time-bar-chart
           title="帰国者接触者センター相談件数"
           :title-id="'number-of-reports-to-covid19-consultation-desk'"
@@ -96,14 +96,14 @@
           :unit="'件'"
           :url="''"
         />
-      </v-col>
+      </v-col> -->
     </v-row>
   </div>
 </template>
 
 <script>
 import PageHeader from '@/components/PageHeader.vue'
-import TimeBarChart from '@/components/TimeBarChart.vue'
+// import TimeBarChart from '@/components/TimeBarChart.vue'
 import TimeStackedBarChart from '@/components/TimeStackedBarChart.vue'
 import WhatsNew from '@/components/WhatsNew.vue'
 import StaticInfo from '@/components/StaticInfo.vue'
@@ -119,7 +119,7 @@ import ConfirmedCasesTable from '@/components/ConfirmedCasesTable.vue'
 export default {
   components: {
     PageHeader,
-    TimeBarChart,
+    // TimeBarChart,
     TimeStackedBarChart,
     WhatsNew,
     StaticInfo,
@@ -136,10 +136,10 @@ export default {
     const dischargesGraph = formatGraph(Data.discharges_summary.data)
     // 退院者数
     const dischargesTable = formatTable(Data.discharges.data)
-    // 相談件数
-    const contactsGraph = formatGraph(Data.contacts.data)
-    // 帰国者・接触者電話相談センター相談件数
-    const querentsGraph = formatGraph(Data.querents.data)
+    // // 相談件数
+    // const contactsGraph = formatGraph(Data.contacts.data)
+    // // 帰国者・接触者電話相談センター相談件数
+    // const querentsGraph = formatGraph(Data.querents.data)
     // 都営地下鉄の利用者数の推移
     // 検査実施日別状況
     const inspectionsGraph = [
@@ -180,8 +180,8 @@ export default {
       patientsGraph,
       dischargesTable,
       dischargesGraph,
-      contactsGraph,
-      querentsGraph,
+      // contactsGraph,
+      // querentsGraph,
       inspectionsGraph,
       inspectionsItems,
       inspectionsLabels,
