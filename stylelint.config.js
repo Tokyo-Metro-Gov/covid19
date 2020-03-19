dee0664c3aff03e63997434d0ae139afd22bc02d
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-19 22:14:23 +0900

    update stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-scss'],
   rules: {}
 }
