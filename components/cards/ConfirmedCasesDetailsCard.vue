9d7a00240b3498f11596dc1f95f03104b1d8bf5d
Shu Fujita <s.fujita@nard.tech>
2020-03-19 18:54:28 +0900

    Refactor ConfirmedCasesDetailsCard and remove SvgCard

--- components/cards/ConfirmedCasesDetailsCard.vue
+++ components/cards/ConfirmedCasesDetailsCard.vue
@@ -1,27 +1,44 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
-    <svg-card
+    <data-view
+      :class="$style.SvgCard"
       :title="$t('検査陽性者の状況')"
       :title-id="'details-of-confirmed-cases'"
       :date="Data.inspections_summary.date"
     >
+      <template v-slot:infoPanel>
+        <p :class="$style.GraphDesc">
+          {{
+            $t(
+              '（注）都内において疑い例または患者の濃厚接触者として検査を行ったものについて掲載'
+            )
+          }}<br />
+          {{ $t('（チャーター機帰国者、クルーズ船乗客等は含まれていない）') }}
+          <br />
+          {{
+            $t(
+              '検査実施人数は、速報値として公開するものであり、後日確定データとして修正される場合があります'
+            )
+          }}
+        </p>
+      </template>
       <confirmed-cases-table
         :aria-label="$t('検査陽性者の状況')"
         v-bind="confirmedCases"
       />
-    </svg-card>
+    </data-view>
   </v-col>
 </template>
 
 <script>
 import Data from '@/data/data.json'
 import formatConfirmedCases from '@/utils/formatConfirmedCases'
-import SvgCard from '@/components/SvgCard.vue'
+import DataView from '@/components/DataView.vue'
 import ConfirmedCasesTable from '@/components/ConfirmedCasesTable.vue'
 
 export default {
   components: {
-    SvgCard,
+    DataView,
     ConfirmedCasesTable
   },
   data() {
@@ -36,3 +53,17 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" module>
+.SvgCard {
+  ::v-deep svg {
+    width: 100%;
+  }
+}
+.GraphDesc {
+  margin-top: 10px;
+  margin-bottom: 0;
+  font-size: 12px;
+  color: $gray-3;
+}
+</style>
