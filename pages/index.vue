2f475b319cff9a89f96399161244d27f421cac6a
westar7 <nishikawa@m-v.jp>
2020-03-19 16:15:24 +0900

    [Fix]index.vueの修正

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,8 @@
     <whats-new class="mb-4" :items="newsItems" />
     <static-info
       class="mb-4"
-      :url="localePath('/flow')"
+      url="https://www.pref.fukui.lg.jp/doc/kouho/navi.html"
+      target="_blank"
       :text="$t('自分や家族の症状に不安や心配があればまずは電話相談をどうぞ')"
       :btn-text="$t('相談の手順を見る')"
     />
