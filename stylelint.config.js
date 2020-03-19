006e5ebedda8b742a0d0896c182bf4725f0e8426
Munieru <20086673+munierujp@users.noreply.github.com>
2020-03-19 22:18:03 +0900

    fix at-rule-no-unknown rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: 'stylelint-config-standard',
   plugins: ['stylelint-scss'],
-  rules: {}
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true
+  }
 }
