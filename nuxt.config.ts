1963302827acad4898a337e692be108d311c6ee1
Matsui Masashi <matsui@kaffeine.jp>
2020-03-20 14:32:43 +0900

    新潟版は現在日本語版だけで言語の切り替え機能もないため、日本語以外のrouteを削除した

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -190,7 +190,7 @@ const config: Configuration = {
   generate: {
     fallback: true,
     routes() {
-      const locales = ['ja', 'en', 'zh-cn', 'zh-tw', 'ko', 'ja-basic']
+      const locales = ['ja']
       const pages = [
         '/cards/details-of-confirmed-cases',
         '/cards/number-of-confirmed-cases',
