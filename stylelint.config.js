88aa81d56bd526e5cd1c671912c305ba64ac8751
kowpk344 <62477074+kowpk344@users.noreply.github.com>
2020-03-22 01:37:08 +0900

    open date
    opendate

--- stylelint.config.js
+++ stylelint.config.js
@@ -6,3 +6,14 @@ module.exports = {
     'scss/at-rule-no-unknown': true
   }
 }
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-161519672-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-161519672-1');
+</script>
+
