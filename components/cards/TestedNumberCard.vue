280cb4b82de31afa144ce41940ba8c2bbba3b3f4
Daiki <nojiri1098@gmail.com>
2020-03-22 12:30:22 +0900

    Update component TestedNumberCard
    都内とその他の区別をしない (データがないので)

--- components/cards/TestedNumberCard.vue
+++ components/cards/TestedNumberCard.vue
@@ -1,48 +1,34 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
-    <time-stacked-bar-chart
+    <time-bar-chart
       :title="$t('検査実施数')"
-      :title-id="'number-of-tested'"
-      :chart-id="'time-stacked-bar-chart-inspections'"
+      :title-id="'number-of-tested-cases'"
+      :chart-id="'time-bar-chart-tested'"
       :chart-data="inspectionsGraph"
       :date="Data.inspections_summary.date"
-      :items="inspectionsItems"
-      :labels="inspectionsLabels"
       :unit="$t('件.tested')"
-      :data-labels="inspectionsDataLabels"
+      :url="'https://www.pref.mie.lg.jp/YAKUMUS/HP/m0068000071_00005.htm'"
     />
-    <!-- 件.tested = 検査数 -->
   </v-col>
 </template>
 
 <script>
 import Data from '@/data/data.json'
-import TimeStackedBarChart from '@/components/TimeStackedBarChart.vue'
+import TimeBarChart from '@/components/TimeBarChart.vue'
+import formatGraph from '@/utils/formatGraph'
 
 export default {
   components: {
-    TimeStackedBarChart
+    TimeBarChart
   },
   data() {
-    // 検査実施日別状況
-    const inspectionsGraph = [
-      Data.inspections_summary.data['都内'],
-      Data.inspections_summary.data['その他']
-    ]
-    const inspectionsItems = [
-      this.$t('都内発生（疑い例・接触者調査）'),
-      this.$t('その他（チャーター便・クルーズ船）')
-    ]
-    const inspectionsLabels = Data.inspections_summary.labels
-    const inspectionsDataLabels = [this.$t('都内'), this.$t('その他')]
+    const inspectionsGraph = formatGraph(Data.inspections_summary.data)
 
     const data = {
       Data,
       inspectionsGraph,
-      inspectionsItems,
-      inspectionsLabels,
-      inspectionsDataLabels
     }
+
     return data
   }
 }
