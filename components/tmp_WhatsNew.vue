6e02a69c86a9237a2c83816aa7f23829fec08ac0
freshcl <freshcl@users.noreply.github.com>
2020-03-22 16:34:00 +0900

    「最新のお知らせ」
    pages/index.vue
    components/WhatsNew.vue
    data/news.json
    変更

---
+++ components/tmp_WhatsNew.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="WhatsNew">
+    <h3 class="WhatsNew-heading">
+      <v-icon size="24" class="WhatsNew-heading-icon">
+        mdi-information
+      </v-icon>
+      {{ $t('最新のお知らせ') }}
+    </h3>
+    <ul class="WhatsNew-list">
+      <li v-for="(item, i) in items" :key="i" class="WhatsNew-list-item">
+        <a
+          class="WhatsNew-list-item-anchor"
+          :href="item.url"
+          target="_blank"
+          rel="noopener"
+        >
+          <time
+            class="WhatsNew-list-item-anchor-time px-2"
+            :datetime="formattedDate(item.date)"
+          >
+            {{ item.date }}
+          </time>
+          <span class="WhatsNew-list-item-anchor-link">
+            {{ item.text }}
+            <v-icon
+              v-if="!isInternalLink(item.url)"
+              class="WhatsNew-item-ExternalLinkIcon"
+              size="12"
+            >
+              mdi-open-in-new
+            </v-icon>
+          </span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { convertDateToISO8601Format } from '@/utils/formatDate'
+
+export default Vue.extend({
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    isInternalLink(path: string): boolean {
+      return !/^https?:\/\//.test(path)
+    },
+    formattedDate(dateString: string) {
+      return convertDateToISO8601Format(dateString)
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.WhatsNew {
+  @include card-container();
+
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.WhatsNew-heading {
+  display: flex;
+  align-items: center;
+
+  @include card-h2();
+
+  margin-bottom: 12px;
+  color: $gray-2;
+  margin-left: 12px;
+
+  &-icon {
+    margin: 3px;
+  }
+}
+
+.WhatsNew .WhatsNew-list {
+  padding-left: 0;
+  list-style-type: none;
+
+  &-item {
+    &-anchor {
+      display: inline-block;
+      text-decoration: none;
+      margin: 5px;
+      font-size: 14px;
+
+      @include lessThan($medium) {
+        display: flex;
+        flex-wrap: wrap;
+      }
+
+      &-time {
+        flex: 0 0 90px;
+
+        @include lessThan($medium) {
+          flex: 0 0 100%;
+        }
+
+        color: $gray-1;
+      }
+
+      &-link {
+        flex: 0 1 auto;
+
+        @include text-link();
+
+        @include lessThan($medium) {
+          padding-left: 8px;
+        }
+      }
+
+      &-ExternalLinkIcon {
+        margin-left: 2px;
+        color: $gray-3 !important;
+      }
+    }
+  }
+}
+</style>
