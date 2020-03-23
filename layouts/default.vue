29762be7a4f333044c954781a550ba3c79974c84
Naoki Yoshida <40266624+5LYqv7LTvu@users.noreply.github.com>
2020-03-24 02:24:04 +0900

    uddated

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app class="app">
     <v-overlay v-if="loading" color="#F8F9FA" opacity="1" z-index="9999">
       <div class="loader">
-        <h1>岩手県コロナウイルス感染症対策サイト（非公式）</h1>
+        <img src="/logo.svg" alt="東京都" />
         <scale-loader color="#00A040" />
       </div>
     </v-overlay>
@@ -87,36 +87,36 @@ export default Vue.extend({
       link: [
         {
           rel: 'canonical',
-          href: ``
+          href: `https://stopcovid19.metro.tokyo.lg.jp${this.$route.path}`
         }
       ],
       meta: [
         {
           hid: 'author',
           name: 'author',
-          content: this.$tc('岩手県')
+          content: this.$tc('東京都')
         },
         {
           hid: 'description',
           name: 'description',
           content: this.$tc(
-            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、岩手県の有志が開設したものです。'
+            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
           )
         },
         {
           hid: 'og:site_name',
           property: 'og:site_name',
           content:
-            this.$t('岩手県') +
+            this.$t('東京都') +
             ' ' +
             this.$t('新型コロナウイルス感染症') +
             ' ' +
-            this.$t('対策サイト非公式')
+            this.$t('対策サイト')
         },
         {
           hid: 'og:url',
           property: 'og:url',
-          content: ``
+          content: `https://stopcovid19.metro.tokyo.lg.jp${this.$route.path}`
         },
         {
           hid: 'og:locale',
@@ -127,7 +127,7 @@ export default Vue.extend({
           hid: 'og:title',
           property: 'og:title',
           content:
-            this.$t('岩手県') +
+            this.$t('東京都') +
             ' ' +
             this.$t('新型コロナウイルス感染症') +
             ' ' +
@@ -137,19 +137,19 @@ export default Vue.extend({
           hid: 'og:description',
           property: 'og:description',
           content: this.$tc(
-            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、岩手県の有志が開設したものです。'
+            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
           )
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          content: this.$tc('')
+          content: this.$tc('ogp.og:image')
         },
         {
           hid: 'apple-mobile-web-app-title',
           name: 'apple-mobile-web-app-title',
           content:
-            this.$t('岩手県') +
+            this.$t('東京都') +
             ' ' +
             this.$t('新型コロナウイルス感染症') +
             ' ' +
@@ -253,12 +253,11 @@ export default Vue.extend({
 
 .loader {
   height: 200px;
-  width: 50%;
+  width: 150px;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translateY(-50%) translateX(-50%);
-  color: rgba(0, 0, 0, 0.959);
 
   img {
     display: block;
