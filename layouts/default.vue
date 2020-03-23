1be086e72b1496cf1310257a091919e9247e4468
Shoma Okamoto <shoma.develop@gmail.com>
2020-03-23 20:10:00 +0900

    Add dev-prefix.

--- layouts/default.vue
+++ layouts/default.vue
@@ -87,7 +87,7 @@ export default Vue.extend({
       link: [
         {
           rel: 'canonical',
-          href: `https://covid19-oita.netlify.com${this.$route.path}`
+          href: `https://dev-covid19-oita.netlify.com${this.$route.path}`
         }
       ],
       meta: [
@@ -116,7 +116,7 @@ export default Vue.extend({
         {
           hid: 'og:url',
           property: 'og:url',
-          content: `https://covid19-oita.netlify.com${this.$route.path}`
+          content: `https://dev-covid19-oita.netlify.com${this.$route.path}`
         },
         {
           hid: 'og:locale',
