c046c71fe3173c545377019695c4142f4f929206
youyo <1003ni2@gmail.com>
2020-03-24 17:37:47 +0900

    有志で作ったよ

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -145,7 +145,7 @@ export default Vue.extend({
       return [
         {
           icon: 'mdi-chart-timeline-variant',
-          title: this.$t('県内の最新感染動向'),
+          title: this.$t('県内の最新感染動向【有志作成版】'),
           link: this.localePath('/')
         },
         // {
