3e8a2f4c3b9b534095e8625d42e22cd5303c510e
Daiki <nojiri1098@gmail.com>
2020-03-24 23:43:07 +0900

    Add tweet button

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,9 @@
       :title="headerItem.title"
       :date="headerItem.date"
     />
+    <div>
+      <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
     <v-row class="DataBlock">
       <confirmed-cases-number-card />
       <confirmed-cases-attributes-card />
