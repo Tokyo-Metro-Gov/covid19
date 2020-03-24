8ac077c907c065be5e53322cfce6d5f4dd16ec1b
youyo <1003ni2@gmail.com>
2020-03-24 14:57:28 +0900

    データが正確じゃないので一回外す

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
       <confirmed-cases-attributes-card />
       <confirmed-cases-number-card />
       <!-- <inspection-persons-number-card /> -->
-      <tested-number-card />
+      <!-- <tested-number-card /> -->
       <!-- <telephone-advisory-reports-number-card /> -->
       <consultation-desk-reports-number-card />
       <!-- <metro-card /> -->
@@ -48,10 +48,10 @@ import News from '@/data/news.json'
 // import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
-import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
+// import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
 // import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
 // import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
-import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
+// import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
 // import MetroCard from '@/components/cards/MetroCard.vue'
 // import AgencyCard from '@/components/cards/AgencyCard.vue'
 import { convertDatetimeToISO8601Format } from '@/utils/formatDate'
@@ -66,8 +66,8 @@ export default Vue.extend({
     // ConfirmedCasesDetailsCard,
     // TestedCasesDetailsCard,
     ConfirmedCasesNumberCard,
-    ConfirmedCasesAttributesCard,
-    TestedNumberCard,
+    ConfirmedCasesAttributesCard
+    // TestedNumberCard,
     // InspectionPersonsNumberCard,
     // TelephoneAdvisoryReportsNumberCard,
     // ConsultationDeskReportsNumberCard
