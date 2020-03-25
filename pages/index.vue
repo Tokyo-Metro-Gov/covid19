9f78e74038054707203a9c9343c63777e3f39530
Daiki Nojiri <nojiri1098@gmail.com>
2020-03-25 16:57:28 +0900

    Add facebook share button (#66)

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,16 @@
         data-show-count="false"
         >Tweet</a
       >
+      <iframe
+        src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Foita.stopcovid19.jp%2F&layout=button_count&size=small&appId=113749536134548&width=100&height=20"
+        width="100"
+        height="20"
+        style="border:none;overflow:hidden"
+        scrolling="no"
+        frameborder="0"
+        allowTransparency="true"
+        allow="encrypted-media"
+      />
       <script
         async
         src="https://platform.twitter.com/widgets.js"
