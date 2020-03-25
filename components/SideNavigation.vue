e1a6e72a0a3c8df60c57b8ff09a5868d741b9397
Daiki Nojiri <nojiri1098@gmail.com>
2020-03-25 17:19:16 +0900

    大分市及び報道機関へのリンクをはる (#65)
    * Add news sites
    
    * Fix translation of 大分合同新聞
    
    * Update a news link

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -131,12 +131,24 @@ export default Vue.extend({
           link: 'https://www.pref.oita.jp/site/bosaianzen/shingatacorona.html'
         },
         {
-          title: this.$t('当サイトについて'),
-          link: this.localePath('/about')
+          title: this.$t('大分県 新型コロナウイルスに関するお知らせ'),
+          link: 'http://www.pref.oita.jp/'
         },
         {
-          title: this.$t('大分県公式ホームページ'),
-          link: 'http://www.pref.oita.jp/'
+          title: this.$t('大分市 新型コロナウイルスに関連する情報'),
+          link: 'https://www.city.oita.oita.jp/o029/coronavirus.html'
+        },
+        {
+          title: this.$t('大分合同新聞'),
+          link: 'https://www.oita-press.co.jp/followup/Covid19'
+        },
+        {
+          title: this.$t('NHK NEWS 大分'),
+          link: 'https://www3.nhk.or.jp/lnews/oita/'
+        },
+        {
+          title: this.$t('当サイトについて'),
+          link: this.localePath('/about')
         }
       ]
     }
