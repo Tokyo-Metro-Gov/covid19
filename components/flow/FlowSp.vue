35c9631ccd8172c5eb8b4eb0d28d120011a4c96c
Yuta Sukekawa <yutasukekawa@thyme-field.com>
2020-03-27 23:25:30 +0900

    スマホ版の相談フローページに「帰国者、接触者センター（各保健所）一覧」を追加

--- components/flow/FlowSp.vue
+++ components/flow/FlowSp.vue
@@ -93,6 +93,172 @@
         </li>
       </ul>
     </div>
+
+    <h3 :class="['mb-3', $style.FlowCardHeading]">
+      {{ $t('帰国者、接触者センター（各保健所）一覧') }}
+    </h3>
+    <div :class="[$style.OuterUpper, $style.HealthCenterList]">
+      <ul :class="$style.fzRegular">
+        <li>
+          {{
+            $t(
+              '帰国者・接触者センターは、感染が疑われる方について、医療機関の紹介や連絡方法をお伝えし、受診調整を行います。'
+            )
+          }}
+        </li>
+        <li>
+          {{
+            $t(
+              '今後国内での感染を広げないためにも、上記のような症状がある場合には、医療機関を受診する前にまずは各保健所に設置している「帰国者・接触者相談センター」へご連絡ください。'
+            )
+          }}
+        </li>
+      </ul>
+
+      <div class="scroll-table-container">
+        <table :class="[$style.fzNormal, $style.HealthCenterListTable, 'mt-4']">
+          <thead>
+            <tr>
+              <th>{{ $t('保健所名') }}</th>
+              <th>{{ $t('電話番号') }}</th>
+              <th>{{ $t('管轄市区町村') }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{{ $t('東地方保健所') }}</td>
+              <td>
+                <a href="tel:0177395421" :class="$style.TelLink">
+                  017-739-5421
+                </a>
+              </td>
+              <td>{{ $t('平内町、今別町、蓬田村、外ヶ浜町') }}</td>
+            </tr>
+            <tr>
+              <td>{{ $t('弘前保健所') }}</td>
+              <td>
+                <a href="tel:0172338521" :class="$style.TelLink">
+                  0172-33-8521
+                </a>
+              </td>
+              <td>
+                {{
+                  $t(
+                    '弘前市、黒石市、平川市、西目屋村、藤原町、大鰐町、田舎館村、板柳町'
+                  )
+                }}
+              </td>
+            </tr>
+            <tr>
+              <td>{{ $t('三戸地方保健所') }}</td>
+              <td>
+                <a href="tel:0178275111" :class="$style.TelLink">
+                  0178-27-5111
+                </a>
+              </td>
+              <td>
+                {{
+                  $t(
+                    '三戸町、五戸町、田子町、南部町、階上町、新郷村、おいらせ町'
+                  )
+                }}
+              </td>
+            </tr>
+            <tr>
+              <td>{{ $t('五所川原保健所') }}</td>
+              <td>
+                <a href="tel:0173342108" :class="$style.TelLink">
+                  0173-34-2108
+                </a>
+              </td>
+              <td>
+                {{
+                  $t('五所川原市、つがる市、鯵ヶ沢町、深浦町、鶴田町、中泊町')
+                }}
+              </td>
+            </tr>
+            <tr>
+              <td>{{ $t('上十三保健所') }}</td>
+              <td>
+                <a href="tel:0176223510" :class="$style.TelLink">
+                  0176-22-3510
+                </a>
+              </td>
+              <td>
+                {{
+                  $t(
+                    '十和田市、三沢町、野辺地町、七戸町、六戸町、横浜町、東北町、六ヶ所村'
+                  )
+                }}
+              </td>
+            </tr>
+            <tr>
+              <td>{{ $t('むつ保健所') }}</td>
+              <td>
+                <a href="tel:0175311891" :class="$style.TelLink">
+                  0175-31-1891
+                </a>
+              </td>
+              <td>{{ $t('むつ市、大間町、東通村、風間浦村、佐井村') }}</td>
+            </tr>
+            <tr>
+              <td>{{ $t('青森市保健所') }}</td>
+              <td>
+                <a href="tel:0177655280" :class="$style.TelLink">
+                  017-765-5280
+                </a>
+              </td>
+              <td>
+                {{ $t('青森市') }}
+                <a
+                  href="http://www.city.aomori.aomori.jp/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  {{ $t('青森市ホームページへ') }}
+                  <v-icon
+                    role="img"
+                    aria-hidden="false"
+                    :aria-label="$t('別タブで開く')"
+                    class="MenuList-ExternalIcon"
+                    size="14"
+                  >
+                    mdi-open-in-new
+                  </v-icon>
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>{{ $t('八戸市保健所') }}</td>
+              <td>
+                <a href="tel:0178432291" :class="$style.TelLink">
+                  0178-43-2291
+                </a>
+              </td>
+              <td>
+                {{ $t('八戸市') }}
+                <a
+                  href="https://www.city.hachinohe.aomori.jp/"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  {{ $t('八戸市ホームページへ') }}
+                  <v-icon
+                    role="img"
+                    aria-hidden="false"
+                    :aria-label="$t('別タブで開く')"
+                    class="MenuList-ExternalIcon"
+                    size="14"
+                  >
+                    mdi-open-in-new
+                  </v-icon>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -164,10 +330,13 @@ export default {
   @include flowText($gray-1, $font-weight);
 }
 
-@mixin telLink {
-  margin-left: 0 !important;
-  text-decoration: none;
-  outline: 1px dotted #707070;
+@mixin spListStyle {
+  li {
+    margin-bottom: 1em;
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+  }
 }
 
 .Knowledge {
@@ -181,12 +350,7 @@ export default {
     padding: 0;
     text-indent: -1.15em;
 
-    li {
-      margin-bottom: 1em;
-      &:last-of-type {
-        margin-bottom: 0;
-      }
-    }
+    @include spListStyle;
 
     > li::before {
       content: '';
@@ -216,5 +380,46 @@ export default {
       margin-bottom: 0;
     }
   }
+}
+
+.HealthCenterList {
+  ul {
+    @include flowText($gray-1, normal);
+    @include spListStyle;
+  }
+
+  &Table {
+    border-collapse: collapse;
+    border-spacing: 0;
+    border: solid 1px #e5e5e5;
+
+    thead {
+      background-color: #efefef;
+
+      > tr > th {
+        @include tableCellStyle(bolder);
+      }
+    }
+
+    tbody > tr > td {
+      @include tableCellStyle(normal);
+
+      a {
+        margin-left: 1em;
+      }
+    }
+  }
+}
+
+.TelLink {
+  margin-left: 0 !important;
+  text-decoration: none;
+}
+</style>
+
+<style lang="scss" scoped>
+.scroll-table-container {
+  overflow: scroll;
+  white-space: nowrap;
 }
 </style>
