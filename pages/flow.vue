e198e925291f39ea08c99c72dceeb7b84d4c4ad7
macropygia <3162324+macropygia@users.noreply.github.com>
2020-03-28 18:33:36 +0900

    update visually hidden styles

--- pages/flow.vue
+++ pages/flow.vue
@@ -126,14 +126,7 @@ export default Vue.extend({
 
 @include largerThan($medium) {
   .only-sp {
-    clip: rect(1px, 1px, 1px, 1px);
-    clip-path: inset(50%);
-    height: 1px;
-    width: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
+    @include visually-hidden;
   }
 
   .only-pc {
