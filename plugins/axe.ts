9258d4f19bd9ddc31df27d41c64333a59372896b
Shunsuke Ito <shunsuke.ito@gmail.com>
2020-03-29 12:17:37 +0900

    開発サーバでaxeのチェックを無効にするコマンドを追加
    以下コマンドでaxeを無効にした開発サーバが立ち上がります。
    yarn dev-no-axe

--- plugins/axe.ts
+++ plugins/axe.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 
-if (process.env.NODE_ENV !== 'production') {
+const NODE_ENV = process.env.NODE_ENV
+const VUE_AXE = process.env.VUE_AXE
+
+if (NODE_ENV === 'development' && VUE_AXE === 'true') {
   const VueAxe = require('vue-axe')
   const AXE_LOCALE_JA = require('axe-core/locales/ja.json')
 
