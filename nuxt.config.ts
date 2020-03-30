b7d7c3c319dca612da271e395a477fbd32765735
Naoki Yoshida <40266624+5LYqv7LTvu@users.noreply.github.com>
2020-03-30 21:03:43 +0900

    chenged favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,7 @@ const config: Configuration = {
 
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', href: 'apple-touch-icon-precomposed.png' },
       {
         rel: 'stylesheet',
         href: 'https://use.fontawesome.com/releases/v5.6.1/css/all.css'
