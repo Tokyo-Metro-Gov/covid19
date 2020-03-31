6bd0b3c1dd1c41afa98ce2ba5289201da2ec5e12
sugigaku <gakuya124ftb@gmail.com>
2020-03-31 14:04:09 +0900

    Lineリンク削除

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -51,17 +51,6 @@
       <footer class="SideNavigation-Footer">
         <div class="SideNavigation-Social">
           <a
-            href="https://line.me/R/ti/p/%40822sysfc"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="SideNavigation-SocialLink"
-          >
-            <picture>
-              <source srcset="/line.webp" type="image/webp" />
-              <img src="/line.png" alt="LINE" />
-            </picture>
-          </a>
-          <a
             href="https://twitter.com/tokyo_bousai"
             target="_blank"
             rel="noopener noreferrer"
