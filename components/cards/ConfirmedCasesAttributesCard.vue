2f05ffdad160187ff7eaa2d092b07bca323db375
yutoogi <jacoyutorius@gmail.com>
2020-03-31 21:39:59 +0900

    refs #41 hand-typed latest data

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import Data from '@/data/data.json'
+import Data from '@/data/hamamatsu/data.json'
 import formatGraph from '@/utils/formatGraph'
 import formatTable from '@/utils/formatTable'
 import DataTable from '@/components/DataTable.vue'
