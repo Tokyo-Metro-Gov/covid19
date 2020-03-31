3d55cf0127969f703f8ed64bd31192261b847083
kazuyukiHATANKAKA <cavy@nifty.com>
2020-03-31 19:17:01 +0900

    陽性患者の属性一覧から退院欄を削除

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -4,8 +4,7 @@ const headers = [
   { text: '日付', value: '日付' },
   { text: '居住地', value: '居住地' },
   { text: '年代', value: '年代' },
-  { text: '性別', value: '性別' },
-  { text: '退院※', value: '退院', align: 'center' }
+  { text: '性別', value: '性別' }
 ]
 
 type DataType = {
@@ -13,7 +12,6 @@ type DataType = {
   居住地: string | null
   年代: string | null
   性別: '男性' | '女性'
-  退院: '◯' | null
   [key: string]: any
 }
 
@@ -22,7 +20,6 @@ type TableDataType = {
   居住地: DataType['居住地']
   年代: DataType['年代']
   性別: DataType['性別'] | '不明'
-  退院: DataType['退院']
 }
 
 type TableDateType = {
@@ -40,8 +37,7 @@ export default (data: DataType[]) => {
       日付: dayjs(d['リリース日']).format('MM/DD') ?? '不明',
       居住地: d['居住地'] ?? '不明',
       年代: d['年代'] ?? '不明',
-      性別: d['性別'] ?? '不明',
-      退院: d['退院']
+      性別: d['性別'] ?? '不明'
     }
     tableDate.datasets.push(TableRow)
   })
