20ca08fa9baff853da455fee911ebb98b4edc2ca
shgtkshruch <shgtk.shruch@gmail.com>
2020-04-01 20:45:38 +0900

    Cut down needless span tags

--- components/TI18n.vue
+++ components/TI18n.vue
@@ -1,13 +1,14 @@
 <template>
   <span v-if="locale === 'ja-basic'">
-    <span v-for="(t, i) in rubyText" :key="i">
-      <ruby v-if="t.kana">
+    <template v-for="(t, i) in rubyText">
+      <ruby v-if="t.kana" :key="i">
         {{ t.ja }}
-        <rp>(</rp><rt>{{ t.kana }}</rt
-        ><rp>)</rp>
+        <rp>(</rp>
+        <rt>{{ t.kana }}</rt>
+        <rp>)</rp>
       </ruby>
-      <template v-else>{{ t.ja }}</template>
-    </span>
+      <span v-else :key="i">{{ t.ja }}</span>
+    </template>
   </span>
   <span v-else>{{ text }}</span>
 </template>
@@ -48,19 +49,20 @@ const options: ThisTypedComponentOptionsWithRecordProps<
   },
   computed: {
     rubyText() {
-      let match: any
+      let match
+      let prevIndex = 0
       const res: object[] = []
       const regp = new RegExp(/(\p{sc=Han}+?)（(.+?)）/, 'gu')
-      let previndex = 0
       while ((match = regp.exec(this.text)) !== null) {
         if (match.index > 0) {
-          const ja = match.input.slice(previndex, match.index)
+          const ja = match.input.slice(prevIndex, match.index)
           res.push({ ja })
         }
         res.push({ ja: match[1], kana: match[2] })
-        previndex = match.index + match[0].length
+        prevIndex = match.index + match[0].length
       }
-      res.push({ ja: this.text.slice(previndex) })
+      if (prevIndex < this.text.length)
+        res.push({ ja: this.text.slice(prevIndex) })
       return res
     }
   },
