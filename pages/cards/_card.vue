b2ccd2cbc61ea5e72d400137c2b2c3f651971d24
Chris Wu <chris.wu@oist.jp>
2020-04-06 20:07:53 +0900

    Update translation of About the Website

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -161,7 +161,7 @@ export default {
         ? `${url}/ogp/${this.$route.params.card}.png?t=${timestamp}`
         : `${url}/ogp/${this.$i18n.locale}/${this.$route.params.card}.png?t=${timestamp}`
     const description = `${this.updatedAt} | ${this.$t(
-      '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
+      '当サイトは沖縄県の新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、有志の仲間が開設したものです。'
     )}`
 
     return {
