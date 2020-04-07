d4157d7623f87a9fbeca82fa7e6560331c1d1014
Shu Fujita <s.fujita@nard.tech>
2020-04-07 20:57:28 +0900

    Refactor Sass in WhatsNew component

--- components/WhatsNew.vue
+++ components/WhatsNew.vue
@@ -70,61 +70,61 @@ export default Vue.extend({
 
   padding: 10px;
   margin-bottom: 20px;
-}
 
-.WhatsNew-heading {
-  display: flex;
-  align-items: center;
+  &-heading {
+    display: flex;
+    align-items: center;
 
-  @include card-h2();
+    @include card-h2();
 
-  margin-bottom: 12px;
-  color: $gray-2;
-  margin-left: 12px;
+    margin-bottom: 12px;
+    color: $gray-2;
+    margin-left: 12px;
 
-  &-icon {
-    margin: 3px;
+    &-icon {
+      margin: 3px;
+    }
   }
-}
 
-.WhatsNew .WhatsNew-list {
-  padding-left: 0;
-  list-style-type: none;
+  .WhatsNew-list {
+    padding-left: 0;
+    list-style-type: none;
 
-  &-item {
-    &-anchor {
-      text-decoration: none;
-      margin: 5px;
-      font-size: 14px;
-
-      @include lessThan($medium) {
-        display: flex;
-        flex-wrap: wrap;
-      }
-
-      &-time {
-        flex: 0 0 90px;
+    &-item {
+      &-anchor {
+        text-decoration: none;
+        margin: 5px;
+        font-size: 14px;
 
         @include lessThan($medium) {
-          flex: 0 0 100%;
+          display: flex;
+          flex-wrap: wrap;
         }
 
-        color: $gray-1;
-      }
+        &-time {
+          flex: 0 0 90px;
 
-      &-link {
-        flex: 0 1 auto;
+          @include lessThan($medium) {
+            flex: 0 0 100%;
+          }
 
-        @include text-link();
-
-        @include lessThan($medium) {
-          padding-left: 8px;
+          color: $gray-1;
         }
-      }
 
-      &-ExternalLinkIcon {
-        margin-left: 2px;
-        color: $gray-3 !important;
+        &-link {
+          flex: 0 1 auto;
+
+          @include text-link();
+
+          @include lessThan($medium) {
+            padding-left: 8px;
+          }
+        }
+
+        &-ExternalLinkIcon {
+          margin-left: 2px;
+          color: $gray-3 !important;
+        }
       }
     }
   }
