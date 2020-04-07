40e75c69c90c2c805c5745c594bccdc05e21042b
kebhr <42484226+kebhr@users.noreply.github.com>
2020-04-08 05:47:42 +0900

    fix #2949 sort function evaluates '10歳未満' < '10代'

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -12,6 +12,7 @@
       :height="240"
       :fixed-header="true"
       :mobile-breakpoint="0"
+      :custom-sort="customSort"
       class="cardTable"
     />
     <div class="note">
@@ -129,6 +130,33 @@ export default Vue.extend({
     tables.forEach((table: HTMLElement) => {
       table.setAttribute('tabindex', '0')
     })
+  },
+  methods: {
+    // '10歳未満' < '10代' となるようにソートする
+    customSort: (items: Object[], index: string[], isDesc: boolean[]) => {
+      items.sort((a: any, b: any) => {
+        if (
+          index[0] === '年代' &&
+          (a[index[0]] === '10歳未満' || b[index[0]] === '10歳未満')
+        ) {
+          if (a[index[0]] === '10歳未満') {
+            // 公表日を常に降順にする
+            if (b[index[0]] === '10歳未満') {
+              return a['公表日'] > b['公表日'] ? -1 : 1
+            } else {
+              return !isDesc[0] ? -1 : 1
+            }
+          } else {
+            return !isDesc[0] ? 1 : -1
+          }
+        } else if (!isDesc[0]) {
+          return a[index[0]] < b[index[0]] ? -1 : 1
+        } else {
+          return b[index[0]] < a[index[0]] ? -1 : 1
+        }
+      })
+      return items
+    }
   }
 })
 </script>
