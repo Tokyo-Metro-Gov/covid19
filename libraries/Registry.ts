eb4fba2b885156946b004ad85d87752b06bd81bb
鈴木 光 <hikyaru.suzuki@gmail.com>
2020-04-08 15:43:09 +0900

    add: Instance Registry

---
+++ libraries/Registry.ts
@@ -0,0 +1,33 @@
+import {
+  AgencyRepository,
+  IAgencyRepository
+} from '~/libraries/repositories/AgencyRepository'
+import {
+  DataRepository,
+  IDataRepository
+} from '~/libraries/repositories/DataRepository'
+import {
+  IMetroRepository,
+  MetroRepository
+} from '~/libraries/repositories/MetroRepository'
+import {
+  INewsRepository,
+  NewsRepository
+} from '~/libraries/repositories/NewsRepository'
+import {
+  ChiyodaRepository,
+  IChiyodaRepository
+} from '~/libraries/repositories/ChiyodaRepository'
+import {
+  IShinjukuRepository,
+  ShinjukuRepository
+} from '~/libraries/repositories/ShinjukuRepository'
+
+export class Registry {
+  public static readonly AgencyRepository: IAgencyRepository = new AgencyRepository()
+  public static readonly DataRepository: IDataRepository = new DataRepository()
+  public static readonly MetroRepository: IMetroRepository = new MetroRepository()
+  public static readonly NewsRepository: INewsRepository = new NewsRepository()
+  public static readonly ChiyodaRepository: IChiyodaRepository = new ChiyodaRepository()
+  public static readonly ShinjukuRepository: IShinjukuRepository = new ShinjukuRepository()
+}
