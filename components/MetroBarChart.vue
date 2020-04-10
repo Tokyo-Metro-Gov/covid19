ec2396b7748efc4b46792e4cb13c7a41876680ed
shgtkshruch <shgtk.shruch@gmail.com>
2020-04-11 02:07:53 +0900

    Restore external link in metro bar chart

--- components/MetroBarChart.vue
+++ components/MetroBarChart.vue
@@ -30,6 +30,12 @@
         item-key="name"
       />
     </template>
+    <template v-slot:footer>
+      <external-link
+        :url="'https://smooth-biz.metro.tokyo.lg.jp/pdf/202004date3.pdf'"
+        :label="$t('鉄道利用者数の推移（新宿、東京、渋谷、各駅エリア）[PDF]')"
+      />
+    </template>
   </data-view>
 </template>
 
