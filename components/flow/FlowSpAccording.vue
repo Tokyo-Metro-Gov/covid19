e5d34aa232511fa9263d66a83805c015308ba17d
kzhrk <info@kzhrk.com>
2020-04-12 09:40:58 +0900

    単一名のコンポーネントタグを複数名のコンポーネントタグに変更

--- components/flow/FlowSpAccording.vue
+++ components/flow/FlowSpAccording.vue
@@ -67,7 +67,7 @@
           </i18n>
         </p>
         <div :class="$style.arrow" aria-hidden="true">
-          <arrow />
+          <arrow-downward-icon />
         </div>
       </a>
     </div>
@@ -111,7 +111,7 @@
           <span :class="$style.fzXLarge">{{ $t('陽性') }}</span>
         </p>
         <div :class="$style.arrow" aria-hidden="true">
-          <arrow />
+          <arrow-downward-icon />
         </div>
       </a>
     </div>
@@ -130,13 +130,13 @@
     <div :class="[$style.rectContainer, $style.double]">
       <div :class="[$style.rect, $style.solution]">
         <div :class="$style.icon" aria-hidden="true">
-          <house />
+          <house-icon />
         </div>
         <p>{{ $t('自宅で安静に過ごす') }}</p>
       </div>
       <div :class="[$style.rect, $style.solution]">
         <div :class="$style.icon" aria-hidden="true">
-          <apartment />
+          <apartment-icon />
         </div>
         <p>{{ $t('一般の医療機関を受診') }}</p>
       </div>
@@ -161,16 +161,16 @@
 </template>
 
 <script>
-import Apartment from '@/static/flow/responsive/apartment.svg'
-import House from '@/static/flow/responsive/house.svg'
-import Arrow from '@/static/flow/responsive/arrow_downward.svg'
+import ApartmentIcon from '@/static/flow/responsive/apartment.svg'
+import HouseIcon from '@/static/flow/responsive/house.svg'
+import ArrowDownwardIcon from '@/static/flow/responsive/arrow_downward.svg'
 import GreenArrow from '@/static/flow/responsive/arrow_green.svg'
 
 export default {
   components: {
-    Apartment,
-    House,
-    Arrow,
+    ApartmentIcon,
+    HouseIcon,
+    ArrowDownwardIcon,
     GreenArrow
   },
   computed: {
