6546afa0e684b03a3734904979a66c34766d7ed1
shgtkshruch <shgtk.shruch@gmail.com>
2020-04-15 18:27:00 +0900

    Improve type

--- utils/ruby.ts
+++ utils/ruby.ts
@@ -1,4 +1,6 @@
 import XRegExp from 'xregexp/'
+import Vue from 'vue'
+import { ChartTooltipModel } from 'chart.js'
 
 export type RubyText = {
   ja: string
@@ -24,11 +26,15 @@ export const createRubyObject = (text: string) => {
   return texts
 }
 
-export const customTooltip = (self: any, tooltipModel: any) => {
+interface TooltipModel extends ChartTooltipModel {
+  title?: string
+}
+
+export const customTooltip = (self: Vue, tooltipModel: TooltipModel) => {
   // Tooltip Element
   let tooltipEl = document.getElementById('chartjs-tooltip')
-  let titleEl = tooltipEl?.querySelector('.chartjs-tooltip-title')
-  let bodyEl = tooltipEl?.querySelector('.chartjs-tooltip-body')
+  let titleEl: HTMLElement | null
+  let bodyEl: HTMLElement | null
 
   // Create element on first render
   if (!tooltipEl) {
@@ -38,79 +44,61 @@ export const customTooltip = (self: any, tooltipModel: any) => {
   }
 
   if (tooltipModel.opacity === 0) {
-    tooltipEl.style.opacity = 0
+    tooltipEl.style.opacity = '0'
     return
   }
 
-  if (!titleEl) {
+  if ((titleEl = tooltipEl.querySelector('.chartjs-tooltip-title'))) {
+    titleEl!.textContent = ''
+  } else {
     titleEl = document.createElement('div')
     titleEl.classList.add('chartjs-tooltip-title')
     titleEl.style.fontFamily = tooltipModel._titleFontFamily
     titleEl.style.fontSize = tooltipModel.titleFontSize + 'px'
     titleEl.style.fontStyle = tooltipModel._titleFontStyle
     tooltipEl.appendChild(titleEl)
-  } else {
-    titleEl.textContent = ''
   }
 
-  if (!bodyEl) {
+  if ((bodyEl = tooltipEl.querySelector('.chartjs-tooltip-body'))) {
+    bodyEl!.textContent = ''
+  } else {
     bodyEl = document.createElement('div')
     bodyEl.classList.add('chartjs-tooltip-body')
     bodyEl.style.fontFamily = tooltipModel._bodyFontFamily
     bodyEl.style.fontSize = tooltipModel.bodyFontSize + 'px'
     bodyEl.style.fontStyle = tooltipModel._bodyFontStyle
-  } else {
-    bodyEl.textContent = ''
+    tooltipEl.appendChild(bodyEl)
   }
 
+  // titie
   const title = tooltipModel.title && tooltipModel.title[0]
-  if (!title) return
-  const t = createRubyObject(title as string)
 
-  t.forEach(a => {
-    let dom: HTMLElement
-    if (!a.kana) {
-      dom = document.createElement('span')
-      dom.textContent = a.ja
-    } else {
-      dom = document.createElement('ruby')
-      const rt = document.createElement('rt')
-      rt.textContent = a.kana || ''
-      dom.textContent = a.ja
-      dom.appendChild(rt)
-    }
-    titleEl.appendChild(dom)
-  })
+  if (title) {
+    createRubyObject(title).forEach(text => {
+      const dom = createDom(text)
+      titleEl!.appendChild(dom)
+    })
+  }
 
+  // body
   const bodyLines = tooltipModel.body.map(b => b.lines).join()
-  const b = createRubyObject(bodyLines as string)
 
-  b.forEach(a => {
-    let dom: HTMLElement
-    if (!a.kana) {
-      dom = document.createElement('span')
-      dom.textContent = a.ja
-    } else {
-      dom = document.createElement('ruby')
-      const rt = document.createElement('rt')
-      rt.textContent = a.kana || ''
-      dom.textContent = a.ja
-      dom.appendChild(rt)
-    }
-    bodyEl!.appendChild(dom)
-  })
-  tooltipEl.appendChild(bodyEl)
+  if (bodyLines) {
+    createRubyObject(bodyLines).forEach(text => {
+      const dom = createDom(text)
+      bodyEl!.appendChild(dom)
+    })
+  }
 
   const chart = self.$refs.barChart as Vue
   const el = chart.$el
   const canvas = el.querySelector('canvas')
-  const position = canvas.getBoundingClientRect()
+  const position = canvas!.getBoundingClientRect()
 
   tooltipEl.style.opacity = '1'
   tooltipEl.style.position = 'absolute'
   tooltipEl.style.left =
     position.left + window.pageXOffset + tooltipModel.caretX + 'px'
-
   tooltipEl.style.top =
     position.top + window.pageYOffset + tooltipModel.caretY + 'px'
   tooltipEl.style.color = tooltipModel.titleFontColor
@@ -119,4 +107,19 @@ export const customTooltip = (self: any, tooltipModel: any) => {
   tooltipEl.style.pointerEvents = 'none'
   tooltipEl.style.backgroundColor = tooltipModel.backgroundColor
   tooltipEl.style.borderRadius = tooltipModel.cornerRadius + 'px'
+}
+
+function createDom(text: RubyText) {
+  let dom: HTMLElement
+  if (!text.kana) {
+    dom = document.createElement('span')
+    dom.textContent = text.ja
+  } else {
+    dom = document.createElement('ruby')
+    const rt = document.createElement('rt')
+    rt.textContent = text.kana || ''
+    dom.textContent = text.ja
+    dom.appendChild(rt)
+  }
+  return dom
 }
