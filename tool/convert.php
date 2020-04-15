c863a5ed7f061f2ddaedd0eb193557ff5d09e01b
Chris Wu <chris.wu@oist.jp>
2020-04-15 22:54:45 +0900

    Map unknown patient data to "調査中"

--- tool/convert.php
+++ tool/convert.php
@@ -223,9 +223,9 @@ function readPatients() : array
         }
         $result = [
           "確定日" => $row['公表_年月日'],
-          "居住地" => $row['患者_居住地'],
-          "年代" => $row['患者_年代'],
-          "性別" => $row['患者_性別'],
+          "居住地" => !empty($row['患者_居住地']) ? $row['患者_居住地'] : "調査中",
+          "年代" => !empty($row['患者_年代']) ? $row['患者_年代'] : "調査中",
+          "性別" => !empty($row['患者_性別']) ? $row['患者_性別'] : "調査中",
           "退院" => $dischargeStatus,
           "備考" => $row['備考'],
           "date" => $carbon->format('Y-m-d'),
