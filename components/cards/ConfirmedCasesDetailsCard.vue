4f38935afdca15aa889eee388b0664155a33cda6
kebhr <42484226+kebhr@users.noreply.github.com>
2020-04-18 18:34:26 +0900

    update notes

--- components/cards/ConfirmedCasesDetailsCard.vue
+++ components/cards/ConfirmedCasesDetailsCard.vue
@@ -5,14 +5,26 @@
       :title-id="'details-of-confirmed-cases'"
       :date="Data.patients.date"
     >
-      <template v-slot:button>
-        <p :class="$style.note">
-          {{ $t('（注）チャーター機帰国者、クルーズ船乗客等は含まれていない') }}
-          <br />
-          {{
-            $t('（注）「入院中」には、入院調整中・宿泊療養に移行した方を含む')
-          }}
-        </p>
+      <template v-slot:description>
+        <ul>
+          <li>
+            {{
+              $t('（注）チャーター機帰国者、クルーズ船乗客等は含まれていない')
+            }}
+          </li>
+          <li>
+            {{
+              $t('（注）「入院中」には、入院調整中・宿泊療養に移行した方を含む')
+            }}
+          </li>
+          <li>
+            {{
+              $t(
+                '（注）退院者数の把握には一定の期間を要しており、確認次第数値を更新している'
+              )
+            }}
+          </li>
+        </ul>
       </template>
       <confirmed-cases-details-table
         :aria-label="$t('検査陽性者の状況')"
@@ -21,15 +33,6 @@
     </data-view>
   </v-col>
 </template>
-
-<style lang="scss" module>
-.note {
-  margin-top: 10px;
-  margin-bottom: 0;
-  font-size: 12px;
-  color: $gray-3;
-}
-</style>
 
 <script>
 import Data from '@/data/data.json'
