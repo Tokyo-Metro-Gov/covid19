76d20360cb6a67888168a58ccd0832d5b7ee68a2
Satoru Nakakubo <hinapa@gmail.com>
2020-04-19 23:31:23 +0900

    エラー修正
    L51,L53文法修正

--- pages/parent.vue
+++ pages/parent.vue
@@ -48,9 +48,7 @@ export default {
           title: `3. ${this.$t('お子様への読みきかせに')}`,
           body: [
             this.$t('学校がお休みになることや不要な外出を控えることの理由について、学校やご自宅での読みきかせにご利用ください。'),
-            <br />,
             this.$t('「だろう」じゃなくて「かもしれない」〜新型コロナウイルスのお話〜'),
-            <br />,
             '<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vS9VSQYMsRwQuHVkbp2PEH8OZNKesr4CpIxpg0iwuXmQJjlzWxvVfLGHYw-NL1s5bGBJVDSnZ5bzjcR/embed?start=false&loop=false&delayms=3000" frameborder="0" width="480" height="389" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>'
             this.$t('Author　Tsutomu Hamada'),
             '<a href="https://creativecommons.org/licenses/by/4.0/deed.ja" target="_blank" rel="noopener">CC-BY4.0</a>'
