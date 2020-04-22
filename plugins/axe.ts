b7c179fff618017bd41ed5dcdbbeeebd7c97a685
Takayuki Shimada <taka@tsmd.jp>
2020-04-22 15:03:54 +0900

    vue-axe で色コントラストの検査を行わないようにする

--- plugins/axe.ts
+++ plugins/axe.ts
@@ -9,7 +9,15 @@ if (NODE_ENV === 'development' && VUE_AXE === 'true') {
 
   Vue.use(VueAxe, {
     config: {
-      locale: AXE_LOCALE_JA
+      locale: AXE_LOCALE_JA,
+      rules: [
+        {
+          // 色コントラストの検査を無効化する。
+          // Node数の多い環境で検査コストが高いため。
+          id: 'color-contrast',
+          enabled: false
+        }
+      ]
     },
     clearConsoleOnUpdate: false
   })
