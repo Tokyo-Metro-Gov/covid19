0c94be98f10f0180ed5043d8cdd067082e496109
mayuritanaka <mayuri.tanaka@info-lounge.jp>
2020-04-24 15:00:37 +0900

    #2837 add copyright mark

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -107,7 +107,7 @@
           </a>
           {{ $t('の下に提供されています。') }}
           <br />
-          2020 Tokyo Metropolitan Government
+          ©︎ 2020 Tokyo Metropolitan Government
         </small>
       </footer>
     </div>
