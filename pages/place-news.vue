b3533668aea08f6420e8e519ee134d3d3376e47d
TaigaMikami <mikamimikami60@gmail.com>
2020-04-26 01:44:59 +0900

    add place_news.json

--- pages/place-news.vue
+++ pages/place-news.vue
@@ -3,24 +3,12 @@
     <page-header class="mb-3" :icon="headerItem.icon">
       {{ $t('県内各所のお知らせ') }}
     </page-header>
-    <StaticCard>
-      <h3>{{ $t('島根県立中央病院') }}</h3>
+    <StaticCard v-for="(item, i) in newsItems" :key="i">
+      <h3>{{ $t(item.place_name) }}</h3>
       <ul>
         <li>
-          <a href="https://www.spch.izumo.shimane.jp/new/2020/03/06165409.html">
-            {{ $t('【重要】新型コロナウイルス感染症対策について') }}
-          </a>
-        </li>
-      </ul>
-    </StaticCard>
-    <StaticCard>
-      <h3>{{ $t('島根大学') }}</h3>
-      <ul>
-        <li>
-          <a
-            href="https://www.shimane-u.ac.jp/introduction/crisis_management/index2.html?node_id=9045"
-          >
-            {{ $t('新型コロナウイルスに対する対応について') }}
+          <a href="item.url">
+            {{ $t(item.title) }}
           </a>
         </li>
       </ul>
@@ -38,6 +26,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
+import PlaceNews from '@/data/place_news.json'
 import StaticCard from '@/components/StaticCard.vue'
 import PageHeader from '@/components/PageHeader.vue'
 
@@ -59,6 +48,12 @@ export default Vue.extend({
   head(): MetaInfo {
     return {
       title: this.$t('県内各所のお知らせ') as string
+    }
+  },
+
+  computed: {
+    newsItems() {
+      return PlaceNews.placeNewsItems
     }
   }
 })
