5a9b2a7d053a76ae7f5212482c30fffaa0138cd5
Saeki Minami <photon.planula@gmail.com>
2020-04-26 02:17:15 +0900

    への警告回避

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -4,5 +4,6 @@ import { NuxtI18nSeo } from 'nuxt-i18n/types/nuxt-i18n'
 declare module 'vue/types/vue' {
   interface Vue {
     $nuxtI18nSeo(): NuxtI18nSeo
+    $style: { [key: string]: string }
   }
 }
