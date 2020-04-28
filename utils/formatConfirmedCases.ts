3925a76dcba07d6457f56f3497843cbe4af94ce8
hagume <haguehackme@gmail.com>
2020-04-28 21:02:15 +0900

    modefied format

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -9,16 +9,16 @@ type DataType = {
         {
           attr: '入院中'
           value: number
-          children: [
-            {
-              attr: '軽症・中等症'
-              value: number
-            },
-            {
-              attr: '重症'
-              value: number
-            }
-          ]
+          // children: [
+          //   {
+          //     attr: '軽症・中等症'
+          //     value: number
+          //   },
+          //   {
+          //     attr: '重症'
+          //     value: number
+          //   }
+          // ]
         },
         {
           attr: '退院'
@@ -37,8 +37,8 @@ type ConfirmedCasesType = {
   検査実施人数: number
   陽性物数: number
   入院中: number
-  軽症中等症: number
-  重症: number
+  // 軽症中等症: number
+  // 重症: number
   死亡: number
   退院: number
 }
@@ -53,8 +53,8 @@ export default (data: DataType) => {
     検査実施人数: data.value,
     陽性物数: data.children[0].value,
     入院中: data.children[0].children[0].value,
-    軽症中等症: data.children[0].children[0].children[0].value,
-    重症: data.children[0].children[0].children[1].value,
+    // 軽症中等症: data.children[0].children[0].children[0].value,
+    // 重症: data.children[0].children[0].children[1].value,
     死亡: data.children[0].children[2].value,
     退院: data.children[0].children[1].value
   }
