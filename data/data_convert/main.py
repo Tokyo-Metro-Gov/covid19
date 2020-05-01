de3ec7a56a36b960dfc17174cd41d1b791fd9d4b
hagume <62798392+hagume@users.noreply.github.com>
2020-05-02 01:22:06 +0900

    Update main.py
    退院日情報が 数字 or Nan 以外は除外(サイトに反映されない）

--- data/data_convert/main.py
+++ data/data_convert/main.py
@@ -62,7 +62,8 @@ df_kanja = pd.read_excel(link_kanja, index_col="番号", header=1, skipfooter=2)
 df_kanja["陽性判明日"] = df_kanja["陽性判明日"].apply(
     lambda date: pd.to_datetime(date, unit="D", origin=pd.Timestamp("1899/12/30"))
 )
-df_kanja["退院日"] = pd.to_numeric(df_kanja["退院日"], errors='coerce')
+# df_kanja["退院日"] = pd.to_numeric(df_kanja["退院日"], errors='coerce')
+df_kanja = df_kanja[df_kanja['退院日'].apply(lambda s: True if (type(s) == int or type(s) == float) else False)]
 df_kanja["退院日"] = df_kanja["退院日"].apply(
     lambda date: pd.to_datetime(date, unit="D", origin=pd.Timestamp("1899/12/30"))
 )
@@ -122,5 +123,5 @@ data["patients_summary"] = {
     "date": dt_update,
 }
 
-with open("./data/data.json", "w", encoding="utf-8") as fw:
+with open("../data.json", "w", encoding="utf-8") as fw:
     json.dump(data, fw, ignore_nan=True, ensure_ascii=False, indent=4)
