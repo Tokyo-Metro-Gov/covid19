e451ead2e0c92b832d9f4e17bdaeee32ed4fa422
hagume <haguehackme@gmail.com>
2020-05-02 00:25:28 +0900

    updated main.py

--- data/data_convert/main.py
+++ data/data_convert/main.py
@@ -59,13 +59,10 @@ link_kanja = urljoin(url, tag_kanja.get("href"))
 
 df_kanja = pd.read_excel(link_kanja, index_col="番号", header=1, skipfooter=2)
 
-#drop if not value or nan
-df_kanja = df_kanja[df_kanja['退院日'].apply(lambda s: True if (type(s) == int or type(s) == float) else False)] 
-
 df_kanja["陽性判明日"] = df_kanja["陽性判明日"].apply(
     lambda date: pd.to_datetime(date, unit="D", origin=pd.Timestamp("1899/12/30"))
 )
-
+df_kanja["退院日"] = pd.to_numeric(df_kanja["退院日"], errors='coerce')
 df_kanja["退院日"] = df_kanja["退院日"].apply(
     lambda date: pd.to_datetime(date, unit="D", origin=pd.Timestamp("1899/12/30"))
 )
@@ -125,5 +122,5 @@ data["patients_summary"] = {
     "date": dt_update,
 }
 
-with open("../data.json", "w", encoding="utf-8") as fw:
+with open("./data/data.json", "w", encoding="utf-8") as fw:
     json.dump(data, fw, ignore_nan=True, ensure_ascii=False, indent=4)
