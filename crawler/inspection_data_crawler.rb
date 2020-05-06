f943defa6f067fef77507afe0aa650c822c2d8ce
TaigaMikami <mikamimikami60@gmail.com>
2020-05-06 13:46:27 +0900

    陽性者数も更新する

--- crawler/inspection_data_crawler.rb
+++ crawler/inspection_data_crawler.rb
@@ -3,30 +3,61 @@ Bundler.require
 require 'open-uri'
 require 'json'
 
-json_file = "./data/inspection_persons.json"
+URL = 'https://www.pref.shimane.lg.jp/bousai_info/bousai/kikikanri/shingata_taisaku/new_coronavirus_portal.html'
+CHARSET = nil
+inspection_json_file = "./data/inspection_persons.json"
+patients_summary_json_file = "./data/patients_summary.json"
+
+def doc
+ Nokogiri::HTML(open(URL), nil, CHARSET)
+end
+
+def insperson
+  doc.css('table')[0].css('tbody')[0].css('th')[22].text.gsub('人', '')
+end
+
+def positive_insperson
+  doc.css('table')[0].css('tbody')[0].css('th')[23].text.gsub('人', '')
+end
+
+def html_date
+  date = doc.css('table')[0].css('tbody')[0].css('th')[21].text.gsub('日', '')
+  DateTime.new(Date.today.year, Date.today.month, date.to_i).iso8601
+end
 
 if __FILE__ == $0
-  url = 'https://www.pref.shimane.lg.jp/bousai_info/bousai/kikikanri/shingata_taisaku/new_coronavirus_portal.html'
-  charset = nil
-  doc = Nokogiri::HTML(open(url), nil, charset)
+  ENV['TZ'] = 'JP'
+  update_date_time = DateTime.parse(html_date)
 
-  insperson = doc.css('table')[0].css('tbody')[0].css('th')[22].text.gsub('人', '')
-  # positive = doc.css('table')[0].css('tbody')[0].css('th')[23].text.gsub('人', '')
-  date = doc.css('table')[0].css('tbody')[0].css('th')[21].text.gsub('日', '')
-  update_time =  DateTime.new(Date.today.year, Date.today.month, date.to_i).iso8601
-
-  json_data = open(json_file) do |f|
+  # inspection_persons.jsonの更新
+  json_data = open(inspection_json_file) do |f|
     JSON.load(f)
   end
 
-  json_date_time = DateTime.parse(json_data['inspection_persons']['labels'].last)
-  update_date_time = DateTime.parse(update_time)
+  inspection_json_date_time = DateTime.parse(json_data['inspection_persons']['labels'].last)
 
   # jsonファイルのlabelsとスクレイピング日付の比較
-  if (update_date_time.day != json_date_time.day && update_date_time > json_date_time)
-    json_data['inspection_persons']['labels'].push(update_time)
+  if (update_date_time.day != inspection_json_date_time.day && update_date_time > inspection_json_date_time)
+    json_data['inspection_persons']['date'] = (DateTime.now + Rational(9, 24)).strftime("%Y\/%m/%d %H:%M")
+    json_data['inspection_persons']['labels'].push(update_date_time)
     json_data['inspection_persons']['datasets'][0]['data'].push(insperson.to_i)
-    open(json_file, 'w') do |f|
+    open(inspection_json_file, 'w') do |f|
+      JSON.dump(json_data, f)
+    end
+  end
+
+  # patients_summary.jsonの更新
+  json_data = open(patients_summary_json_file) do |f|
+    JSON.load(f)
+  end
+
+  patients_summary_json_date_time = DateTime.parse(json_data['patients_summary']['data'].last["日付"])
+
+  if (update_date_time.day != patients_summary_json_date_time.day && update_date_time > patients_summary_json_date_time)
+    json_data['patients_summary']['date'] = (DateTime.now + Rational(9, 24)).strftime("%Y\/%m/%d %H:%M")
+    hash = {"日付": update_date_time, "小計": positive_insperson.to_i}
+    json_data['patients_summary']['data'].push(hash)
+    open(patients_summary_json_file, 'w') do |f|
       JSON.dump(json_data, f)
     end
   end
