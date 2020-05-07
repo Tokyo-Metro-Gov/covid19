f4dce7863233f0cfc5d5f52c6e68d391aafdef6d
Aqua-ix <souichi1997@gmail.com>
2020-05-07 17:19:16 +0000

    保健所分と民間分の積み上げ棒グラフに変更 #3957

--- components/TimeStackedBarChart.vue
+++ components/TimeStackedBarChart.vue
@@ -1,29 +1,6 @@
 <template>
   <data-view :title="title" :title-id="titleId" :date="date">
     <template v-slot:button>
-      <ul :class="$style.GraphDesc">
-        <li>
-          {{
-            $t(
-              '（注）日々の速報値（医療機関が保険適用で行った検査は含まない）は、毎日更新'
-            )
-          }}
-        </li>
-        <li>
-          {{
-            $t(
-              '（注）医療機関が保険適用で行った検査件数を含む検査実施件数は、毎週金曜日に前週木曜日から当該週水曜日までの日々の保険適用分の件数を反映して更新'
-            )
-          }}
-        </li>
-        <li>
-          {{
-            $t(
-              '（注）医療機関が保険適用で行った検査については、４月２９日分までを計上'
-            )
-          }}
-        </li>
-      </ul>
       <data-selector
         v-model="dataKind"
         :target-id="chartId"
@@ -81,6 +58,32 @@
         :width="chartWidth"
       />
     </div>
+    <template v-slot:dataTable>
+      <v-data-table
+        :headers="tableHeaders"
+        :items="tableData"
+        :items-per-page="-1"
+        :hide-default-footer="true"
+        :height="240"
+        :fixed-header="true"
+        :disable-sort="true"
+        :mobile-breakpoint="0"
+        class="cardTable"
+        item-key="name"
+      >
+        <template v-slot:body="{ items }">
+          <tbody>
+            <tr v-for="item in items" :key="item.text">
+              <th>{{ item.text }}</th>
+              <td class="text-end">{{ item['0'] }}</td>
+              <td class="text-end">{{ item['1'] }}</td>
+              <td class="text-end">{{ item['2'] }}</td>
+              <td class="text-end">{{ item['3'] }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-data-table>
+    </template>
     <slot name="additionalNotes" />
     <template v-slot:infoPanel>
       <data-view-basic-info-panel
@@ -338,7 +341,9 @@ const options: ThisTypedComponentOptionsWithRecordProps<
                 ].toLocaleString()
               }
 
-              label = `${cases} ${unit}`
+              label = `${
+                this.dataLabels[tooltipItem.datasetIndex!]
+              } : ${cases} ${unit}`
               if (this.dataKind === 'cumulative') {
                 label += ` (${this.$t('合計')}: ${casesTotal} ${unit})`
               }
