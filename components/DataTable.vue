5a491d22258e0979d7286a769d848a8f1a58b0c3
goki90210 <goki90210@gmail.com>
2020-05-07 16:02:52 +0900

    add scope="row"

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -18,7 +18,7 @@
       <template v-slot:body="{ items }">
         <tbody>
           <tr v-for="item in items" :key="item.text">
-            <th class="text-start">{{ item['公表日'] }}</th>
+            <th class="text-start" scope="row">{{ item['公表日'] }}</th>
             <td class="text-start">{{ item['居住地'] }}</td>
             <td class="text-start">{{ item['年代'] }}</td>
             <td class="text-start">{{ item['性別'] }}</td>
