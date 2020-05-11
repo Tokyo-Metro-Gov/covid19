ff2f69d0ad41a8861e0d4d1342bab49665c3b674
Nekoya3 <taiyu31@gmail.com>
2020-05-11 21:20:48 +0900

    Datasetの順番を変更

--- components/PositiveRate2MixedChart.vue
+++ components/PositiveRate2MixedChart.vue
@@ -261,6 +261,19 @@ const options: ThisTypedComponentOptionsWithRecordProps<
         labels: this.labels,
         datasets: [
           {
+            type: 'line',
+            yAxisID: 'y-axis-2',
+            label: this.items[2],
+            data: this.chartData[2],
+            pointBackgroundColor: '#cc7004',
+            pointBorderColor: '#CC7004',
+            borderColor: '#CC7004',
+            borderWidth: 3,
+            fill: false,
+            order: 2,
+            lineTension: 0
+          },
+          {
             type: 'bar',
             yAxisID: 'y-axis-1',
             label: this.items[0],
@@ -279,19 +292,6 @@ const options: ThisTypedComponentOptionsWithRecordProps<
             borderColor: graphSeries[1].strokeColor,
             borderWidth: 1,
             order: 1
-          },
-          {
-            type: 'line',
-            yAxisID: 'y-axis-2',
-            label: this.items[2],
-            data: this.chartData[2],
-            pointBackgroundColor: '#cc7004',
-            pointBorderColor: '#CC7004',
-            borderColor: '#CC7004',
-            borderWidth: 3,
-            fill: false,
-            order: 2,
-            lineTension: 0
           }
         ]
       }
