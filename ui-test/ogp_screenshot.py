d4e57065808e403545a91bef5bb290c3ca3e306c
MaySoMusician <maysomusician@gmail.com>
2020-05-21 17:51:19 +0900

    Add /cards/monitoring-status-overview to ogp screenshot settings

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -20,6 +20,7 @@ PATHS = {
     "/cards/number-of-inspection-persons": (959, 600),
     "/cards/positive-number-by-diagnosed-date":(959, 730),
     "/cards/positive-rate": (959, 730),
+    "/cards/monitoring-status-overview": (959, 570),
 }
 
 options = webdriver.ChromeOptions()
