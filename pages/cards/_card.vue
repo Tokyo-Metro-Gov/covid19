5b89e6ab535c297b19aec2438457beca8c6896b8
shgtkshruch <shgtk.shruch@gmail.com>
2020-05-25 17:34:09 +0900

    Fetch page title and updated at from each card component

--- pages/cards/_card.vue
+++ pages/cards/_card.vue
@@ -1,19 +1,8 @@
 <template>
-  <component :is="cardComponent" />
+  <component :is="cardComponent" ref="card" />
 </template>
 
 <script>
-import Data from '@/data/data.json'
-import MetroData from '@/data/metro.json'
-import agencyData from '@/data/agency.json'
-import patientData from '@/data/patient.json'
-import PositiveByDiagnosedData from '@/data/positive_by_diagnosed.json'
-import PositiveRate from '@/data/positive_rate.json'
-import DailyPositiveDetail from '@/data/daily_positive_detail.json'
-import UntrackedRate from '@/data/daily_positive_detail.json'
-import PositiveStatus from '@/data/positive_status.json'
-import positiveStatus from '@/data/positive_status.json'
-import monitoringStatusData from '@/data/monitoring_status.json'
 import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
 import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
@@ -62,103 +51,63 @@ export default {
     let title, updatedAt, cardComponent
     switch (this.$route.params.card) {
       case 'details-of-confirmed-cases':
-        title = this.$t('検査陽性者の状況')
-        updatedAt = Data.inspections_summary.date
         cardComponent = 'confirmed-cases-details-card'
         break
       case 'details-of-tested-cases':
-        title = this.$t('検査実施状況')
-        updatedAt = Data.inspection_status_summary.date
         cardComponent = 'tested-cases-details-card'
         break
       case 'number-of-confirmed-cases':
-        title = this.$t('陽性患者数')
-        updatedAt = Data.patients_summary.date
         cardComponent = 'confirmed-cases-number-card'
         break
       case 'number-of-confirmed-cases-by-municipalities':
-        title = this.$t('陽性患者数（区市町村別）')
-        updatedAt = patientData.date
         cardComponent = 'confirmed-cases-by-municipalities-card'
         break
       case 'attributes-of-confirmed-cases':
-        title = this.$t('陽性患者の属性')
-        updatedAt = Data.patients.date
         cardComponent = 'confirmed-cases-attributes-card'
         break
       case 'number-of-tested':
-        title = this.$t('検査実施件数')
-        updatedAt = Data.inspections_summary.date
         cardComponent = 'tested-number-card'
         break
       case 'number-of-inspection-persons':
-        title = this.$t('検査実施人数')
-        updatedAt = Data.inspection_persons.date
         cardComponent = 'inspection-persons-number-card'
         break
       case 'number-of-reports-to-covid19-telephone-advisory-center':
-        title = this.$t('新型コロナコールセンター相談件数')
-        updatedAt = Data.contacts.date
         cardComponent = 'telephone-advisory-reports-number-card'
         break
       case 'number-of-reports-to-covid19-consultation-desk':
-        title = this.$t('新型コロナ受診相談窓口相談件数')
-        updatedAt = Data.querents.date
         cardComponent = 'consultation-desk-reports-number-card'
         break
       case 'predicted-number-of-toei-subway-passengers':
-        title = this.$t('都営地下鉄の利用者数の推移')
-        updatedAt = MetroData.date
         cardComponent = 'metro-card'
         break
       case 'agency':
-        title = this.$t('都庁来庁者数の推移')
-        updatedAt = agencyData.date
         cardComponent = 'agency-card'
         break
       case 'positive-number-by-diagnosed-date':
-        title = this.$t('陽性患者数（検査結果判明日別）')
-        updatedAt = PositiveByDiagnosedData.date
         cardComponent = 'positive-number-by-diagnosed-date-card'
         break
       case 'positive-rate':
-        title = this.$t('検査実施人数（陰性確認を除く）と陽性率の推移')
-        updatedAt = PositiveRate.date
         cardComponent = 'positive-rate-card'
         break
       case 'monitoring-number-of-confirmed-cases':
-        title = this.$t('モニタリング指標(1)新規陽性者数')
-        updatedAt = DailyPositiveDetail.date
         cardComponent = 'monitoring-confirmed-cases-number-card'
         break
       case 'untracked-rate':
-        title = this.$t('モニタリング指標(2)新規陽性者における接触歴等不明率')
-        updatedAt = UntrackedRate.date
         cardComponent = 'untracked-rate-card'
         break
       case 'increase-ratio-of-confirmed-cases-by-daily':
-        title = this.$t('モニタリング指標(3)週単位の陽性者増加比')
-        updatedAt = DailyPositiveDetail.date
         cardComponent = 'confirmed-cases-increase-ratio-by-week-card'
         break
       case 'positive-status-severe-case':
-        title = this.$t('モニタリング指標(4)重症患者数')
-        updatedAt = PositiveStatus.date
         cardComponent = 'severe-case-card'
         break
       case 'number-of-hospitalized':
-        title = this.$t('モニタリング指標(5)入院患者数')
-        updatedAt = positiveStatus.date
         cardComponent = 'hospitalized-number-card'
         break
       case 'monitoring-number-of-reports-to-covid19-consultation-desk':
-        title = this.$t('（７）受診相談窓口における相談件数')
-        updatedAt = Data.querents.date
         cardComponent = 'monitoring-consultation-desk-reports-number-card'
         break
       case 'monitoring-status-overview':
-        title = this.$t('モニタリング指標')
-        updatedAt = monitoringStatusData.date
         cardComponent = 'monitoring-status-overview-card'
         break
     }
@@ -168,6 +117,11 @@ export default {
       title,
       updatedAt
     }
+  },
+  mounted() {
+    const cardContent = this.$refs.card.$children[0]
+    this.title = this.title || cardContent.title
+    this.updatedAt = this.updatedAt || cardContent.date
   },
   head() {
     const url = 'https://stopcovid19.metro.tokyo.lg.jp'
