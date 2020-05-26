80ad55f7e8acf2a12b11f62824ea74f1079db245
Takayuki Inadome <t.inadome1985.rd@gmail.com>
2020-05-26 17:35:04 +0900

    #4515「検査実施人数」グラフタイトル変更・「モニタリング指標」注釈修正

--- components/cards/InspectionPersonsNumberCard.vue
+++ components/cards/InspectionPersonsNumberCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
     <time-bar-chart
-      :title="$t('検査実施人数')"
+      :title="$t('検査実施人数（健康安全研究センターによる実施分）')"
       :title-id="'number-of-inspection-persons'"
       :chart-id="'number-of-inspection-persons'"
       :chart-data="graphData"
