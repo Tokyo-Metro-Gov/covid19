176e287cfe0237cdbc57351bafd14e91c17f850f
Souta Ito <soutaito@info-lounge.jp>
2020-06-02 10:48:59 +0900

    Revert "Merge branch 'master' into staging"
    This reverts commit de5b2d72a4d95b970df859afef7abbc28535e7cc, reversing
    changes made to ff8ccd0ec85d1b07a1a8696b3ca1a621763e8787.

--- components/MonitoringConfirmedCasesChart.vue
+++ components/MonitoringConfirmedCasesChart.vue
@@ -7,7 +7,15 @@
       <li v-for="(item, i) in dataLabels" :key="i" @click="onClickLegend(i)">
         <button>
           <div
-            v-if="i === 2"
+            v-if="i === 1"
+            :style="{
+              background: colors[i].fillColor,
+              border: 0,
+              height: '3px'
+            }"
+          />
+          <div
+            v-else-if="i === 2"
             :style="{
               background: `repeating-linear-gradient(90deg, ${colors[i].fillColor}, ${colors[i].fillColor} 2px, #fff 2px, #fff 4px)`,
               border: 0,
@@ -70,22 +78,11 @@
         />
       </div>
     </div>
-    <template v-slot:dataTable>
-      <v-data-table
-        :headers="tableHeaders"
-        :items="tableData"
-        :items-per-page="-1"
-        :hide-default-footer="true"
-        :height="240"
-        :fixed-header="true"
-        :disable-sort="true"
-        :mobile-breakpoint="0"
-        class="cardTable"
-        item-key="name"
-      />
-    </template>
     <template v-slot:additionalDescription>
       <slot name="additionalDescription" />
+    </template>
+    <template v-slot:dataTable>
+      <data-view-table :headers="tableHeaders" :items="tableData" />
     </template>
     <template v-slot:infoPanel>
       <data-view-basic-info-panel
@@ -107,6 +104,10 @@ import { TranslateResult } from 'vue-i18n'
 import { Chart } from 'chart.js'
 import dayjs from 'dayjs'
 import DataView from '@/components/DataView.vue'
+import DataViewTable, {
+  TableHeader,
+  TableItem
+} from '@/components/DataViewTable.vue'
 import OpenDataLink from '@/components/OpenDataLink.vue'
 import DataViewBasicInfoPanel from '@/components/DataViewBasicInfoPanel.vue'
 import { DisplayData, yAxesBgPlugin, scrollPlugin } from '@/plugins/vue-chart'
@@ -137,13 +138,8 @@ type Computed = {
   displayDataHeader: DisplayData
   displayOptionHeader: Chart.ChartOptions
   scaledTicksYAxisMax: number
-  tableHeaders: {
-    text: TranslateResult
-    value: string
-  }[]
-  tableData: {
-    [key: number]: number
-  }[]
+  tableHeaders: TableHeader[]
+  tableData: TableItem[]
 }
 
 type Props = {
@@ -170,7 +166,12 @@ const options: ThisTypedComponentOptionsWithRecordProps<
   created() {
     this.canvas = process.browser
   },
-  components: { DataView, DataViewBasicInfoPanel, OpenDataLink },
+  components: {
+    DataView,
+    DataViewTable,
+    DataViewBasicInfoPanel,
+    OpenDataLink
+  },
   props: {
     title: {
       type: String,
