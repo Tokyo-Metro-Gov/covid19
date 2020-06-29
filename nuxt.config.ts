a1efb3e5dd339ed2d132d99e4b5138752efdc502
goki90210 <goki90210@gmail.com>
2020-06-29 18:22:01 +0900

    require('webpack')の削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 import { Configuration } from '@nuxt/types'
 import { Configuration as WebpackConfiguration } from 'webpack'
 import i18n from './nuxt-i18n.config'
-require('webpack')
 const purgecss = require('@fullhuman/postcss-purgecss')
 const autoprefixer = require('autoprefixer')
 const environment = process.env.NODE_ENV || 'development'
