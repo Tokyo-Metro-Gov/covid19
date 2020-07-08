b7f0347b014225e4aeafeca46f814f0247cc79dd
MaySoMusician <maysomusician@gmail.com>
2020-07-08 15:25:38 +0900

    Use getNumberToFixedFunction to format values

--- utils/formatMonitoringItems.ts
+++ utils/formatMonitoringItems.ts
@@ -1,5 +1,7 @@
 // monitoring_items.json の型チェック用
 
+import { getNumberToFixedFunction } from '@/utils/monitoringItemsValueFormatters'
+
 type DataKey =
   | '(1)新規陽性者数'
   | '(2)#7119（東京消防庁救急相談センター）における発熱等相談件数 '
@@ -12,7 +14,6 @@ type DataKey =
   | '(6)入院患者確保病床数'
   | '(7)重症患者数'
   | '(7)重症患者確保病床数'
-type StringOrNumber = string | number
 
 type RawData = {
   '(1)新規陽性者数': number
@@ -27,13 +28,14 @@ type RawData = {
   '(7)重症患者数': number
   '(7)重症患者確保病床数': string
 }
+
 // -----------------------------------------
 // フォーマット済み モニタリング指標データ用
 
 export type MonitoringItems = Record<DataKey, MonitoringItemValue>
 
 interface MonitoringItemValue {
-  value: StringOrNumber
+  value: string
   unit: Unit | null // 元データに無いので独自に追加, 単位がない場合は null
 }
 
@@ -55,40 +57,76 @@ export default (rawDataObj: RawData): MonitoringItems => {
   }
   const unitPercentage: Unit = { text: '%', translatable: false }
 
+  const toInteger = getNumberToFixedFunction(0)
+  const toNumberIn10thPlace = getNumberToFixedFunction(1)
+
+  /**
+   * getNumberToFixedFunction の実装を変更せずに、
+   * 結果値が undefined だった場合に空文字に置き換える
+   * @param fn - getNumberToFixedFunction で得られる関数
+   * @param n - fn に渡す数値
+   */
+  const undefinedEraser = (
+    fn: (d: number) => string | undefined,
+    n: number
+  ) => {
+    const result = fn(n)
+    return typeof result === 'undefined' ? '' : result
+  }
   return {
     '(1)新規陽性者数': {
-      value: rawDataObj['(1)新規陽性者数'],
+      value: undefinedEraser(
+        toNumberIn10thPlace,
+        rawDataObj['(1)新規陽性者数']
+      ),
       unit: unitPerson
     },
     '(2)#7119（東京消防庁救急相談センター）における発熱等相談件数 ': {
-      value:
+      value: undefinedEraser(
+        toNumberIn10thPlace,
         rawDataObj[
           '(2)#7119（東京消防庁救急相談センター）における発熱等相談件数 '
-        ],
+        ]
+      ),
       unit: unitReports
     },
     '(3)新規陽性者における接触歴等不明者（人数）': {
-      value: rawDataObj['(3)新規陽性者における接触歴等不明者（人数）'],
+      value: undefinedEraser(
+        toNumberIn10thPlace,
+        rawDataObj['(3)新規陽性者における接触歴等不明者（人数）']
+      ),
       unit: unitPerson
     },
     '(3)新規陽性者における接触歴等不明者（増加比）': {
-      value: rawDataObj['(3)新規陽性者における接触歴等不明者（増加比）'],
+      value: undefinedEraser(
+        toNumberIn10thPlace,
+        rawDataObj['(3)新規陽性者における接触歴等不明者（増加比）']
+      ),
       unit: unitPercentage
     },
     '(4)PCR・抗原検査（検査人数）': {
-      value: rawDataObj['(4)PCR・抗原検査（検査人数）'],
+      value: undefinedEraser(
+        toNumberIn10thPlace,
+        rawDataObj['(4)PCR・抗原検査（検査人数）']
+      ),
       unit: unitPerson
     },
     '(4)PCR・抗原検査（陽性率）': {
-      value: rawDataObj['(4)PCR・抗原検査（陽性率）'],
+      value: undefinedEraser(
+        toNumberIn10thPlace,
+        rawDataObj['(4)PCR・抗原検査（陽性率）']
+      ),
       unit: unitPercentage
     },
     '(5)救急医療の東京ルールの適用件数': {
-      value: rawDataObj['(5)救急医療の東京ルールの適用件数'],
+      value: undefinedEraser(
+        toNumberIn10thPlace,
+        rawDataObj['(5)救急医療の東京ルールの適用件数']
+      ),
       unit: unitReports
     },
     '(6)入院患者数': {
-      value: rawDataObj['(6)入院患者数'],
+      value: undefinedEraser(toInteger, rawDataObj['(6)入院患者数']),
       unit: unitPerson
     },
     '(6)入院患者確保病床数': {
@@ -96,7 +134,7 @@ export default (rawDataObj: RawData): MonitoringItems => {
       unit: null
     },
     '(7)重症患者数': {
-      value: rawDataObj['(7)重症患者数'],
+      value: undefinedEraser(toInteger, rawDataObj['(7)重症患者数']),
       unit: unitPerson
     },
     '(7)重症患者確保病床数': {
