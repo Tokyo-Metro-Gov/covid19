04949303c4b9fb037d7776f02d81527aa086a13f
Shu Fujita <s.fujita@nard.tech>
2020-08-14 23:36:31 +0900

    Sort components in components/CardsMonitoring.vue in order of appearance

--- components/CardsMonitoring.vue
+++ components/CardsMonitoring.vue
@@ -28,30 +28,40 @@
 <script lang="ts">
 import Vue from 'vue'
 import CardRow from '@/components/cards/CardRow.vue'
+// 検査陽性者の状況
 import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
+// 報告日別による陽性者数の推移
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
-import MonitoringConfirmedCasesNumberCard from '@/components/cards/MonitoringConfirmedCasesNumberCard.vue'
-import PositiveRateCard from '@/components/cards/PositiveRateCard.vue'
-import SevereCaseCard from '@/components/cards/SevereCaseCard.vue'
-import UntrackedRateCard from '@/components/cards/UntrackedRateCard.vue'
-import HospitalizedNumberCard from '@/components/cards/HospitalizedNumberCard.vue'
+// モニタリング項目
 import MonitoringItemsOverviewCard from '@/components/cards/MonitoringItemsOverviewCard.vue'
+// モニタリング項目(1)新規陽性者数
+import MonitoringConfirmedCasesNumberCard from '@/components/cards/MonitoringConfirmedCasesNumberCard.vue'
+// モニタリング項目(2)#7119における発熱等相談件数
 import ConsultationAboutFeverNumberCard from '@/components/cards/ConsultationAboutFeverNumberCard.vue'
+// モニタリング項目(3)新規陽性者における接触歴等不明者数
+import UntrackedRateCard from '@/components/cards/UntrackedRateCard.vue'
+// モニタリング項目(4)検査の陽性率
+import PositiveRateCard from '@/components/cards/PositiveRateCard.vue'
+// モニタリング項目(5)救急医療の東京ルールの適用件数
 import TokyoRulesApplicationNumberCard from '@/components/cards/TokyoRulesApplicationNumberCard.vue'
+// モニタリング項目(6)入院患者数
+import HospitalizedNumberCard from '@/components/cards/HospitalizedNumberCard.vue'
+// モニタリング項目(7)重症患者数
+import SevereCaseCard from '@/components/cards/SevereCaseCard.vue'
 
 export default Vue.extend({
   components: {
     CardRow,
-    MonitoringConfirmedCasesNumberCard,
-    UntrackedRateCard,
-    SevereCaseCard,
-    PositiveRateCard,
     ConfirmedCasesDetailsCard,
     ConfirmedCasesNumberCard,
-    HospitalizedNumberCard,
-    ConsultationAboutFeverNumberCard,
-    TokyoRulesApplicationNumberCard,
     MonitoringItemsOverviewCard,
+    MonitoringConfirmedCasesNumberCard,
+    ConsultationAboutFeverNumberCard,
+    UntrackedRateCard,
+    PositiveRateCard,
+    TokyoRulesApplicationNumberCard,
+    HospitalizedNumberCard,
+    SevereCaseCard,
   },
 })
 </script>
