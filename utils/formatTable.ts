14ff7508d0e301d3f1665f392c7f7045d04a504b
Nekoya3 <taiyu31@gmail.com>
2020-08-17 01:35:52 +0900

    Change: attributes-of-confirmed-cases get from API

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -12,24 +12,21 @@ const headers: Header[] = [
   { text: '居住地', value: '居住地' },
   { text: '年代', value: '年代' },
   { text: '性別', value: '性別' },
-  { text: '退院※', value: '退院', align: 'center' },
 ]
 
 type DataType = {
-  リリース日: string
-  居住地: string | null
-  年代: string | null
-  性別: '男性' | '女性' | string
-  退院: '◯' | null
+  公表_年月日: string
+  都道府県名: string | null
+  患者_年代: string | null
+  患者_性別: '男性' | '女性' | string
   [key: string]: any
 }
 
 type TableDataType = {
   公表日: string
   居住地: DataType['居住地']
-  年代: DataType['年代']
+  年代: DataType['患者_年代']
   性別: DataType['性別'] | '不明'
-  退院: DataType['退院']
 }
 
 type TableDateType = {
@@ -45,14 +42,12 @@ type TableDateType = {
 export default function (data: DataType[]): TableDateType {
   const datasets = data
     .map((d) => ({
-      公表日: formatDateString(d['リリース日']) ?? '不明',
-      居住地: d['居住地'] ?? '調査中',
-      年代: d['年代'] ?? '不明',
-      性別: d['性別'] ?? '不明',
-      退院: d['退院'],
+      公表日: formatDateString(d['公表_年月日']) ?? '不明',
+      居住地: d['都道府県名'] ?? '調査中',
+      年代: d['患者_年代'] ?? '不明',
+      性別: d['患者_性別'] ?? '不明',
     }))
     .sort((a, b) => dayjs(a.公表日).unix() - dayjs(b.公表日).unix())
-    .reverse()
   return {
     headers,
     datasets,
