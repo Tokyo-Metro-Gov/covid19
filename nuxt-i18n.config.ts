896f0a6edec49fed542cfa3680edcec6a8e92840
Taiyu Yoshizawa <taiyu31@gmail.com>
2020-08-17 12:37:32 +0900

    nuxt-i18n.config.ts fix

--- nuxt-i18n.config.ts
+++ nuxt-i18n.config.ts
@@ -1,4 +1,4 @@
-import { NuxtVueI18n } from 'nuxt-i18n'
+import type { NuxtVueI18n } from 'nuxt-i18n'
 
 const dateTimeFormatsCommon = {
   dateTime: {
@@ -7,22 +7,22 @@ const dateTimeFormatsCommon = {
     day: 'numeric',
     hour: 'numeric',
     minute: 'numeric',
-    hour12: false
+    hour12: false,
   },
   date: {
     year: 'numeric',
     month: 'short',
-    day: 'numeric'
+    day: 'numeric',
   },
   dateWithoutYear: {
     month: 'long',
-    day: 'numeric'
+    day: 'numeric',
   },
   dateWithDayOfWeek: {
     weekday: 'short',
     month: 'short',
-    day: 'numeric'
-  }
+    day: 'numeric',
+  },
 }
 
 const options: NuxtVueI18n.Options.AllOptionsInterface = {
@@ -38,9 +38,9 @@ const options: NuxtVueI18n.Options.AllOptionsInterface = {
       'zh-cn': dateTimeFormatsCommon,
       'zh-tw': dateTimeFormatsCommon,
       ko: dateTimeFormatsCommon,
-      'ja-basic': dateTimeFormatsCommon
+      'ja-basic': dateTimeFormatsCommon,
     },
-    formatFallbackMessages: true
+    formatFallbackMessages: true,
   },
   // vueI18nLoader: true,
   lazy: true,
@@ -51,35 +51,35 @@ const options: NuxtVueI18n.Options.AllOptionsInterface = {
       name: '日本語',
       iso: 'ja-JP',
       file: 'ja.json',
-      description: 'Japanese'
+      description: 'Japanese',
     },
     {
       code: 'en',
       name: 'English',
       iso: 'en-US',
       file: 'en.json',
-      description: 'English'
+      description: 'English',
     },
     {
       code: 'zh-cn',
       name: '简体中文',
       iso: 'zh-CN',
       file: 'zh_CN.json',
-      description: 'Simplified Chinese'
+      description: 'Simplified Chinese',
     },
     {
       code: 'zh-tw',
       name: '繁體中文',
       iso: 'zh-TW',
       file: 'zh_TW.json',
-      description: 'Traditional Chinese'
+      description: 'Traditional Chinese',
     },
     {
       code: 'ko',
       name: '한국어',
       iso: 'ko-KR',
       file: 'ko.json',
-      description: 'Korean'
+      description: 'Korean',
     },
     // #1126, #872 (comment)
     // ポルトガル語は訳が揃っていないため非表示
@@ -95,9 +95,9 @@ const options: NuxtVueI18n.Options.AllOptionsInterface = {
       name: 'やさしい にほんご',
       iso: 'ja-JP',
       file: 'ja-Hira.json',
-      description: 'Easy Japanese'
-    }
-  ]
+      description: 'Easy Japanese',
+    },
+  ],
 }
 
 export default options
