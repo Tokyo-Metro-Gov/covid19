33773799d086b649de3e48f3ff32de2534727f68
maekawa <niche.and.such@gmail.com>
2020-08-20 08:59:57 +0900

    外部リンクを扱うExternalLinkと内部リンクを扱うNuxtLinkをパスから判断するAppLinkというコンポーネントを作成して利用する。 v-btnに直接リンクを指定しているPrinterButton.vueは使おうとするとデザインが崩れるのと、ローカルパスを_blankで指定するため、AppLinkは使わない。 要デザイン適用

---
+++ components/AppLink.vue
@@ -0,0 +1,86 @@
+<template>
+  <component
+    :is="linkTag(to)"
+    :to="to"
+    :show-icon="_showIcon"
+    :icon-type="iconType"
+    :icon-size="iconSize"
+    :icon-class="iconClass"
+  >
+    <slot />
+    <v-icon
+      v-if="showIcon"
+      class="LinkIcon"
+      :icon-class="iconClass"
+      :size="`${iconSize / 10}rem`"
+      :aria-label="this.$t('別タブで開く')"
+      role="img"
+      :aria-hidden="false"
+    >
+      {{ iconType }}
+    </v-icon>
+  </component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ExternalLink from '@/components/ExternalLink.vue'
+
+export default Vue.extend({
+  components: {
+    ExternalLink
+  },
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    external: {
+      type: Boolean,
+      default: null
+    },
+    showIcon: {
+      type: Boolean,
+      default: null
+    },
+    iconSize: {
+      type: Number,
+      default: 15
+    },
+    iconType: {
+      type: String,
+      default: 'mdi-open-in-new'
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    _showIcon(): boolean {
+      // 指定がない場合、外部なら表示、内部なら表示しない
+      if (this.showIcon == null) {
+        if (this.isExternal(this.to)) {
+          return true
+        } else {
+          return false
+        }
+      } else {
+        if (this.isExternal(this.to)) {
+          return false
+        }
+        return this.showIcon
+      }
+    }
+  },
+  methods: {
+    isExternal(path: string): boolean {
+      if (this.external) return true
+      return /^https?:\/\//.test(path)
+    },
+    linkTag(path: string): string {
+      return this.isExternal(path) ? 'external-link' : 'nuxt-link'
+    }
+  }
+})
+</script>
--- components/ExternalLink.vue
+++ components/ExternalLink.vue
@@ -1,14 +1,16 @@
 <template>
-  <a class="ExternalLink" :href="url" target="_blank" rel="noopener noreferrer">
+  <a class="ExternalLink" :href="to" target="_blank" rel="noopener noreferrer">
     <slot />
     <v-icon
+      v-if="showIcon"
       class="ExternalLinkIcon"
+      :class="iconClass"
       :size="`${iconSize / 10}rem`"
       :aria-label="this.$t('別タブで開く')"
       role="img"
       :aria-hidden="false"
     >
-      mdi-open-in-new
+      {{ iconType }}
     </v-icon>
   </a>
 </template>
@@ -26,14 +28,26 @@ import Vue from 'vue'
 
 export default Vue.extend({
   props: {
-    url: {
+    to: {
       type: String,
-      default: '',
+      default: ''
+    },
+    showIcon: {
+      type: Boolean,
+      default: true
     },
     iconSize: {
       type: Number,
-      default: 15,
+      default: 15
     },
-  },
+    iconType: {
+      type: String,
+      default: 'mdi-open-in-new'
+    },
+    iconClass: {
+      type: String,
+      default: ''
+    }
+  }
 })
 </script>
