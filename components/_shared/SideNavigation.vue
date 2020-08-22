0ccced445fe355985f4c6689e5f33bd73b38665f
Shu Fujita <s.fujita@nard.tech>
2020-08-22 16:07:08 +0900

    Move component MenuList from 'components' to 'components/_shared/SideNavigation'

--- components/_shared/SideNavigation.vue
+++ components/_shared/SideNavigation.vue
@@ -123,9 +123,9 @@
 import Vue from 'vue'
 import { TranslateResult } from 'vue-i18n'
 
+import MenuList from '@/components/_shared/SideNavigation/MenuList.vue'
 import AppLink from '@/components/AppLink.vue'
 import LanguageSelector from '@/components/LanguageSelector.vue'
-import MenuList from '@/components/MenuList.vue'
 
 type Item = {
   icon?: string
