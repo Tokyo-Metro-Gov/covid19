75880421d06be05cd610f9b25d5dc44e6fe4f259
Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>
2020-11-24 16:06:34 +0900

    Update MonitoringConsultationDeskReportsNumberCard.vue

--- components/cards/MonitoringConsultationDeskReportsNumberCard.vue
+++ components/cards/MonitoringConsultationDeskReportsNumberCard.vue
@@ -19,11 +19,6 @@
                 '「東京都発熱相談センター」の開設に伴い、「新型コロナ受診相談窓口」は終了したため、10月30日（金曜日）17時をもって本グラフの数値更新を終了しました。'
               )
             }}
-            <app-link
-              to="/cards/number-of-reports-to-tokyo-fever-consultation-center"
-            >
-              {{ $t('「東京都発熱相談センターにおける相談件数グラフ」はこちら。') }}
-            </app-link>
           </p>
         </template>
         <template v-slot:additionalDescription>
