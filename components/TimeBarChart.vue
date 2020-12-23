cd30e03c769889ed9853cd6bb038afcdee0828b2
Shu Fujita <s.fujita@nard.tech>
2020-12-24 00:16:22 +0900

    TimeBarChart:  getComplementedDate を使用しない

--- components/TimeBarChart.vue
+++ components/TimeBarChart.vue
@@ -77,7 +77,6 @@ import OpenDataLink from '@/components/OpenDataLink.vue'
 import ScrollableChart from '@/components/ScrollableChart.vue'
 import { DisplayData, yAxesBgPlugin } from '@/plugins/vue-chart'
 import { getGraphSeriesStyle } from '@/utils/colors'
-import { getComplementedDate } from '@/utils/formatDate'
 import { calcDayBeforeRatio } from '@/utils/formatDayBeforeRatio'
 import { GraphDataType } from '@/utils/formatGraph'
 
@@ -293,7 +292,7 @@ const options: ThisTypedComponentOptionsWithRecordProps<
             },
             title(tooltipItem, data) {
               const label = data.labels![tooltipItem[0].index!] as string
-              return self.$d(getComplementedDate(label), 'date')
+              return self.$d(new Date(label), 'date')
             },
           },
         },
