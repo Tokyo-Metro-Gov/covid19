d90289e9c2c6428f4368957733a7bd4322a4cf18
Shu Fujita <s.fujita@nard.tech>
2020-12-23 23:58:31 +0900

    MixedBarAndLineChart（救急医療の東京ルールの適用件数）:  getComplementedDate を使用しない

--- components/MixedBarAndLineChart.vue
+++ components/MixedBarAndLineChart.vue
@@ -102,7 +102,6 @@ import OpenDataLink from '@/components/OpenDataLink.vue'
 import ScrollableChart from '@/components/ScrollableChart.vue'
 import { DisplayData, yAxesBgPlugin } from '@/plugins/vue-chart'
 import { getGraphSeriesColor, SurfaceStyle } from '@/utils/colors'
-import { getComplementedDate } from '@/utils/formatDate'
 import { calcDayBeforeRatio } from '@/utils/formatDayBeforeRatio'
 import { getNumberToFixedFunction } from '@/utils/monitoringStatusValueFormatters'
 
@@ -323,7 +322,7 @@ const options: ThisTypedComponentOptionsWithRecordProps<
             },
             title(tooltipItem, data) {
               const label = data.labels![tooltipItem[0].index!] as string
-              return self.$d(getComplementedDate(label), 'date')
+              return self.$d(new Date(label), 'date')
             },
           },
         },
