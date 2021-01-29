77f508a3cf29f25dfd8416eda7dbfec23fbcec09
Murayu0225 <yu.murama2@gmail.com>
2021-01-29 13:25:26 +0900

    項目を追加

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -260,6 +260,11 @@ export default Vue.extend({
             'https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/kannsenyobouhandbook.html',
         },
         {
+          title: this.$t('新型コロナウイルス感染症自宅療養者向けハンドブック'),
+          link:
+            'https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/zitakuryouyouhandbook.html',
+        },
+        {
           title: this.$t('都民利用施設・都主催イベントに関する情報'),
           link:
             'https://www.seisakukikaku.metro.tokyo.lg.jp/information/event00.html',
