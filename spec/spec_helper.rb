7a07c7d2910d56c9ec5658a55866f53b6d3afa8d
suzuryo <suzuryo@gmail.com>
2021-02-01 22:47:21 +0900

    テストに失敗した時はスクリーンショットを保存する

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,3 +20,19 @@ end
 Capybara.default_driver = :emulated_chrome_ios
 Capybara.app_host = APP_HOST
 
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  # Save Screenshot for failed test cases
+  config.after do |example|
+    if example.exception
+      meta = example.metadata
+      spec_file_name = File.basename(meta[:file_path])
+      line_number = meta[:line_number]
+      screenshot_file_name = "#{Time.now.strftime('%Y%m%d%H%M%S%3N').to_i}-#{spec_file_name}-#{line_number}.png"
+      screenshot_path = "spec/screenshot/#{screenshot_file_name}"
+      page.save_screenshot screenshot_path
+    end
+  end
+end
+
