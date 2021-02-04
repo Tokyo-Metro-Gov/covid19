90ece954e6a2566a12f0bbf1784c257b166a5343
suzuryo <suzuryo@gmail.com>
2021-02-04 23:10:49 +0900

    Cardのid属性によるセレクタに変更
    つまり nth-child でセレクタを指定しないので
    ```
    CardsReference のカードの位置を変えた、つまり、<cards-lazy-row :rows="rows" />のrows[]の配列の順番が変わった
    ```
    という暗黙的なテスト項目はテスト対象要素から外れた

--- spec/feature/index_CardsMonitoring_h3_spec.rb
+++ spec/feature/index_CardsMonitoring_h3_spec.rb
@@ -13,16 +13,16 @@ describe 'page [/]', type: :feature do
 
         context 'CardsMonitoring' do
           it 'has h3' do
-            expect(find('#tab-0 > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, '検査陽性者の状況')
-            expect(find('#tab-0 > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.DataView-Header > h3').text).to eq t(lang, '報告日別による陽性者数の推移')
-            expect(find('#tab-0 > div > div:nth-child(2) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目')
-            expect(find('#tab-0 > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(1)')
-            expect(find('#tab-0 > div > div:nth-child(3) > div > div:nth-child(1) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(2)')
-            expect(find('#tab-0 > div > div:nth-child(3) > div > div:nth-child(2) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(3)')
-            expect(find('#tab-0 > div > div:nth-child(4) > div > div:nth-child(1) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(4)')
-            expect(find('#tab-0 > div > div:nth-child(4) > div > div:nth-child(2) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(5)')
-            expect(find('#tab-0 > div > div:nth-child(5) > div > div:nth-child(1) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(6)')
-            expect(find('#tab-0 > div > div:nth-child(5) > div > div:nth-child(2) > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(7)')
+            expect(find('#tab-0 #ConfirmedCasesDetailsCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '検査陽性者の状況')
+            expect(find('#tab-0 #ConfirmedCasesNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '報告日別による陽性者数の推移')
+            expect(find('#tab-0 #MonitoringItemsOverviewCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目')
+            expect(find('#tab-0 #MonitoringConfirmedCasesNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(1)')
+            expect(find('#tab-0 #ConsultationAboutFeverNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(2)')
+            expect(find('#tab-0 #UntrackedRateCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(3)')
+            expect(find('#tab-0 #PositiveRateCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(4)')
+            expect(find('#tab-0 #TokyoRulesApplicationNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(5)')
+            expect(find('#tab-0 #HospitalizedNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(6)')
+            expect(find('#tab-0 #SevereCaseCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(7)')
           end
         end
       end
--- spec/feature/index_CardsReference_h3_spec.rb
+++ spec/feature/index_CardsReference_h3_spec.rb
@@ -15,16 +15,16 @@ describe 'page [/]', type: :feature do
 
         context 'CardsReference' do
           it 'has h3' do
-            expect(find('#tab-1 > div > div:nth-child(1) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, '陽性者の属性')
-            expect(find('#tab-1 > div > div:nth-child(1) > div > div:nth-child(2) > div > div > div.DataView-Header > h3').text).to eq t(lang, '陽性者数（区市町村別）')
-            expect(find('#tab-1 > div > div:nth-child(2) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, '発症日別による陽性者数の推移')
-            expect(find('#tab-1 > div > div:nth-child(2) > div > div:nth-child(2) > div > div > div.DataView-Header > h3').text).to eq t(lang, '確定日別による陽性者数の推移')
-            expect(find('#tab-1 > div > div:nth-child(3) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, '検査実施件数')
-            expect(find('#tab-1 > div > div:nth-child(3) > div > div:nth-child(2) > div > div > div.DataView-Header > h3').text).to eq t(lang, '新型コロナコールセンター相談件数')
-            expect(find('#tab-1 > div > div:nth-child(4) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, '受診相談窓口における相談件数')
-            expect(find('#tab-1 > div > div:nth-child(4) > div > div:nth-child(2) > div > div > div.DataView-Header > h3').text).to eq t(lang, '東京都発熱相談センターにおける相談件数')
-            expect(find('#tab-1 > div > div:nth-child(5) > div > div:nth-child(1) > div > div > div.DataView-Header > h3').text).to eq t(lang, '都営地下鉄の利用者数の推移')
-            expect(find('#tab-1 > div > div:nth-child(5) > div > div:nth-child(2) > div > div > div.DataView-Header > h3').text).to eq t(lang, '都庁来庁者数の推移')
+            expect(find('#tab-1 #ConfirmedCasesAttributesCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '陽性者の属性')
+            expect(find('#tab-1 #ConfirmedCasesByMunicipalitiesCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '陽性者数（区市町村別）')
+            expect(find('#tab-1 #PositiveNumberByDevelopedDateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '発症日別による陽性者数の推移')
+            expect(find('#tab-1 #PositiveNumberByDiagnosedDateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '確定日別による陽性者数の推移')
+            expect(find('#tab-1 #TestedNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '検査実施件数')
+            expect(find('#tab-1 #TelephoneAdvisoryReportsNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '新型コロナコールセンター相談件数')
+            expect(find('#tab-1 #MonitoringConsultationDeskReportsNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '受診相談窓口における相談件数')
+            expect(find('#tab-1 #TokyoFeverConsultationCenterReportsNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '東京都発熱相談センターにおける相談件数')
+            expect(find('#tab-1 #MetroCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '都営地下鉄の利用者数の推移')
+            expect(find('#tab-1 #AgencyCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '都庁来庁者数の推移')
           end
         end
       end
