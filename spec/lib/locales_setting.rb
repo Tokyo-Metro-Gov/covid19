8ebefcd74d44bfc3155073fc3538d29bf58dcf1c
suzuryo <suzuryo@gmail.com>
2021-02-09 18:22:57 +0900

    テスト対象をjaとenのみにする

--- spec/lib/locales_setting.rb
+++ spec/lib/locales_setting.rb
@@ -12,26 +12,6 @@ LOCALES = {
     path: '/en/',
     locale: JSON.parse(File.read(File.join(__dir__, '../../assets/locales/en.json'))),
     logo: 'logo-en.png'
-  },
-  zh_cn: {
-    path: '/zh-cn/',
-    locale: JSON.parse(File.read(File.join(__dir__, '../../assets/locales/zh_CN.json'))),
-    logo: 'logo-zh-cn.png'
-  },
-  zh_tw: {
-    path: '/zh-tw/',
-    locale: JSON.parse(File.read(File.join(__dir__, '../../assets/locales/zh_TW.json'))),
-    logo: 'logo.svg'
-  },
-  ko: {
-    path: '/ko/',
-    locale: JSON.parse(File.read(File.join(__dir__, '../../assets/locales/ko.json'))),
-    logo: 'logo-ko.png'
-  },
-  ja_basic: {
-    path: '/ja-basic',
-    locale: JSON.parse(File.read(File.join(__dir__, '../../assets/locales/ja-Hira.json'))),
-    logo: 'logo.svg'
   }
 }.freeze
 
