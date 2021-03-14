a2202404dbd94bad4b4d3bd3830bb6bd7351229d
Shu Fujita <s.fujita@nard.tech>
2021-03-14 19:17:41 +0900

    tool/generateDataConverters.ts: 追加
    cf. a0a9e950acb7928386897375e5906154af752881
    https://github.com/tokyo-metropolitan-gov/covid19/pull/3008#pullrequestreview-391636626
    https://github.com/tokyo-metropolitan-gov/covid19/pull/3008#pullrequestreview-391639829

---
+++ tool/generateDataConverters.ts
@@ -0,0 +1,50 @@
+import { promises as fs } from 'fs'
+import { camelCase, upperFirst } from 'lodash'
+// eslint-disable-next-line import/order
+import * as path from 'path'
+import * as quicktype from 'quicktype'
+// TODO: rimraf は Node v12.0.0 でビルトインに追加されたので
+// Node.js のバージョンアップに合わせて除去する
+import { sync as rimrafSync } from 'rimraf'
+
+const basePath = path.dirname(__dirname)
+
+const inputPath = path.resolve(basePath, 'data')
+const outputPath = path.resolve(
+  basePath,
+  'libraries',
+  'auto_generated',
+  'data_converter'
+)
+
+// ディレクトリ utils/data_converter を削除する
+rimrafSync(outputPath)
+;(async () => {
+  // ディレクトリ utils/data_converter を作成する
+  await fs.mkdir(outputPath, { recursive: true })
+  // ディレクトリ data 内のファイル名の配列
+  const dataFileNames = await fs.readdir(inputPath)
+
+  for (let index = 0; index < dataFileNames.length; index++) {
+    const dataFileName = dataFileNames[index]
+    const dataFileBaseName = path.parse(dataFileName).name
+    const pascalFileName = upperFirst(camelCase(dataFileBaseName))
+
+    const converterFilePath = path.resolve(
+      outputPath,
+      `convert${pascalFileName}.ts`
+    )
+    const dataFilePath = path.resolve(inputPath, dataFileName)
+
+    await quicktype.main({
+      lang: 'ts',
+      topLevel: pascalFileName,
+      out: converterFilePath,
+      src: [dataFilePath],
+      rendererOptions: {
+        'nice-property-names': 'true',
+        'explicit-unions': 'true',
+      },
+    })
+  }
+})()
