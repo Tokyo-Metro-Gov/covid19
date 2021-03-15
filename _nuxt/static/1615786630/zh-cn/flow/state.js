4d16ab5c4e7bc47cda3b2f3222d7935a5482a14d
MasaGon <MasaGon@users.noreply.github.com>
2021-03-15 09:43:03 +0000

    deploy: 96c39d0e04335d13b2ef31d3bc29f426a2a3fd60

--- _nuxt/static/1615786630/zh-cn/flow/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615786630",layout:"default",error:a,serverRendered:false,routePath:"\u002Fzh-cn\u002Fflow",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
