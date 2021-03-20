c9d0e24becc63f2f43187c00fc73ef083b1f731b
Kazuki Imamura <kaizumaki@gmail.com>
2021-03-20 13:49:57 +0900

    MonitoringCommentCardのスタイルを書き直す

--- components/MonitoringCommentCard.vue
+++ components/MonitoringCommentCard.vue
@@ -1,51 +1,47 @@
 <template>
   <div class="MonitoringComment">
-    <div class="MonitoringComment-heading">
-      <h3 class="MonitoringComment-title">
-        {{ $t('感染状況・医療提供体制の分析') }}
-      </h3>
-    </div>
-    <div class="MonitoringComment-comments">
-      <v-row class="MonitoringComment-row">
-        <v-col class="MonitoringComment-col">
-          <v-col cols="12">
-            <p class="MonitoringComment-summary">
-              <span
-                v-for="(comment, i) in translatedMonitoringSummarizedComments"
-                :key="i"
-              >
-                {{ comment }}
-              </span>
-            </p>
-            <h4 class="MonitoringCommentFrame-title">{{ $t('感染状況') }}</h4>
-            <monitoring-comment-frame
-              :level="monitoringComment['総括コメント-感染状況'].level - 1"
-              :comment="commentMonitoring('総括コメント-感染状況')"
-            />
-          </v-col>
-          <v-col cols="12">
-            <h4 class="MonitoringCommentFrame-title">
-              {{ $t('医療提供体制') }}
-            </h4>
-            <monitoring-comment-frame
-              :level="monitoringComment['総括コメント-医療提供体制'].level - 1"
-              :comment="commentMonitoring('総括コメント-医療提供体制')"
-            />
-          </v-col>
-        </v-col>
-        <v-col class="MonitoringComment-col">
-          <div class="MonitoringComment-description">
-            <monitoring-comment-card-image-swipe />
-            <v-icon color="#D9D9D9">{{ mdiChevronRight }}</v-icon>
+    <v-row class="MonitoringComment-row">
+      <v-col class="pb-0">
+        <h3 class="MonitoringComment-title">
+          {{ $t('感染状況・医療提供体制の分析') }}
+        </h3>
+        <p class="MonitoringComment-summary">
+          <span
+            v-for="(comment, i) in translatedMonitoringSummarizedComments"
+            :key="i"
+          >
+            {{ comment }}
+          </span>
+        </p>
+        <h4 class="MonitoringCommentFrame-title">{{ $t('感染状況') }}</h4>
+        <monitoring-comment-frame
+          class="mb-4"
+          :level="monitoringComment['総括コメント-感染状況'].level - 1"
+          :comment="commentMonitoring('総括コメント-感染状況')"
+        />
+        <h4 class="MonitoringCommentFrame-title">
+          {{ $t('医療提供体制') }}
+        </h4>
+        <monitoring-comment-frame
+          :level="monitoringComment['総括コメント-医療提供体制'].level - 1"
+          :comment="commentMonitoring('総括コメント-医療提供体制')"
+        />
+      </v-col>
+    </v-row>
+    <v-row class="MonitoringComment-row">
+      <v-col class="pb-0">
+        <div class="MonitoringComment-description">
+          <div class="MonitoringComment-link">
             <app-link
               to="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/monitoring.html"
             >
               {{ $t('専門家による分析・総括コメントの詳細はこちら') }}
             </app-link>
           </div>
-        </v-col>
-      </v-row>
-    </div>
+          <monitoring-comment-card-image-swipe />
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -105,61 +101,55 @@ export default Vue.extend({
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .MonitoringComment {
   @include card-container();
 
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
   padding: 5px 12px 10px 12px;
   margin: 0 0 10px 0;
 
-  .MonitoringComment-heading {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
+  .MonitoringComment-title {
+    @include card-h2();
 
-    .MonitoringComment-title {
-      display: flex;
-      align-items: center;
-      padding: 8px 6px;
-      @include card-h2();
+    margin-bottom: 12px;
+  }
+
+  .MonitoringComment-row {
+    flex: 0 0 50%;
+    margin: 0;
+
+    @include lessThan($medium) {
+      flex-basis: 100%;
     }
   }
 
-  .MonitoringComment-comments {
-    margin: 0 10px 0 10px;
-    padding: 2px;
+  .MonitoringComment-summary {
+    @include font-size(12);
+  }
 
-    .MonitoringComment-row {
-      .MonitoringComment-col {
-        float: left;
-        clear: both;
-        padding: 3px;
-        min-width: 300px;
+  .MonitoringCommentFrame-title {
+    margin-bottom: 1px;
+    color: $gray-3;
+    font-weight: normal;
 
-        .MonitoringComment-summary {
-          @include font-size(12);
-        }
-
-        .MonitoringCommentFrame-title {
-          margin-bottom: 1px;
-          color: $gray-3;
-          font-weight: normal;
-
-          @include font-size(14);
-        }
-      }
-    }
+    @include font-size(14);
   }
 
   .MonitoringComment-description {
-    padding: 6px;
     text-align: center;
 
     @include font-size(14);
     > a {
       @include text-link();
     }
+  }
+
+  .MonitoringComment-link {
+    text-align: right;
+    margin-bottom: 16px;
   }
 }
 </style>
