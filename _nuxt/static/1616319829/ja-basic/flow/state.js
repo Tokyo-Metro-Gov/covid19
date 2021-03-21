0ab8ebe8f6739798f918a95f13cad25137fcd65c
MasaGon <MasaGon@users.noreply.github.com>
2021-03-21 09:44:10 +0000

    deploy: 61024d633d65ca265443ce5e32c54ee44ea0adc5

---
+++ _nuxt/static/1616319829/ja-basic/flow/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616319829",layout:"default",error:a,serverRendered:false,routePath:"\u002Fja-basic\u002Fflow",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
