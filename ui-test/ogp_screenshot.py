0e74564dd27441388f7626987187b240505b234e
Kazuki Imamura <kaizumaki@gmail.com>
2021-04-25 01:55:23 +0900

    variantをパーマリンク・OGP設定に追加

--- ui-test/ogp_screenshot.py
+++ ui-test/ogp_screenshot.py
@@ -29,6 +29,7 @@ PATHS = {
     "/cards/positive-number-by-developed-date": (959, 570),
     "/cards/number-of-reports-to-tokyo-fever-consultation-center": (959, 570),
     "/cards/deaths-by-death-date": (959, 570),
+    "/cards/variant": (959, 730),
 }
 
 options = webdriver.ChromeOptions()
