756be94b54d293526f2807472f2e627810c3faa4
Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>
2021-04-28 07:21:59 +0900

    Fix typo
    Co-authored-by: Kazuki Imamura <kaizumaki@gmail.com>

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -201,7 +201,7 @@ export default Vue.extend({
         },
         {
           svg: 'CovidIcon',
-          title: this.$t('コロナウイルス感染症で入院・宿泊療養される方へ'),
+          title: this.$t(' 新型コロナウイルス感染症で入院・宿泊療養される方へ'),
           link:
             'https://www.fukushihoken.metro.tokyo.lg.jp/oshirase/corona_0401.files/050101.pdf',
         },
