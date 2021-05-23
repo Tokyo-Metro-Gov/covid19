c987834d97a239159ba22fc083a1ff60549d6298
Shu Fujita <s.fujita@nard.tech>
2021-05-24 08:49:21 +0900

    Rename components/StaticCard.vue to components/_shared/StaticCard.vue

--- pages/worker.vue
+++ pages/worker.vue
@@ -74,7 +74,7 @@ import { MetaInfo } from 'vue-meta'
 
 import AppLink from '@/components/_shared/AppLink.vue'
 import PageHeader from '@/components/_shared/PageHeader.vue'
-import StaticCard from '@/components/StaticCard.vue'
+import StaticCard from '@/components/_shared/StaticCard.vue'
 
 export default Vue.extend({
   components: {
