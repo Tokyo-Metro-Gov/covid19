192f10c95c8acd4ba71818ab73ba3fa0ff2c68bc
Shu Fujita <s.fujita@nard.tech>
2021-05-31 18:35:39 +0900

    Add test comments

--- libraries/auto_generated/data_converter/convertMonitoringCommentImage.ts
+++ libraries/auto_generated/data_converter/convertMonitoringCommentImage.ts
@@ -7,6 +7,7 @@
 // These functions will throw an error if the JSON doesn't
 // match the expected interface, even if the JSON is valid.
 
+// test comment: MonitoringCommentImage
 export interface MonitoringCommentImage {
     date: string;
     data: Data;
--- libraries/auto_generated/data_converter/convertMonitoringItems.ts
+++ libraries/auto_generated/data_converter/convertMonitoringItems.ts
@@ -7,6 +7,7 @@
 // These functions will throw an error if the JSON doesn't
 // match the expected interface, even if the JSON is valid.
 
+// test comment: MonitoringItems
 export interface MonitoringItems {
     date: string;
     data: Data;
