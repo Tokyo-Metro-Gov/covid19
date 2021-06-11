c7ee2a33b8b0861a8e016a04a40450e8ce0f4a19
Shu Fujita <s.fujita@nard.tech>
2021-06-11 12:54:03 +0900

    Revert "Add test comments"
    This reverts commit 192f10c95c8acd4ba71818ab73ba3fa0ff2c68bc.

--- libraries/auto_generated/data_converter/convertVaccination.ts
+++ libraries/auto_generated/data_converter/convertVaccination.ts
@@ -7,7 +7,6 @@
 // These functions will throw an error if the JSON doesn't
 // match the expected interface, even if the JSON is valid.
 
-// test comment: Vaccination
 export interface Vaccination {
     date:     string;
     datasets: Dataset[];
