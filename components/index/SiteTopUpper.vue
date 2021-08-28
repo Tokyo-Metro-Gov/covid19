90f59fc45f24a172889c01e4e971e61b7b318dc9
Kazuki Imamura <kaizumaki@gmail.com>
2021-08-28 14:04:54 +0900

    ファーストビューの「感染状況・医療提供体制（サマリ）」を削除

--- components/index/SiteTopUpper.vue
+++ components/index/SiteTopUpper.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="SiteTopUpper">
     <whats-new :items="newsItems" />
-    <infection-medical-care-provision-status />
     <monitoring-comment-card />
     <div class="row mb-4">
       <staying-population />
@@ -14,7 +13,6 @@
 import Vue from 'vue'
 
 import Consultation from '@/components/index/SiteTopUpper/Consultation.vue'
-import InfectionMedicalCareProvisionStatus from '@/components/index/SiteTopUpper/InfectionMedicalCareProvisionStatus.vue'
 import MonitoringCommentCard from '@/components/index/SiteTopUpper/MonitoringComment/Card.vue'
 import StayingPopulation from '@/components/index/SiteTopUpper/StayingPopulation.vue'
 import WhatsNew from '@/components/index/SiteTopUpper/WhatsNew.vue'
@@ -34,7 +32,6 @@ type Props = {}
 export default Vue.extend<Data, Methods, Computed, Props>({
   components: {
     WhatsNew,
-    InfectionMedicalCareProvisionStatus,
     MonitoringCommentCard,
     Consultation,
     StayingPopulation,
