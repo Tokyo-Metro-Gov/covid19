3ff78419101309c982a15d2464bef3507a5c2d1d
Foo-x <www.sand.x@gmail.com>
2021-09-19 16:35:37 +0000

    fix: flicker on switching tabs
    Fixes #6683

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -8,13 +8,25 @@ export default function (to, from, savedPosition) {
     return { selector: to.hash }
   }
 
-  if (to.meta.tabs) {
-    const element = document.querySelector('[id^="cardTab"]')
-    if (element) {
-      const y = element.getBoundingClientRect().top + window.pageYOffset
-      return { x: 0, y }
-    }
+  if (!to.meta.tabs) {
+    return { x: 0, y: 0 }
   }
 
-  return { x: 0, y: 0 }
+  const element = document.querySelector('[id^="cardTab"]')
+  if (!element) {
+    return { x: 0, y: 0 }
+  }
+
+  const elementTop = element.getBoundingClientRect().top + window.pageYOffset
+
+  if (!window.$nuxt.context.$vuetify.breakpoint.xsOnly) {
+    return { x: 0, y: elementTop }
+  }
+
+  const naviContainer = document.querySelector('.naviContainer')
+  if (!naviContainer) {
+    return { x: 0, y: elementTop }
+  }
+
+  return { x: 0, y: elementTop - naviContainer.scrollHeight }
 }
