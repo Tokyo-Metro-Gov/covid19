8357f484a7a227b487a1fa7da9fc69fd5ede6e94
Foo-x <www.sand.x@gmail.com>
2021-09-20 04:13:01 +0000

    feat: not to scroll on switching tabs

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -8,25 +8,9 @@ export default function (to, from, savedPosition) {
     return { selector: to.hash }
   }
 
-  if (!to.meta.tabs) {
-    return { x: 0, y: 0 }
+  if (from.meta.tabs && to.meta.tabs) {
+    return
   }
 
-  const element = document.querySelector('[id^="cardTab"]')
-  if (!element) {
-    return { x: 0, y: 0 }
-  }
-
-  const elementTop = element.getBoundingClientRect().top + window.pageYOffset
-
-  if (!window.$nuxt.context.$vuetify.breakpoint.xsOnly) {
-    return { x: 0, y: elementTop }
-  }
-
-  const naviContainer = document.querySelector('.naviContainer')
-  if (!naviContainer) {
-    return { x: 0, y: elementTop }
-  }
-
-  return { x: 0, y: elementTop - naviContainer.scrollHeight }
+  return { x: 0, y: 0 }
 }
