bef01522cbabe3e48cb798a2e27db1faeaa4f95f
suzuryo <suzuryo@gmail.com>
2021-10-06 17:37:09 +0900

    rspec : CardMonitoring を 3タブ化 に対応

--- spec/feature/index_CardsMonitoring_h3_spec.rb
+++ spec/feature/index_CardsMonitoring_h3_spec.rb
@@ -7,22 +7,24 @@ describe 'page [/]', type: :feature do
     context "#{lang}" do
       describe 'h3' do
         before do
-          visit data[:path]
+          visit "#{data[:path]}monitoring"
           render_lazy_contents
         end
 
         context 'CardsMonitoring' do
+          it 'has cardTab' do
+            expect(find('.v-tabs a#cardTab-1.v-tab--active').text).to eq t(lang, 'モニタリング項目').upcase
+          end
+
           it 'has h3' do
-            expect(find('#tab-0 .DataCard.ConfirmedCasesDetailsCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '検査陽性者の状況')
-            expect(find('#tab-0 .DataCard.ConfirmedCasesNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '報告日別による陽性者数の推移')
-            expect(find('#tab-0 .DataCard.MonitoringItemsOverviewCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目')
-            expect(find('#tab-0 .DataCard.MonitoringConfirmedCasesNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(1)')
-            expect(find('#tab-0 .DataCard.ConsultationAboutFeverNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(2)')
-            expect(find('#tab-0 .DataCard.UntrackedRateCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(3)')
-            expect(find('#tab-0 .DataCard.PositiveRateCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(4)')
-            expect(find('#tab-0 .DataCard.TokyoRulesApplicationNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(5)')
-            expect(find('#tab-0 .DataCard.HospitalizedNumberCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(6)')
-            expect(find('#tab-0 .DataCard.SevereCaseCard > div > div > div.DataView-Header.with-dataSetPanel > h3').text).to eq t(lang, 'モニタリング項目(7)')
+            expect(find('.DataCard.MonitoringItemsOverviewCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目')
+            expect(find('.DataCard.MonitoringConfirmedCasesNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(1)')
+            expect(find('.DataCard.ConsultationAboutFeverNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(2)')
+            expect(find('.DataCard.UntrackedRateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(3)')
+            expect(find('.DataCard.PositiveRateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(4)')
+            expect(find('.DataCard.TokyoRulesApplicationNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(5)')
+            expect(find('.DataCard.HospitalizedNumberCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(6)')
+            expect(find('.DataCard.SevereCaseCard > div > div > div.DataView-Header > h3').text).to eq t(lang, 'モニタリング項目(7)')
           end
         end
       end
