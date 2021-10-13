e6fb2b701c7f654fb87809614e3e0f0f3c594e7b
Kazuki Imamura <kaizumaki@gmail.com>
2021-10-13 11:56:24 +0900

    stylelintのat-rule-empty-line-beforeを無効にする

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   plugins: ['stylelint-scss'],
   rules: {
+    'at-rule-empty-line-before': null,
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'selector-pseudo-element-no-unknown': [
