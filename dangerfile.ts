4bf057650f9445dd74c2b49f275007aea9b49f36
Kazuki Imamura <kaizumaki@gmail.com>
2021-11-09 23:16:24 +0900

    revert commit

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,13 +1,14 @@
-import Danger from 'danger'
+// eslint-disable-next-line import/named
+import { danger, warn } from 'danger'
 
-const modifiedConverterFilePaths = Danger.danger.git.modified_files.filter(
+const modifiedConverterFilePaths = danger.git.modified_files.filter(
   (filePath) => {
     return /^libraries\/auto_generated\/data_converter\/.*\.ts$/.test(filePath)
   }
 )
 
 modifiedConverterFilePaths.forEach((modifiedConverterFilePath) => {
-  Danger.warn(
+  warn(
     `\`${modifiedConverterFilePath}\` が変更されています。追加したコメントなどが削除されないよう注意してください。`
   )
 })
