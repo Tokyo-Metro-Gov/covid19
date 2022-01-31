3a5c848e0bb206036fac192c1e2a0cc9d8ccd986
Kazuki Imamura <kaizumaki@gmail.com>
2022-01-31 23:22:50 +0900

    utilsにisSingleCardのfunctionを追加

--- utils/urls.ts
+++ utils/urls.ts
@@ -1,3 +1,7 @@
 export function isExternal(path: string): boolean {
   return /^https?:\/\//.test(path)
 }
+
+export function isSingleCard(path: string): boolean {
+  return path.startsWith('/cards/')
+}
