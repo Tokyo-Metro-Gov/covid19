cd4aae51c8b3d313e023021ff068f6c6e8f013ea
suzuryo <suzuryo@gmail.com>
2022-09-14 17:10:07 +0900

    rspec: 英語文言の末尾に半角スペースが入っているので取り除いて比較する

--- spec/feature/index_CardsReference_h3_spec.rb
+++ spec/feature/index_CardsReference_h3_spec.rb
@@ -19,9 +19,9 @@ describe 'page [/]', type: :feature do
           it 'has h3' do
             expect(find('.DataCard.ConfirmedCasesAttributesCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '陽性者の属性')
             expect(find('.DataCard.ConfirmedCasesByMunicipalitiesCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '陽性者数（区市町村別）')
-            expect(find('.DataCard.PositiveNumberOver65Card > div > div > div.DataView-Header > h3').text).to eq t(lang, '報告日別による陽性者数（65歳以上）の推移')
             expect(find('.DataCard.PositiveNumberByDevelopedDateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '発症日別による陽性者数の推移')
             expect(find('.DataCard.PositiveNumberByDiagnosedDateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '確定日別による陽性者数の推移')
+            expect(find('.DataCard.PositiveNumberOver65Card > div > div > div.DataView-Header > h3').text).to eq t(lang, '報告日別による陽性者数（65歳以上）の推移').strip
             expect(find('.DataCard.DeathsByDeathDateCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '死亡日別による死亡者数の推移')
             expect(find('.DataCard.MetroCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '都営地下鉄の利用者数の推移')
             expect(find('.DataCard.AgencyCard > div > div > div.DataView-Header > h3').text).to eq t(lang, '都庁来庁者数の推移')
--- spec/feature/index_SiteTopUpper_h3_spec.rb
+++ spec/feature/index_SiteTopUpper_h3_spec.rb
@@ -14,7 +14,7 @@ describe 'page [/]', type: :feature do
         context 'SiteTopUpper' do
           it 'has h3' do
             expect(find('.WhatsNew h3.WhatsNew-title').text).to eq t(lang, '最新のお知らせ')
-            expect(find('.StayingPopulation h3.StayingPopulation-heading').text).to eq t(lang, '都内の滞在人口の増減状況（毎週月曜日更新）')
+            expect(find('.StayingPopulation h3.StayingPopulation-heading').text).to eq t(lang, '都内の滞在人口の増減状況（毎週月曜日更新）').strip
             expect(find('.ConsultationWrap h3.StaticInfo-Text').text).to eq t(lang, 'ワクチン情報・変異株情報・検査情報等の新型コロナ関連情報はこちら')
           end
         end
